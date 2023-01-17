de068e0793891b1d103c192f3eac45b4d5cbca74
Till Rohrmann <till@restate.dev>
2023-01-17 11:05:10 +0100

    Replace Sender with Sink in Consensus module
    Sink is a trait that allows to replace the underlying communication
    channel implementation easily.

--- src/consensus/src/lib.rs
+++ src/consensus/src/lib.rs
@@ -1,15 +1,24 @@
-use tokio::sync::mpsc;
+use futures_sink::Sink;
+use std::marker::PhantomData;
 use tracing::debug;
 
 #[derive(Debug)]
-pub struct Consensus<T> {
-    command_writer: mpsc::Sender<T>
+pub struct Consensus<S, T>
+where
+    S: Sink<T>,
+{
+    _command_sink: S,
+    phantom_data: PhantomData<T>,
 }
 
-impl<T> Consensus<T> {
-    pub fn build(command_writer: mpsc::Sender<T>) -> Self {
+impl<S, T> Consensus<S, T>
+where
+    S: Sink<T>,
+{
+    pub fn build(command_sink: S) -> Self {
         Self {
-            command_writer,
+            _command_sink: command_sink,
+            phantom_data: PhantomData::default(),
         }
     }
 
--- src/worker/src/lib.rs
+++ src/worker/src/lib.rs
@@ -1,25 +1,29 @@
 use crate::partition::PartitionProcessor;
+use consensus::Consensus;
 use tokio::sync::mpsc;
 use tokio::try_join;
 use tokio_stream::wrappers::ReceiverStream;
-use consensus::Consensus;
+use tokio_util::sync::PollSender;
 
 mod fsm;
 mod partition;
 
 #[derive(Debug)]
 pub struct Worker {
-    consensus: Consensus<fsm::Command>,
+    consensus: Consensus<PollSender<fsm::Command>, fsm::Command>,
     processor: PartitionProcessor<ReceiverStream<fsm::Command>>,
 }
 
 impl Worker {
     pub fn build() -> Self {
         let (command_tx, command_rx) = mpsc::channel(1);
-        let consensus = Consensus::build(command_tx);
+        let consensus = Consensus::build(PollSender::new(command_tx));
         let processor = PartitionProcessor::build(command_rx.into());
 
-        Self { consensus, processor }
+        Self {
+            consensus,
+            processor,
+        }
     }
 
     pub async fn run(self, drain: drain::Watch) {
