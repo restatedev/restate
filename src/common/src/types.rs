524f11a75b6fd3cb3bf9c49ebc3adb230d17d0da
Till Rohrmann <till@restate.dev>
2023-02-03 10:07:20 +0100

    Introduce more common types
    The new common types are:
    
    * LeaderEpoch
    * PartitionId
    * PartitionLeaderEpoch
    * InovcationId
    
    This fixes #56.

--- src/common/src/types.rs
+++ src/common/src/types.rs
@@ -1,2 +1,15 @@
+use uuid::Uuid;
+
 /// Identifying a member of a raft group
-pub type PeerId = usize;
+pub type PeerId = u64;
+
+/// Identifying the leader epoch of a raft group leader
+pub type LeaderEpoch = u64;
+
+/// Identifying the partition
+pub type PartitionId = u64;
+
+/// The leader epoch of a given partition
+pub type PartitionLeaderEpoch = (PartitionId, LeaderEpoch);
+
+pub type InvocationId = Uuid;
