62697db935089ae7a5f2b962877510a2f54de09d
Till Rohrmann <till@restate.dev>
2023-02-08 22:02:25 +0100

    Add Rust docs to enum_inner macro

--- src/worker/src/partition/state_machine.rs
+++ src/worker/src/partition/state_machine.rs
@@ -37,6 +37,42 @@ pub(super) struct StateMachine {
     outbox_seq_number: u64,
 }
 
+/// Unwraps the inner value of a given enum variant.
+///
+/// # Panics
+/// If the enum variant does not match the given enum variant, it panics.
+///
+/// # Example
+///
+/// ```
+/// enum Enum {
+///     A(u64),
+///     B(String),
+/// }
+///
+/// let variant = Enum::A(42);
+///
+/// let inner = enum_inner!(variant, Enum::A);
+/// assert_eq!(inner, 42);
+/// ```
+///
+/// ## Expansion
+///
+/// The given example will expand to:
+///
+/// ```
+/// enum Enum {
+///     A(u64),
+///     B(String),
+/// }
+///
+/// let variant = Enum::A(42);
+///
+/// let inner = match variant {
+///     Enum::A(inner) => inner,
+///     _ => panic!()
+/// };
+/// ```
 macro_rules! enum_inner {
     ($ty:expr, $variant:path) => {
         match $ty {
