097396da66e252e7562f37b6fc1774a3ba81fb50
slinkydeveloper <francescoguard@gmail.com>
2023-02-13 09:50:19 +0100

    Added constants for message type ids

--- src/invoker/src/message/header.rs
+++ src/invoker/src/message/header.rs
@@ -52,7 +52,7 @@ impl MessageType {
         }
     }
 
-    fn allows_completed_flag(&self) -> bool {
+    fn have_completed_flag(&self) -> bool {
         matches!(
             self,
             MessageType::PollInputStreamEntry
@@ -63,30 +63,43 @@ impl MessageType {
         )
     }
 
-    fn allows_protocol_version(&self) -> bool {
+    fn have_protocol_version(&self) -> bool {
         *self == MessageType::Start
     }
 
-    fn allows_requires_ack_flag(&self) -> bool {
+    fn have_requires_ack_flag(&self) -> bool {
         matches!(self, MessageType::Custom(_))
     }
 }
 
+const START_MESSAGE_TYPE: u16 = 0x0000;
+const COMPLETION_MESSAGE_TYPE: u16 = 0x0001;
+const POLL_INPUT_STREAM_ENTRY_MESSAGE_TYPE: u16 = 0x0400;
+const OUTPUT_STREAM_ENTRY_MESSAGE_TYPE: u16 = 0x0401;
+const GET_STATE_ENTRY_MESSAGE_TYPE: u16 = 0x0800;
+const SET_STATE_ENTRY_MESSAGE_TYPE: u16 = 0x0801;
+const CLEAR_STATE_ENTRY_MESSAGE_TYPE: u16 = 0x0802;
+const SLEEP_ENTRY_MESSAGE_TYPE: u16 = 0x0C00;
+const INVOKE_ENTRY_MESSAGE_TYPE: u16 = 0x0C01;
+const BACKGROUND_INVOKE_ENTRY_MESSAGE_TYPE: u16 = 0x0C02;
+const AWAKEABLE_ENTRY_MESSAGE_TYPE: u16 = 0x0C03;
+const COMPLETE_AWAKEABLE_ENTRY_MESSAGE_TYPE: u16 = 0x0C04;
+
 impl From<MessageType> for MessageTypeId {
     fn from(mt: MessageType) -> Self {
         match mt {
-            MessageType::Start => 0x0000,
-            MessageType::Completion => 0x0001,
-            MessageType::PollInputStreamEntry => 0x0400,
-            MessageType::OutputStreamEntry => 0x0401,
-            MessageType::GetStateEntry => 0x0800,
-            MessageType::SetStateEntry => 0x0801,
-            MessageType::ClearStateEntry => 0x0802,
-            MessageType::SleepEntry => 0x0C00,
-            MessageType::InvokeEntry => 0x0C01,
-            MessageType::BackgroundInvokeEntry => 0x0C02,
-            MessageType::AwakeableEntry => 0x0C03,
-            MessageType::CompleteAwakeableEntry => 0x0C04,
+            MessageType::Start => START_MESSAGE_TYPE,
+            MessageType::Completion => COMPLETION_MESSAGE_TYPE,
+            MessageType::PollInputStreamEntry => POLL_INPUT_STREAM_ENTRY_MESSAGE_TYPE,
+            MessageType::OutputStreamEntry => OUTPUT_STREAM_ENTRY_MESSAGE_TYPE,
+            MessageType::GetStateEntry => GET_STATE_ENTRY_MESSAGE_TYPE,
+            MessageType::SetStateEntry => SET_STATE_ENTRY_MESSAGE_TYPE,
+            MessageType::ClearStateEntry => CLEAR_STATE_ENTRY_MESSAGE_TYPE,
+            MessageType::SleepEntry => SLEEP_ENTRY_MESSAGE_TYPE,
+            MessageType::InvokeEntry => INVOKE_ENTRY_MESSAGE_TYPE,
+            MessageType::BackgroundInvokeEntry => BACKGROUND_INVOKE_ENTRY_MESSAGE_TYPE,
+            MessageType::AwakeableEntry => AWAKEABLE_ENTRY_MESSAGE_TYPE,
+            MessageType::CompleteAwakeableEntry => COMPLETE_AWAKEABLE_ENTRY_MESSAGE_TYPE,
             MessageType::Custom(id) => id,
         }
     }
@@ -101,18 +114,18 @@ impl TryFrom<MessageTypeId> for MessageType {
 
     fn try_from(value: MessageTypeId) -> Result<Self, Self::Error> {
         match value {
-            0x0000 => Ok(MessageType::Start),
-            0x0001 => Ok(MessageType::Completion),
-            0x0400 => Ok(MessageType::PollInputStreamEntry),
-            0x0401 => Ok(MessageType::OutputStreamEntry),
-            0x0800 => Ok(MessageType::GetStateEntry),
-            0x0801 => Ok(MessageType::SetStateEntry),
-            0x0802 => Ok(MessageType::ClearStateEntry),
-            0x0C00 => Ok(MessageType::SleepEntry),
-            0x0C01 => Ok(MessageType::InvokeEntry),
-            0x0C02 => Ok(MessageType::BackgroundInvokeEntry),
-            0x0C03 => Ok(MessageType::AwakeableEntry),
-            0x0C04 => Ok(MessageType::CompleteAwakeableEntry),
+            START_MESSAGE_TYPE => Ok(MessageType::Start),
+            COMPLETION_MESSAGE_TYPE => Ok(MessageType::Completion),
+            POLL_INPUT_STREAM_ENTRY_MESSAGE_TYPE => Ok(MessageType::PollInputStreamEntry),
+            OUTPUT_STREAM_ENTRY_MESSAGE_TYPE => Ok(MessageType::OutputStreamEntry),
+            GET_STATE_ENTRY_MESSAGE_TYPE => Ok(MessageType::GetStateEntry),
+            SET_STATE_ENTRY_MESSAGE_TYPE => Ok(MessageType::SetStateEntry),
+            CLEAR_STATE_ENTRY_MESSAGE_TYPE => Ok(MessageType::ClearStateEntry),
+            SLEEP_ENTRY_MESSAGE_TYPE => Ok(MessageType::SleepEntry),
+            INVOKE_ENTRY_MESSAGE_TYPE => Ok(MessageType::InvokeEntry),
+            BACKGROUND_INVOKE_ENTRY_MESSAGE_TYPE => Ok(MessageType::BackgroundInvokeEntry),
+            AWAKEABLE_ENTRY_MESSAGE_TYPE => Ok(MessageType::AwakeableEntry),
+            COMPLETE_AWAKEABLE_ENTRY_MESSAGE_TYPE => Ok(MessageType::CompleteAwakeableEntry),
             v if ((v & CUSTOM_MESSAGE_MASK) != 0) => Ok(MessageType::Custom(v)),
             v => Err(UnknownMessageType(v)),
         }
@@ -165,11 +178,11 @@ pub struct MessageHeader {
     length: u32,
 
     // Flags
-    /// Only `CompletableEntries` allow completed flag. See [`MessageType#allows_completed_flag`].
+    /// Only `CompletableEntries` have completed flag. See [`MessageType#allows_completed_flag`].
     completed_flag: Option<bool>,
-    /// Only `StartMessage` allows protocol_version.
+    /// Only `StartMessage` has protocol_version.
     protocol_version: Option<u16>,
-    /// Only `Custom` entries allow requires ack flag.
+    /// Only `Custom` entries have requires ack flag.
     requires_ack_flag: Option<bool>,
 }
 
@@ -192,7 +205,7 @@ impl MessageHeader {
 
     #[inline]
     pub fn new_completable_entry(ty: MessageType, completed: bool, length: u32) -> Self {
-        debug_assert!(ty.allows_completed_flag());
+        debug_assert!(ty.have_completed_flag());
 
         Self::_new(ty, Some(completed), None, None, length)
     }
@@ -248,20 +261,22 @@ impl MessageHeader {
 impl TryFrom<u64> for MessageHeader {
     type Error = UnknownMessageType;
 
+    /// Deserialize the protocol header.
+    /// TODO link protocol spec.
     fn try_from(value: u64) -> Result<Self, Self::Error> {
         let ty_code = (value >> 48) as u16;
         let ty: MessageType = ty_code.try_into()?;
-        let completed_flag = if ty.allows_completed_flag() {
+        let completed_flag = if ty.have_completed_flag() {
             Some((value & COMPLETED_MASK) != 0)
         } else {
             None
         };
-        let protocol_version = if ty.allows_protocol_version() {
+        let protocol_version = if ty.have_protocol_version() {
             Some(((value & VERSION_MASK) >> 32) as u16)
         } else {
             None
         };
-        let requires_ack_flag = if ty.allows_requires_ack_flag() {
+        let requires_ack_flag = if ty.have_requires_ack_flag() {
             Some((value & REQUIRES_ACK_MASK) != 0)
         } else {
             None
@@ -279,6 +294,8 @@ impl TryFrom<u64> for MessageHeader {
 }
 
 impl From<MessageHeader> for u64 {
+    /// Serialize the protocol header.
+    /// TODO link protocol spec.
     fn from(message_header: MessageHeader) -> Self {
         let mut res =
             ((u16::from(message_header.ty) as u64) << 48) | (message_header.length as u64);
