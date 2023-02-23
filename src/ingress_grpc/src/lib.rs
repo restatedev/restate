2316c52696b1dfa734118ff84eac216f0281b35e
slinkydeveloper <francescoguard@gmail.com>
2023-02-23 13:54:54 +0100

    Port mocks from https://github.com/restatedev/restate/pull/113

--- src/ingress_grpc/src/lib.rs
+++ src/ingress_grpc/src/lib.rs
@@ -50,3 +50,51 @@ const _: () = {
         assert_send::<IngressResponseMessage>();
     }
 };
+
+// Contains some mocks we use in unit tests in this crate
+#[cfg(test)]
+mod mocks {
+    use super::*;
+
+    pub(super) mod pb {
+        #![allow(warnings)]
+        #![allow(clippy::all)]
+        #![allow(unknown_lints)]
+        include!(concat!(env!("OUT_DIR"), "/greeter.rs"));
+    }
+
+    use prost_reflect::{DescriptorPool, MethodDescriptor, ServiceDescriptor};
+
+    static DESCRIPTOR: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/file_descriptor_set.bin"));
+
+    pub(super) fn test_descriptor_pool() -> DescriptorPool {
+        DescriptorPool::decode(DESCRIPTOR).unwrap()
+    }
+
+    pub(super) fn test_descriptor_registry() -> InMemoryMethodDescriptorRegistry {
+        let registry = InMemoryMethodDescriptorRegistry::default();
+        registry.register(greeter_service_descriptor());
+        registry
+    }
+
+    pub(super) fn greeter_service_descriptor() -> ServiceDescriptor {
+        test_descriptor_pool()
+            .services()
+            .find(|svc| svc.full_name() == "greeter.Greeter")
+            .unwrap()
+    }
+
+    pub(super) fn greeter_greet_method_descriptor() -> MethodDescriptor {
+        greeter_service_descriptor()
+            .methods()
+            .find(|m| m.name() == "Greet")
+            .unwrap()
+    }
+
+    pub(super) fn greeter_get_count_method_descriptor() -> MethodDescriptor {
+        greeter_service_descriptor()
+            .methods()
+            .find(|m| m.name() == "GetCount")
+            .unwrap()
+    }
+}
