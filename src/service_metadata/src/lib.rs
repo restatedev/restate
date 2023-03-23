e04645d7c2cd4f24f954dda859051d7495a1bd4b
slinkydeveloper <francescoguard@gmail.com>
2023-03-23 16:19:18 +0100

    Introduce ServiceMetadata

--- src/service_metadata/src/lib.rs
+++ src/service_metadata/src/lib.rs
@@ -1,9 +1,9 @@
+use std::collections::HashMap;
+
 use common::retry_policy::RetryPolicy;
 use http::header::{HeaderName, HeaderValue};
 use http::Uri;
-use serde::Serialize;
-use serde_with::serde_as;
-use std::collections::HashMap;
+use service_key_extractor::ServiceInstanceType;
 
 mod descriptors_registry;
 mod endpoint_registry;
@@ -11,36 +11,16 @@ mod endpoint_registry;
 pub use descriptors_registry::{InMemoryMethodDescriptorRegistry, MethodDescriptorRegistry};
 pub use endpoint_registry::{InMemoryServiceEndpointRegistry, ServiceEndpointRegistry};
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq, Serialize)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum ProtocolType {
     RequestResponse,
     BidiStream,
 }
 
-#[derive(Debug, Clone, Default, Serialize)]
+#[derive(Debug, Clone, Default)]
 pub struct DeliveryOptions {
-    #[serde(with = "header_map")]
     additional_headers: HashMap<HeaderName, HeaderValue>,
     retry_policy: Option<RetryPolicy>,
-}
-
-mod header_map {
-    use http::{HeaderName, HeaderValue};
-    use serde::Serializer;
-    use std::collections::HashMap;
-
-    pub fn serialize<S: Serializer>(
-        headers: &HashMap<HeaderName, HeaderValue>,
-        ser: S,
-    ) -> Result<S::Ok, S::Error> {
-        ser.collect_map(headers.iter().map(|(k, v)| {
-            (
-                k.as_str(),
-                v.to_str()
-                    .expect("additional_headers map contains non-ASCII characters"),
-            )
-        }))
-    }
 }
 
 impl DeliveryOptions {
@@ -55,10 +35,8 @@ impl DeliveryOptions {
     }
 }
 
-#[serde_as]
-#[derive(Debug, Clone, Serialize)]
+#[derive(Debug, Clone)]
 pub struct EndpointMetadata {
-    #[serde_as(as = "serde_with::DisplayFromStr")]
     address: Uri,
     protocol_type: ProtocolType,
     delivery_options: DeliveryOptions,
@@ -91,5 +69,28 @@ impl EndpointMetadata {
 
     pub fn additional_headers(&self) -> &HashMap<HeaderName, HeaderValue> {
         &self.delivery_options.additional_headers
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ServiceMetadata {
+    name: String,
+    instance_type: ServiceInstanceType,
+}
+
+impl ServiceMetadata {
+    pub fn new(name: String, instance_type: ServiceInstanceType) -> Self {
+        Self {
+            name,
+            instance_type,
+        }
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn instance_type(&self) -> &ServiceInstanceType {
+        &self.instance_type
     }
 }
