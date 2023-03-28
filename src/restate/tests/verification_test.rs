3f44bc67e357dce74249150531a456904ac162cf
Jack Kleeman <jackkleeman@gmail.com>
2023-03-28 14:44:20 +0100

    Respond to review

--- src/restate/tests/verification_test.rs
+++ src/restate/tests/verification_test.rs
@@ -1,132 +1,158 @@
-use std::process::Command;
+extern crate core;
 
+use crate::Method::{Clear, Execute, Verify};
 use assert_cmd::prelude::*;
-use hyper::StatusCode;
+use common::retry_policy::RetryPolicy;
+use core::fmt;
+use futures_util::TryFutureExt;
+use hyper::{http, StatusCode};
+use rand::distributions::{Alphanumeric, DistString};
+use std::process::Command;
+use tempfile::Builder;
+use test_utils::test;
+use tracing::{info, warn};
 
-// Run programs // Add methods on commands
+struct SafeChild(std::process::Child);
 
-#[tokio::test]
-#[ignore]
+impl Drop for SafeChild {
+    fn drop(&mut self) {
+        match self.0.kill() {
+            Err(e) => warn!("Could not kill child process: {}", e),
+            Ok(_) => info!("Successfully killed child process"),
+        }
+    }
+}
+
+#[test(tokio::test)]
+#[ignore = "Ignored because it requires the verification service running on localhost:8000. See .github/workflows/ci.yaml and https://github.com/restatedev/restate-verification for more details."]
 async fn verification() -> Result<(), Box<dyn std::error::Error>> {
-    let (ready_tx, ready_rx) = tokio::sync::oneshot::channel();
-    let (kill_tx, kill_rx) = tokio::sync::oneshot::channel();
+    let seed = Alphanumeric.sample_string(&mut rand::thread_rng(), 16);
+    let seed = seed.as_str();
+    let width = 10;
+    let depth = 4;
+    let dir = Builder::new().prefix("restate").tempdir()?;
 
-    let restate = tokio::task::spawn(async {
-        let mut child = Command::cargo_bin("restate")
-            .unwrap()
-            .env(
-                "RUST_LOG",
-                "debug,tokio=debug,runtime=debug,hyper=warn,h2=warn,mio=warn",
-            )
-            .spawn()
-            .unwrap();
-        tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
-        ready_tx.send(()).unwrap();
-        kill_rx.await.unwrap();
-        child.kill().unwrap();
+    info!(
+        "using test parameters seed: {seed}, width: {width}, depth: {depth}, dir: {}",
+        dir.path().display(),
+    );
+
+    let child = Command::cargo_bin("restate")?
+        .env("RUST_LOG", "debug,hyper=warn,h2=warn,mio=warn")
+        .current_dir(dir.path())
+        .spawn()?;
+    let mut _safe_child = SafeChild(child);
+
+    let client = &hyper::Client::new();
+
+    let policy = RetryPolicy::FixedDelay {
+        interval: std::time::Duration::from_millis(1000),
+        max_attempts: 5,
+    };
+
+    policy
+        .retry_operation(|| discover(client, "http://127.0.0.1:8000"))
+        .await?;
+
+    let policy = RetryPolicy::FixedDelay {
+        interval: std::time::Duration::from_millis(3000),
+        max_attempts: 5,
+    };
+
+    let verification_result = call(client, Execute, seed, width, depth)
+        .and_then(|_| policy.retry_operation(|| call(client, Verify, seed, width, depth)))
+        .await;
+
+    // try and call clear even if verification failed
+    call(client, Clear, seed, width, depth).await?;
+
+    verification_result.unwrap_or_else(|e| {
+        panic!("verification failed with seed {seed}, width {width}, depth {depth} and error {e:?}")
     });
-
-    let test = tokio::task::spawn(async {
-        ready_rx.await.unwrap();
-
-        let client = hyper::Client::new();
-
-        let req = hyper::Request::builder()
-            .method(hyper::Method::POST)
-            .uri("http://127.0.0.1:8081/endpoint/discover")
-            .header("Content-Type", "application/json")
-            .body(hyper::Body::from(r#"{"uri": "http://127.0.0.1:8000"}"#))
-            .unwrap();
-
-        let resp = client.request(req).await.unwrap();
-        assert_eq!(
-            resp.status(),
-            StatusCode::OK,
-            "bad status code {} for discover: {}",
-            resp.status(),
-            String::from_utf8(
-                hyper::body::to_bytes(resp.into_body())
-                    .await
-                    .unwrap()
-                    .to_vec()
-            )
-            .unwrap()
-        );
-        let body = r#"{"width": 10, "depth": 4}"#;
-
-        let req = hyper::Request::builder()
-            .method(hyper::Method::POST)
-            .uri("http://127.0.0.1:9090/verifier.CommandVerifier/Execute")
-            .header("Content-Type", "application/json")
-            .body(hyper::Body::from(body))
-            .unwrap();
-
-        let resp = client.request(req).await.unwrap();
-        assert_eq!(
-            resp.status(),
-            StatusCode::OK,
-            "bad status code {} for Execute: {}",
-            resp.status(),
-            String::from_utf8(
-                hyper::body::to_bytes(resp.into_body())
-                    .await
-                    .unwrap()
-                    .to_vec()
-            )
-            .unwrap()
-        );
-
-        // wait a bit for the various non synchronous calls to complete
-        tokio::time::sleep(tokio::time::Duration::from_millis(10000)).await;
-
-        let req = hyper::Request::builder()
-            .method(hyper::Method::POST)
-            .uri("http://127.0.0.1:9090/verifier.CommandVerifier/Verify")
-            .header("Content-Type", "application/json")
-            .body(hyper::Body::from(body))
-            .unwrap();
-
-        let resp = client.request(req).await.unwrap();
-        assert_eq!(
-            resp.status(),
-            StatusCode::OK,
-            "bad status code {} for Verify: {}",
-            resp.status(),
-            String::from_utf8(
-                hyper::body::to_bytes(resp.into_body())
-                    .await
-                    .unwrap()
-                    .to_vec()
-            )
-            .unwrap()
-        );
-
-        let req = hyper::Request::builder()
-            .method(hyper::Method::POST)
-            .uri("http://127.0.0.1:9090/verifier.CommandVerifier/Clear")
-            .header("Content-Type", "application/json")
-            .body(hyper::Body::from(body))
-            .unwrap();
-
-        let resp = client.request(req).await.unwrap();
-        assert_eq!(
-            resp.status(),
-            StatusCode::OK,
-            "bad status code {} for Clear: {}",
-            resp.status(),
-            String::from_utf8(
-                hyper::body::to_bytes(resp.into_body())
-                    .await
-                    .unwrap()
-                    .to_vec()
-            )
-            .unwrap()
-        );
-    });
-
-    let test_result = test.await;
-    kill_tx.send(()).unwrap();
-    restate.await?;
-    test_result?;
     Ok(())
+}
+
+async fn discover(
+    client: &hyper::Client<hyper::client::HttpConnector, hyper::Body>,
+    uri: &str,
+) -> Result<(), RequestError> {
+    request(
+        client,
+        "http://127.0.0.1:8081/endpoint/discover".into(),
+        format!("{{\"uri\": \"{}\"}}", uri),
+    )
+    .await
+    .map(|_| ())
+}
+
+#[derive(Debug)]
+enum Method {
+    Execute,
+    Verify,
+    Clear,
+}
+
+impl fmt::Display for Method {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+enum CallError {
+    #[error("error calling {0}: {1}")]
+    Request(Method, RequestError),
+}
+
+async fn call(
+    client: &hyper::Client<hyper::client::HttpConnector, hyper::Body>,
+    method: Method,
+    seed: &str,
+    width: i32,
+    depth: i32,
+) -> Result<String, CallError> {
+    request(
+        client,
+        format!(
+            "http://127.0.0.1:9090/verifier.CommandVerifier/{:?}",
+            method
+        ),
+        format!(
+            "{{\"seed\": \"{}\", \"width\": {}, \"depth\": {}}}",
+            seed, width, depth
+        ),
+    )
+    .await
+    .map_err(|e| CallError::Request(method, e))
+}
+
+#[derive(thiserror::Error, Debug)]
+enum RequestError {
+    #[error(transparent)]
+    Http(#[from] http::Error),
+    #[error(transparent)]
+    Hyper(#[from] hyper::Error),
+    #[error(transparent)]
+    FailedToReadBody(#[from] std::string::FromUtf8Error),
+    #[error("status code {0} is not OK: {1}")]
+    BadStatusCode(StatusCode, String),
+}
+
+async fn request(
+    client: &hyper::Client<hyper::client::HttpConnector, hyper::Body>,
+    uri: String,
+    body: String,
+) -> Result<String, RequestError> {
+    let req = hyper::Request::builder()
+        .method(hyper::Method::POST)
+        .uri(uri)
+        .header("Content-Type", "application/json")
+        .body(hyper::Body::from(body))?;
+    let resp = client.request(req).await?;
+    let status = resp.status();
+    let resp_body = String::from_utf8(hyper::body::to_bytes(resp.into_body()).await?.to_vec())?;
+    match status {
+        StatusCode::OK => Ok(resp_body),
+        _ => Err(RequestError::BadStatusCode(status, resp_body)),
+    }
 }
