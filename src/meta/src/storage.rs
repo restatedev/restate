b00878e918591fe0f18aaa73ac267fd775d5021c
slinkydeveloper <francescoguard@gmail.com>
2023-03-28 10:09:17 +0200

    Add Storage reload to load back in memory all the endpoint and service metadata

--- src/meta/src/storage.rs
+++ src/meta/src/storage.rs
@@ -1,20 +1,24 @@
 use std::path::PathBuf;
+
 use base64::Engine;
 use futures::future::BoxFuture;
-use futures::FutureExt;
+use futures::stream::BoxStream;
+use futures::{stream, FutureExt, StreamExt};
 use hyper::Uri;
 use prost_reflect::DescriptorPool;
 use serde::{Deserialize, Serialize};
-use tokio::io;
-use tracing::{info, warn};
 use service_metadata::{EndpointMetadata, ServiceMetadata};
+use tokio::io;
+use tracing::{info, trace, warn};
 
 #[derive(Debug, thiserror::Error)]
 pub enum MetaStorageError {
     #[error("generic io error: {0}")]
     Io(#[from] io::Error),
     #[error("generic serde error: {0}. This is probably a runtime bug.")]
-    Serde(#[from] serde_json::Error)
+    Serde(#[from] serde_json::Error),
+    #[error("generic descriptor error: {0}. This is probably a runtime bug.")]
+    Descriptor(#[from] prost_reflect::DescriptorError),
 }
 
 pub trait MetaStorage {
@@ -25,6 +29,23 @@ pub trait MetaStorage {
         exposed_services: &[ServiceMetadata],
         descriptor_pool: DescriptorPool,
     ) -> BoxFuture<Result<(), MetaStorageError>>;
+
+    // TODO: Replace with async trait or proper future
+    //  This type could also be simplified simply returning the stream,
+    //  and eventually failing the first stream element in case the stream cannot be "created".
+    #[allow(clippy::type_complexity)]
+    fn reload(
+        &self,
+    ) -> BoxFuture<
+        'static,
+        Result<
+            BoxStream<
+                'static,
+                Result<(EndpointMetadata, Vec<ServiceMetadata>, DescriptorPool), MetaStorageError>,
+            >,
+            MetaStorageError,
+        >,
+    >;
 }
 
 // --- No-op in memory storage implementation, useful for testing environments
@@ -41,36 +62,61 @@ impl MetaStorage for InMemoryMetaStorage {
     ) -> BoxFuture<Result<(), MetaStorageError>> {
         async { Ok(()) }.boxed()
     }
+
+    fn reload(
+        &self,
+    ) -> BoxFuture<
+        'static,
+        Result<
+            BoxStream<
+                'static,
+                Result<(EndpointMetadata, Vec<ServiceMetadata>, DescriptorPool), MetaStorageError>,
+            >,
+            MetaStorageError,
+        >,
+    > {
+        async { Ok(stream::empty().boxed()) }.boxed()
+    }
 }
 
 // --- File based implementation, using serde
 
+const JSON_EXTENSION: &str = "json";
+const DESC_EXTENSION: &str = "desc";
+
 #[derive(Debug)]
 pub struct FileMetaStorage {
-    root_path: PathBuf
+    root_path: PathBuf,
 }
 
 impl FileMetaStorage {
     pub fn new(root_path: PathBuf) -> Self {
         info!("MetaStorage root path: {}", root_path.display());
 
-        Self {
-            root_path,
-        }
+        Self { root_path }
     }
 }
 
 #[derive(Serialize, Deserialize)]
 struct MetadataFile {
     endpoint_metadata: EndpointMetadata,
-    exposed_services: Vec<ServiceMetadata>
+    exposed_services: Vec<ServiceMetadata>,
 }
 
 impl MetaStorage for FileMetaStorage {
-    fn register_endpoint(&self, endpoint_metadata: &EndpointMetadata, exposed_services: &[ServiceMetadata], descriptor_pool: DescriptorPool) -> BoxFuture<Result<(), MetaStorageError>> {
+    fn register_endpoint(
+        &self,
+        endpoint_metadata: &EndpointMetadata,
+        exposed_services: &[ServiceMetadata],
+        descriptor_pool: DescriptorPool,
+    ) -> BoxFuture<Result<(), MetaStorageError>> {
         let endpoint_id = endpoint_id(endpoint_metadata.address());
-        let metadata_file_path = self.root_path.join(format!("{}.json", endpoint_id));
-        let descriptor_file_path = self.root_path.join(format!("{}.desc", endpoint_id));
+        let metadata_file_path = self
+            .root_path
+            .join(format!("{}.{}", endpoint_id, JSON_EXTENSION));
+        let descriptor_file_path = self
+            .root_path
+            .join(format!("{}.{}", endpoint_id, DESC_EXTENSION));
 
         // TODO to avoid these clones, we could use Cow and ZeroVec
         //  https://github.com/restatedev/restate/issues/230
@@ -83,17 +129,103 @@ impl MetaStorage for FileMetaStorage {
             remove_if_exists(&metadata_file_path).await?;
             remove_if_exists(&descriptor_file_path).await?;
 
-            tokio::fs::write(metadata_file_path, serde_json::to_vec_pretty(&metadata_file_struct)?).await?;
+            tokio::fs::write(
+                metadata_file_path,
+                serde_json::to_vec_pretty(&metadata_file_struct)?,
+            )
+            .await?;
             tokio::fs::write(descriptor_file_path, descriptor_pool.encode_to_vec()).await?;
 
             Ok(())
-        }.boxed()
+        }
+        .boxed()
+    }
+
+    fn reload(
+        &self,
+    ) -> BoxFuture<
+        'static,
+        Result<
+            BoxStream<
+                'static,
+                Result<(EndpointMetadata, Vec<ServiceMetadata>, DescriptorPool), MetaStorageError>,
+            >,
+            MetaStorageError,
+        >,
+    > {
+        let read_dir_fut = tokio::fs::read_dir(self.root_path.clone());
+        async move {
+            let read_dir = read_dir_fut.await?;
+
+            Ok(StreamExt::boxed(stream::try_unfold(
+                read_dir,
+                |mut read_dir| async move {
+                    // Loop until it finds the first .json file
+                    let mut entry;
+                    loop {
+                        entry = read_dir.next_entry().await?;
+                        match &entry {
+                            None => {
+                                // No more entries in this dir
+                                return Ok(None);
+                            }
+                            Some(dir_entry)
+                                if dir_entry
+                                    .path()
+                                    .extension()
+                                    .and_then(|os_str| os_str.to_str())
+                                    == Some(JSON_EXTENSION) =>
+                            {
+                                // Found a .json file
+                                break;
+                            }
+                            _ => {
+                                // Continue looping
+                            }
+                        }
+                    }
+
+                    // Entry is the json metadata descriptor
+                    let metadata_file_path = entry.unwrap().path();
+                    trace!("Reloading metadata file {}", metadata_file_path.display());
+                    let metadata_bytes = tokio::fs::read(&metadata_file_path).await?;
+                    let metadata_file: MetadataFile = serde_json::from_slice(&metadata_bytes)?;
+
+                    // Now load the descriptor pool
+                    let mut descriptor_pool_file = metadata_file_path.clone();
+                    descriptor_pool_file.set_extension(DESC_EXTENSION);
+                    trace!(
+                        "Reloading descriptor file {} for endpoint {}",
+                        descriptor_pool_file.display(),
+                        metadata_file.endpoint_metadata.address()
+                    );
+                    let mut descriptor_pool = DescriptorPool::new();
+                    descriptor_pool.decode_file_descriptor_set(
+                        tokio::fs::read(&descriptor_pool_file).await?.as_ref(),
+                    )?;
+
+                    Ok(Some((
+                        (
+                            metadata_file.endpoint_metadata,
+                            metadata_file.exposed_services,
+                            descriptor_pool,
+                        ),
+                        read_dir,
+                    )))
+                },
+            )))
+        }
+        .boxed()
     }
 }
 
 fn endpoint_id(uri: &Uri) -> String {
     // We use only authority and path, as those uniquely identify the endpoint.
-    let authority_and_path = format!("{}{}", uri.authority().expect("Must have authority"), uri.path());
+    let authority_and_path = format!(
+        "{}{}",
+        uri.authority().expect("Must have authority"),
+        uri.path()
+    );
     base64::engine::general_purpose::URL_SAFE_NO_PAD.encode(authority_and_path.as_bytes())
 }
 
