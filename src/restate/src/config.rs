8e084e23469c0205eed8cccdcfc1566feb2209df
slinkydeveloper <francescoguard@gmail.com>
2023-03-28 13:08:44 +0200

    Use serde + figment to configure Restate

---
+++ src/restate/src/config.rs
@@ -0,0 +1,26 @@
+use std::time::Duration;
+
+use serde::{Deserialize, Serialize};
+use serde_with::serde_as;
+
+/// This is the entrypoint struct for configuring the Restate single-binary deployment.
+#[serde_as]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Configuration {
+    #[serde_as(as = "serde_with::DisplayFromStr")]
+    pub shutdown_grace_period: humantime::Duration,
+    pub tracing: tracing_instrumentation::Options,
+    pub meta: meta::Options,
+    pub worker: worker::Options,
+}
+
+impl Default for Configuration {
+    fn default() -> Self {
+        Self {
+            shutdown_grace_period: Duration::from_secs(60).into(),
+            tracing: Default::default(),
+            meta: Default::default(),
+            worker: Default::default(),
+        }
+    }
+}
