4694885e10690ba0ac580dfedcbcde03ee3c1f0f
Till Rohrmann <till@restate.dev>
2023-05-29 14:30:29 +0200

    Set default in memory timer limit to 1024
    This fixes #386.

--- src/timer/src/options.rs
+++ src/timer/src/options.rs
@@ -3,13 +3,17 @@ use std::fmt::Debug;
 use tokio::sync::mpsc;
 
 /// # Timer options
-#[derive(Debug, Default, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 #[cfg_attr(feature = "options_schema", derive(schemars::JsonSchema))]
 #[cfg_attr(feature = "options_schema", schemars(rename = "TimerOptions"))]
 pub struct Options {
     /// # Num timers in memory limit
     ///
     /// The number of timers in memory limit is used to bound the amount of timers loaded in memory. If this limit is set, when exceeding it, the timers farther in the future will be spilled to disk.
+    #[cfg_attr(
+        feature = "options_schema",
+        schemars(default = "Options::default_num_timers_in_memory_limit")
+    )]
     num_timers_in_memory_limit: Option<usize>,
 }
 
@@ -33,5 +37,17 @@ impl Options {
             clock,
             channel_size,
         )
+    }
+
+    fn default_num_timers_in_memory_limit() -> usize {
+        1024
+    }
+}
+
+impl Default for Options {
+    fn default() -> Self {
+        Self {
+            num_timers_in_memory_limit: Some(Options::default_num_timers_in_memory_limit()),
+        }
     }
 }
