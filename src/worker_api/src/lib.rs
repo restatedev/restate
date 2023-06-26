a97f48e79dc0beab331d4aa5ccec67543b4811fa
slinkydeveloper <francescoguard@gmail.com>
2023-06-26 11:18:56 +0200

    Introduce worker_api to replace the worker_command from common.

---
+++ src/worker_api/src/lib.rs
@@ -0,0 +1,15 @@
+use restate_common::types::ServiceInvocationId;
+use std::future::Future;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("worker is unreachable")]
+    Unreachable,
+}
+
+pub trait Handle {
+    type Future: Future<Output = Result<(), Error>> + Send;
+
+    /// Send a command to kill an invocation. This command is best-effort.
+    fn kill_invocation(&self, service_invocation_id: ServiceInvocationId) -> Self::Future;
+}
