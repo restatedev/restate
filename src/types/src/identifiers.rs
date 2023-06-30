6963a031f158e24da02749eec34b4bbbb7d1ba1c
slinkydeveloper <francescoguard@gmail.com>
2023-06-30 14:19:54 +0200

    Feedback

--- src/types/src/identifiers.rs
+++ src/types/src/identifiers.rs
@@ -1,7 +1,13 @@
 //! Restate uses many identifiers to uniquely identify its components and entities.
 
+use base64::display::Base64Display;
+use base64::engine::general_purpose::STANDARD as BASE64_STANDARD;
+use base64::Engine;
 use bytes::Bytes;
 use bytestring::ByteString;
+use std::fmt;
+use std::fmt::Display;
+use std::str::FromStr;
 use uuid::Uuid;
 
 /// Identifying a member of a raft group
@@ -17,7 +23,7 @@ pub type PartitionId = u64;
 pub type PartitionLeaderEpoch = (PartitionId, LeaderEpoch);
 
 // Just an alias
-pub use crate::journal::EntryIndex;
+pub type EntryIndex = u32;
 
 /// Identifying to which partition a key belongs. This is unlike the [`PartitionId`]
 /// which identifies a consecutive range of partition keys.
@@ -29,8 +35,89 @@ pub type InvocationId = Uuid;
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 pub struct IngressId(pub std::net::SocketAddr);
 
-// Just an alias
-pub use crate::invocation::ServiceInvocationId;
+/// Id of a single service invocation.
+///
+/// A service invocation id is composed of a [`ServiceId`] and an [`InvocationId`]
+/// that makes the id unique.
+#[derive(Eq, Hash, PartialEq, Clone, Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct ServiceInvocationId {
+    /// Identifies the invoked service
+    pub service_id: ServiceId,
+    /// Uniquely identifies this invocation instance
+    pub invocation_id: InvocationId,
+}
+
+impl ServiceInvocationId {
+    pub fn new(
+        service_name: impl Into<ByteString>,
+        key: impl Into<Bytes>,
+        invocation_id: impl Into<InvocationId>,
+    ) -> Self {
+        Self::with_service_id(ServiceId::new(service_name, key), invocation_id)
+    }
+
+    pub fn with_service_id(service_id: ServiceId, invocation_id: impl Into<InvocationId>) -> Self {
+        Self {
+            service_id,
+            invocation_id: invocation_id.into(),
+        }
+    }
+}
+
+impl Display for ServiceInvocationId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}-{}-{}",
+            self.service_id.service_name,
+            Base64Display::new(&self.service_id.key, &BASE64_STANDARD),
+            self.invocation_id.as_simple()
+        )
+    }
+}
+
+#[derive(Debug, Default, thiserror::Error)]
+#[error("cannot parse the opaque id, bad format")]
+pub struct ServiceInvocationIdParseError {
+    #[source]
+    cause: Option<Box<dyn std::error::Error + Send + Sync + 'static>>,
+}
+
+impl ServiceInvocationIdParseError {
+    pub fn from_cause(cause: impl std::error::Error + Send + Sync + 'static) -> Self {
+        Self {
+            cause: Some(Box::new(cause)),
+        }
+    }
+}
+
+impl FromStr for ServiceInvocationId {
+    type Err = ServiceInvocationIdParseError;
+
+    fn from_str(str: &str) -> Result<Self, Self::Err> {
+        // This encoding is based on the fact that neither invocation_id
+        // nor service_name can contain the '-' character
+        // Invocation id is serialized as simple
+        // Service name follows the fullIdent ABNF here:
+        // https://protobuf.dev/reference/protobuf/proto3-spec/#identifiers
+        let mut splits: Vec<&str> = str.splitn(3, '-').collect();
+        if splits.len() != 3 {
+            return Err(ServiceInvocationIdParseError::default());
+        }
+        let invocation_id: Uuid = splits
+            .pop()
+            .unwrap()
+            .parse()
+            .map_err(ServiceInvocationIdParseError::from_cause)?;
+        let key = BASE64_STANDARD
+            .decode(splits.pop().unwrap())
+            .map_err(ServiceInvocationIdParseError::from_cause)?;
+        let service_name = splits.pop().unwrap().to_string();
+
+        Ok(ServiceInvocationId::new(service_name, key, invocation_id))
+    }
+}
 
 /// Id of a keyed service instance.
 ///
@@ -88,5 +175,43 @@ mod partitioner {
             // Safe to only take the lower 32 bits: See https://github.com/Cyan4973/xxHash/issues/453#issuecomment-696838445
             hasher.finish() as u32
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn roundtrip_test(expected_sid: ServiceInvocationId) {
+        let opaque_sid: String = expected_sid.to_string();
+        let actual_sid: ServiceInvocationId = opaque_sid.parse().unwrap();
+        assert_eq!(expected_sid, actual_sid);
+    }
+
+    #[test]
+    fn roundtrip_keyed_service_with_hyphens() {
+        roundtrip_test(ServiceInvocationId::new(
+            "my.example.Service",
+            "-------stuff------".as_bytes().to_vec(),
+            Uuid::now_v7(),
+        ));
+    }
+
+    #[test]
+    fn roundtrip_unkeyed_service() {
+        roundtrip_test(ServiceInvocationId::new(
+            "my.example.Service",
+            Uuid::now_v7().as_bytes().to_vec(),
+            Uuid::now_v7(),
+        ));
+    }
+
+    #[test]
+    fn roundtrip_empty_key() {
+        roundtrip_test(ServiceInvocationId::new(
+            "my.example.Service",
+            "".as_bytes().to_vec(),
+            Uuid::now_v7(),
+        ));
     }
 }
--- src/types/src/invocation.rs
+++ src/types/src/invocation.rs
@@ -1,103 +1,12 @@
 //! This module contains all the core types representing a service invocation.
 
 use crate::errors::UserErrorCode;
-use crate::identifiers::{EntryIndex, IngressId, InvocationId, ServiceId};
-use base64::display::Base64Display;
-use base64::engine::general_purpose::STANDARD as BASE64_STANDARD;
-use base64::Engine;
+use crate::identifiers::{EntryIndex, IngressId, ServiceInvocationId};
 use bytes::Bytes;
 use bytestring::ByteString;
 use opentelemetry_api::trace::{SpanContext, TraceContextExt};
-use std::fmt;
-use std::fmt::Display;
-use std::str::FromStr;
 use tracing::{info_span, Span};
 use tracing_opentelemetry::OpenTelemetrySpanExt;
-use uuid::Uuid;
-
-/// Id of a single service invocation.
-///
-/// A service invocation id is composed of a [`ServiceId`] and an [`InvocationId`]
-/// that makes the id unique.
-#[derive(Eq, Hash, PartialEq, Clone, Debug)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct ServiceInvocationId {
-    /// Identifies the invoked service
-    pub service_id: ServiceId,
-    /// Uniquely identifies this invocation instance
-    pub invocation_id: InvocationId,
-}
-
-impl ServiceInvocationId {
-    pub fn new(
-        service_name: impl Into<ByteString>,
-        key: impl Into<Bytes>,
-        invocation_id: impl Into<InvocationId>,
-    ) -> Self {
-        Self::with_service_id(ServiceId::new(service_name, key), invocation_id)
-    }
-
-    pub fn with_service_id(service_id: ServiceId, invocation_id: impl Into<InvocationId>) -> Self {
-        Self {
-            service_id,
-            invocation_id: invocation_id.into(),
-        }
-    }
-}
-
-impl Display for ServiceInvocationId {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{}-{}-{}",
-            self.service_id.service_name,
-            Base64Display::new(&self.service_id.key, &BASE64_STANDARD),
-            self.invocation_id.as_simple()
-        )
-    }
-}
-
-#[derive(Debug, Default, thiserror::Error)]
-#[error("cannot parse the opaque id, bad format")]
-pub struct ParseError {
-    #[source]
-    cause: Option<Box<dyn std::error::Error + Send + Sync + 'static>>,
-}
-
-impl ParseError {
-    pub fn from_cause(cause: impl std::error::Error + Send + Sync + 'static) -> Self {
-        Self {
-            cause: Some(Box::new(cause)),
-        }
-    }
-}
-
-impl FromStr for ServiceInvocationId {
-    type Err = ParseError;
-
-    fn from_str(str: &str) -> Result<Self, Self::Err> {
-        // This encoding is based on the fact that neither invocation_id
-        // nor service_name can contain the '-' character
-        // Invocation id is serialized as simple
-        // Service name follows the fullIdent ABNF here:
-        // https://protobuf.dev/reference/protobuf/proto3-spec/#identifiers
-        let mut splits: Vec<&str> = str.splitn(3, '-').collect();
-        if splits.len() != 3 {
-            return Err(ParseError::default());
-        }
-        let invocation_id: Uuid = splits
-            .pop()
-            .unwrap()
-            .parse()
-            .map_err(ParseError::from_cause)?;
-        let key = BASE64_STANDARD
-            .decode(splits.pop().unwrap())
-            .map_err(ParseError::from_cause)?;
-        let service_name = splits.pop().unwrap().to_string();
-
-        Ok(ServiceInvocationId::new(service_name, key, invocation_id))
-    }
-}
 
 /// Struct representing an invocation to a service. This struct is processed by Restate to execute the invocation.
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -243,46 +152,4 @@ impl SpanRelation {
             _ => {}
         };
     }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    macro_rules! roundtrip_test {
-        ($test_name:ident, $sid:expr) => {
-            mod $test_name {
-                use super::*;
-
-                #[test]
-                fn roundtrip() {
-                    let expected_sid = $sid;
-                    let opaque_sid: String = expected_sid.to_string();
-                    let actual_sid: ServiceInvocationId = opaque_sid.parse().unwrap();
-                    assert_eq!(expected_sid, actual_sid);
-                }
-            }
-        };
-    }
-
-    roundtrip_test!(
-        keyed_service_with_hyphens,
-        ServiceInvocationId::new(
-            "my.example.Service",
-            "-------stuff------".as_bytes().to_vec(),
-            Uuid::now_v7(),
-        )
-    );
-    roundtrip_test!(
-        unkeyed_service,
-        ServiceInvocationId::new(
-            "my.example.Service",
-            Uuid::now_v7().as_bytes().to_vec(),
-            Uuid::now_v7(),
-        )
-    );
-    roundtrip_test!(
-        empty_key,
-        ServiceInvocationId::new("my.example.Service", "".as_bytes().to_vec(), Uuid::now_v7(),)
-    );
 }
