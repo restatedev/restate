e046032913058c6a3d7504083ce8ce8719ee8c07
Francesco Guardiani <francescoguard@gmail.com>
2023-07-20 12:31:09 +0200

    Provide a knob to configure HTTP/2 keep-alive (#632)
    * Provide a knob to configure HTTP/2 keep-alive. Enable them by default.
    * Add the description of unset to the http2_keep_alive field as well.

--- src/invoker/src/lib.rs
+++ src/invoker/src/lib.rs
@@ -11,7 +11,10 @@ pub use effects::*;
 pub use entry_enricher::EntryEnricher;
 pub use handle::*;
 pub use journal_reader::JournalReader;
-pub use options::{Options, OptionsBuilder, OptionsBuilderError};
+pub use options::{
+    Http2KeepAliveOptions, Http2KeepAliveOptionsBuilder, Http2KeepAliveOptionsBuilderError,
+    Options, OptionsBuilder, OptionsBuilderError,
+};
 pub use service::*;
 pub use state_reader::{EagerState, StateReader};
 pub use status_handle::{InvocationErrorReport, InvocationStatusReport, StatusHandle};
