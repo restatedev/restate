dc5767e9bf2c486e700c2b1b860666a25c55329c
Ahmed Farghal <me@asoli.dev>
2023-11-23 14:18:02 +0000

    [CLI] Structure and WhoAmI command
    ## Summary:
    Introduces the initial structure of the project, adds the first command "whoami"
    but more importantly, it includes the environment loading from `.env` file, and
    some of the building blocks to load and save a config file and state in `$HOME/.config/restate`
    and the corresponding location on Windows (i.e. AppData/Local)
    
    The following environment variables can be used:
    * `RESTATE_CLI_CONFIG_HOME` override the CLI configuration home directory.
    * `RESTATE_CLI_CONFIG_FILE` override the CLI configuration file path.
    * `RESTATE_INGRESS_URL` sets the base url for ingress service.
    * `RESTATE_META_URL` sets the base url for the meta service.
    
    ## Test Plan:
    
    * Unit tests
    * Manual runs with `cargo run -- whoami`

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,1 +1,18 @@
-pub mod build_info;
+// Copyright (c) 2023 -  Restate Software, Inc., Restate GmbH.
+// All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+mod build_info;
+
+mod app;
+mod art;
+mod cli_env;
+mod commands;
+
+pub use app::CliApp;
