8575e5331920276da38bfcc1d669ce0df96f6964
Ahmed Farghal <me@asoli.dev>
2023-11-24 11:36:06 +0000

    [CLI] Initial implementation of service describe command
    The simplist version for svc describe command. This just to get things off the ground,
    and there are a lot of room for consolidating the rendering code across commands. This
    specific PR doesn't attempt to optimize for this.
    
    ```
    ❯ cargo -q run -- svc describe io.shoppingcart.ShoppingCartService
    
    Service Information
     Name:                    io.shoppingcart.ShoppingCartService
     Flavor (Instance Type):  Keyed
     Revision:                1
     Public:                  true
     Endpoint Id:             bG9jYWxob3N0OjkwODAv
     Endpoint Type:           HTTP
     Endpoint URL:            http://localhost:9080/
     Endpoint Protocol:       BidiStream
    
    Methods
     NAME            INPUT TYPE                             OUTPUT TYPE                             KEY FIELD INDEX
     RemoveProduct   io.shoppingcart.RemoveProductRequest   google.protobuf.Empty                   1
     GetAllProducts  io.shoppingcart.GetAllProductsRequest  io.shoppingcart.GetAllProductsResponse  1
     CreateCart      io.shoppingcart.CreateCartRequest      google.protobuf.Empty                   1
     Checkout        io.shoppingcart.CheckoutRequest        io.shoppingcart.CheckoutResponse        1
     AddProduct      io.shoppingcart.AddProductRequest      io.shoppingcart.AddProductResponse      1
    
    ```
    
    Another example:
    ```
    ❯ cargo -q run -- svc describe OrderService
    Service Information
     Name:                    OrderService
     Flavor (Instance Type):  Keyed
     Revision:                1
     Public:                  true
     Endpoint Id:             bG9jYWxob3N0OjkwODAv
     Endpoint Type:           HTTP
     Endpoint URL:            http://localhost:9080/
     Endpoint Protocol:       BidiStream
    
    Methods
     NAME            INPUT TYPE  OUTPUT TYPE  KEY FIELD INDEX
     getOrderStatus  RpcRequest  RpcResponse  1
     createOrder     RpcRequest  RpcResponse  1
     prepareOrder    RpcRequest  RpcResponse  1
     cancelOrder     RpcRequest  RpcResponse  1
    
    ```
    
    On errors:
    ```
    ❯ cargo -q run -- svc describe io.shoppingcart.ShoppingCartService
    Error: <UNKNOWN> The requested service 'io.shoppingcart.ShoppingCartService' does not exist
    
      -> Http status code 404 Not Found at 'http://localhost:9070/services/io.shoppingcart.ShoppingCartService'
    
    ```

---
+++ cli/src/commands/services/describe.rs
@@ -0,0 +1,126 @@
+// Copyright (c) 2023 -  Restate Software, Inc., Restate GmbH.
+// All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+use std::collections::HashMap;
+
+use cling::prelude::*;
+use comfy_table::Table;
+
+use crate::cli_env::CliEnv;
+use crate::console::c_println;
+use crate::meta_client::{MetaClient, MetaClientInterface};
+use crate::ui::console::{Styled, StyledTable};
+use crate::ui::stylesheet::Style;
+use restate_meta::rest_api::endpoints::{ServiceEndpoint, ServiceEndpointResponse};
+
+use anyhow::Result;
+
+#[derive(Run, Parser, Collect, Clone)]
+#[cling(run = "run_describe")]
+pub struct Describe {
+    /// Service name
+    name: String,
+}
+
+pub async fn run_describe(State(env): State<CliEnv>, describe_opts: &Describe) -> Result<()> {
+    let client = MetaClient::new(&env)?;
+    let svc = client
+        .get_service(&describe_opts.name)
+        .await?
+        .into_body()
+        .await?;
+
+    let mut table = Table::new_styled(&env.ui_config);
+    table.add_row(vec!["Name:", svc.name.as_ref()]);
+    table.add_row(vec![
+        "Flavor (Instance Type):",
+        &format!("{:?}", svc.instance_type),
+    ]);
+    table.add_row(vec!["Revision:", &svc.revision.to_string()]);
+    table.add_row(vec!["Public:", &svc.public.to_string()]);
+    table.add_row(vec!["Endpoint Id:", &svc.endpoint_id.to_string()]);
+
+    let endpoint = client
+        .get_endpoint(&svc.endpoint_id)
+        .await?
+        .into_body()
+        .await?;
+    add_endpoint(&endpoint, &mut table);
+
+    c_println!("{}", Styled(Style::Info, "Service Information"));
+    c_println!("{}", table);
+
+    // Methods
+    c_println!();
+    c_println!("{}", Styled(Style::Info, "Methods"));
+    let mut table = Table::new_styled(&env.ui_config);
+    table.set_header(vec!["NAME", "INPUT TYPE", "OUTPUT TYPE", "KEY FIELD INDEX"]);
+    for method in svc.methods {
+        table.add_row(vec![
+            &method.name,
+            &method.input_type,
+            &method.output_type,
+            &method
+                .key_field_number
+                .as_ref()
+                .map(ToString::to_string)
+                .unwrap_or_default(),
+        ]);
+    }
+    c_println!("{}", table);
+
+    Ok(())
+}
+
+fn add_endpoint(endpoint: &ServiceEndpointResponse, table: &mut Table) {
+    let additional_headers = match &endpoint.service_endpoint {
+        ServiceEndpoint::Http {
+            uri,
+            protocol_type,
+            additional_headers,
+        } => {
+            table.add_row(vec!["Endpoint Type:", "HTTP"]);
+            table.add_row(vec!["Endpoint URL:", &uri.to_string()]);
+            let protocol_type = match protocol_type {
+                restate_schema_api::endpoint::ProtocolType::RequestResponse => "RequestResponse",
+                restate_schema_api::endpoint::ProtocolType::BidiStream => "BidiStream",
+            }
+            .to_string();
+            table.add_row(vec!["Endpoint Protocol:", &protocol_type]);
+            additional_headers.clone()
+        }
+        ServiceEndpoint::Lambda {
+            arn,
+            assume_role_arn,
+            additional_headers,
+        } => {
+            table.add_row(vec!["Endpoint Type:", "AWS Lambda"]);
+            table.add_row(vec!["Endpoint ARN:", &arn.to_string()]);
+            table.add_row_if(
+                |_, _| assume_role_arn.is_some(),
+                vec![
+                    "Endpoint Assume Role ARN:",
+                    &assume_role_arn.as_ref().unwrap(),
+                ],
+            );
+            additional_headers.clone()
+        }
+    };
+
+    let additional_headers: HashMap<http::HeaderName, http::HeaderValue> =
+        additional_headers.into();
+
+    for (header, value) in additional_headers.iter() {
+        table.add_row(vec![
+            "Endpoint Additional Header:",
+            &format!("{}: {}", header, value.to_str().unwrap_or("<BINARY>")),
+        ]);
+    }
+}
