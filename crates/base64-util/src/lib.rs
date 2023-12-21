f43dde0f5649b672a40820fbefb1217d9f6050c1
Ahmed Farghal <me@asoli.dev>
2023-12-21 12:26:47 +0000

    Do not use padding in server-side base64 encoding
    And make AwakeableId clone + display

--- crates/base64-util/src/lib.rs
+++ crates/base64-util/src/lib.rs
@@ -14,6 +14,7 @@ use base64::engine::{DecodePaddingMode, GeneralPurpose, GeneralPurposeConfig};
 // We have this custom configuration for padding because Node's base64url implementation will omit padding,
 // but the spec https://datatracker.ietf.org/doc/html/rfc4648#section-5 doesn't specify that padding MUST be omitted.
 // Hence, Other implementations might include it, so we keep this relaxed.
-pub const INDIFFERENT_PAD: GeneralPurposeConfig =
-    GeneralPurposeConfig::new().with_decode_padding_mode(DecodePaddingMode::Indifferent);
+pub const INDIFFERENT_PAD: GeneralPurposeConfig = GeneralPurposeConfig::new()
+    .with_decode_padding_mode(DecodePaddingMode::Indifferent)
+    .with_encode_padding(false);
 pub const URL_SAFE: GeneralPurpose = GeneralPurpose::new(&alphabet::URL_SAFE, INDIFFERENT_PAD);
