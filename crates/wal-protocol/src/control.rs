2304560dcaccf26c323dd79cd0998b579310ae40
Ahmed Farghal <me@asoli.dev>
2024-02-08 13:17:30 +0000

    WAL protocol init
    This defines a provisional envelope and WAL message types. Currently unused

---
+++ crates/wal-protocol/src/control.rs
@@ -0,0 +1,24 @@
+// Copyright (c) 2024 -  Restate Software, Inc., Restate GmbH.
+// All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+use restate_types::GenerationalNodeId;
+
+///
+/// The header of the message envelope already contains all information needed for the
+/// leadership announcement. The leader_epoch
+///
+/// Reserved for future use.
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct AnnounceLeader {
+    /// The "generational" node id of the processor that is being announced as leader.
+    /// This is used because the header only contains the plan id.
+    node_id: GenerationalNodeId,
+}
