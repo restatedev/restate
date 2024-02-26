55204437634e2af267773e6af80580b61d160808
Till Rohrmann <till@restate.dev>
2024-02-26 13:20:24 +0100

    Move state machine command application into inner struct
    Moving the logic of command application into an inner struct separates a
    bit better the leadership acquisition logic and the command application.

--- crates/worker/src/partition/mod.rs
+++ crates/worker/src/partition/mod.rs
@@ -10,18 +10,16 @@
 
 use crate::metric_definitions::{PARTITION_ACTUATOR_HANDLED, PARTITION_TIMER_DUE_HANDLED};
 use crate::partition::action_effect_handler::ActionEffectHandler;
-use crate::partition::leadership::{ActionEffect, LeadershipState, TaskResult};
-use crate::partition::state_machine::{
-    ActionCollector, DeduplicatingStateMachine, Effects, InterpretationResult,
-};
+use crate::partition::leadership::{ActionEffect, ActionEffectStream, LeadershipState, TaskResult};
+use crate::partition::state_machine::{DeduplicatingStateMachine, Effects};
 use crate::partition::storage::{PartitionStorage, Transaction};
 use crate::util::IdentitySender;
 use futures::StreamExt;
 use metrics::counter;
 use restate_network::Networking;
 use restate_schema_impl::Schemas;
-use restate_storage_rocksdb::RocksDBStorage;
-use restate_types::identifiers::{PartitionId, PartitionKey, PeerId};
+use restate_storage_rocksdb::{RocksDBStorage, RocksDBTransaction};
+use restate_types::identifiers::{LeaderEpoch, PartitionId, PartitionKey, PeerId};
 use std::fmt::Debug;
 use std::future::Future;
 use std::marker::PhantomData;
@@ -46,6 +44,7 @@ use restate_wal_protocol::Envelope;
 type ConsensusReader = mpsc::Receiver<restate_consensus::Command<Envelope>>;
 type ConsensusWriter = IdentitySender<Envelope>;
 use restate_ingress_dispatcher::IngressDispatcherInputSender;
+use restate_network::PartitionProcessorSender;
 
 #[derive(Debug)]
 pub(super) struct PartitionProcessor<RawEntryCodec, InvokerInputSender, NetworkHandle> {
@@ -139,28 +138,27 @@ where
             ..
         } = self;
 
-        // The max number of effects should be 2 atm (e.g. RegisterTimer and AppendJournalEntry)
-        let mut effects = Effects::with_capacity(2);
-
         let mut partition_storage =
             PartitionStorage::new(partition_id, partition_key_range.clone(), rocksdb_storage);
 
-        let (mut actuator_stream, mut leadership_state) = LeadershipState::follower(
+        let state_machine =
+            Self::create_state_machine::<RawEntryCodec>(&mut partition_storage).await?;
+
+        let mut inner = Inner::new_as_follower(
+            state_machine,
             peer_id,
             partition_id,
+            partition_key_range,
+            schemas,
             timer_service_options,
             channel_size,
             invoker_tx,
             network_handle,
             ack_tx,
-            consensus_writer.clone(),
+            consensus_writer,
             ingress_tx,
+            options.max_batch_duration.map(Into::into),
         );
-
-        let mut state_machine =
-            Self::create_state_machine::<RawEntryCodec>(&mut partition_storage).await?;
-
-        let mut actuator_output_handler = None;
 
         loop {
             tokio::select! {
@@ -172,51 +170,13 @@ where
                     while let Some(command) = next_command.take() {
                         match command {
                             restate_consensus::Command::Apply(command) => {
-                                // Clear the effects to reuse the vector
-                                effects.clear();
-
-                                // Prepare transaction
-                                let transaction = partition_storage.create_transaction();
-
-                                // Prepare message collector
-                                let is_leader = leadership_state.is_leader();
-                                let message_collector = leadership_state.into_message_collector();
-
-                                let (application_result, command) = Self::apply_envelope(
-                                    &mut state_machine,
-                                    command,
-                                    &mut effects,
-                                    transaction,
-                                    message_collector,
-                                    is_leader,
-                                    &mut consensus_reader,
-                                    options.max_batch_duration.map(Into::into))
-                                .await?;
-
-                                next_command = command;
-
-                                // Commit actuator messages
-                                let message_collector = application_result.commit().await?;
-                                leadership_state = message_collector.send().await?;
+                                next_command = inner.apply_command(command, &mut consensus_reader, partition_storage.create_transaction()).await?;
                             }
                             restate_consensus::Command::BecomeLeader(leader_epoch) => {
-                                debug!(restate.partition.peer = %peer_id, restate.partition.id = %partition_id, restate.partition.leader_epoch = %leader_epoch, "Become leader");
-
-                                (actuator_stream, leadership_state) = leadership_state.become_leader(
-                                    leader_epoch,
-                                    partition_key_range.clone(),
-                                    &mut partition_storage,
-                                    schemas.clone(),
-
-                                )
-                                .await?;
-
-                                actuator_output_handler = Some(ActionEffectHandler::new(partition_id, leader_epoch, partition_key_range.clone(), consensus_writer.clone()));
+                                inner.become_leader(leader_epoch, &mut partition_storage).await?;
                             }
                             restate_consensus::Command::BecomeFollower => {
-                                info!(restate.partition.peer = %peer_id, restate.partition.id = %partition_id, "Become follower");
-                                (actuator_stream, leadership_state) = leadership_state.become_follower().await?;
-                                actuator_output_handler = None;
+                                inner.become_follower().await?
                             },
                             restate_consensus::Command::ApplySnapshot => {
                                 unimplemented!("Not supported yet.");
@@ -227,26 +187,14 @@ where
                         }
                     }
                 },
-                actuator_output = actuator_stream.next() => {
-                    counter!(PARTITION_ACTUATOR_HANDLED).increment(1);
-                    let actuator_output = actuator_output.ok_or_else(|| anyhow::anyhow!("actuator stream is closed"))?;
-                    actuator_output_handler.as_ref().expect("actuator output handler must be present when being leader").handle(actuator_output).await;
-                },
-                task_result = leadership_state.run_tasks() => {
-                    match task_result {
-                        TaskResult::Timer(timer) => {
-                            counter!(PARTITION_TIMER_DUE_HANDLED).increment(1);
-                            actuator_output_handler.as_ref().expect("actuator output handler must be present when being leader").handle(ActionEffect::Timer(timer)).await;
-                        },
-                        TaskResult::TerminatedTask(result) => {
-                            Err(result)?
-                        }
-                    }
-                },
+                result = inner.poll_actions() => {
+                    result?;
+                }
             }
         }
+
         debug!(%peer_id, %partition_id, "Shutting partition processor down.");
-        let _ = leadership_state.become_follower().await;
+        let _ = inner.become_follower().await;
 
         Ok(())
     }
@@ -264,34 +212,151 @@ where
 
         Ok(state_machine)
     }
+}
 
+struct Inner<C, I, N> {
+    peer_id: PeerId,
+    partition_id: PartitionId,
+    partition_key_range: RangeInclusive<PartitionKey>,
+    schemas: Schemas,
+    state_machine: DeduplicatingStateMachine<C>,
+    leadership_state: Option<LeadershipState<I, N>>,
+    action_effect_stream: ActionEffectStream,
+    action_effect_handler: Option<ActionEffectHandler>,
+    effects_buffer: Effects,
+    consensus_writer: ConsensusWriter,
+    max_batch_duration: Option<Duration>,
+}
+
+impl<C, I, N> Inner<C, I, N>
+where
+    C: restate_types::journal::raw::RawEntryCodec + Default + Debug,
+    I: restate_invoker_api::ServiceHandle + Clone,
+    N: restate_network::NetworkHandle<shuffle::ShuffleInput, Envelope>,
+{
     #[allow(clippy::too_many_arguments)]
-    pub async fn apply_envelope<
-        TransactionType: restate_storage_api::Transaction + Send,
-        Collector: ActionCollector,
-    >(
-        state_machine: &mut DeduplicatingStateMachine<RawEntryCodec>,
-        envelope: Envelope,
-        effects: &mut Effects,
-        transaction: Transaction<TransactionType>,
-        message_collector: Collector,
-        is_leader: bool,
-        consensus_reader: &mut ConsensusReader,
+    fn new_as_follower(
+        state_machine: DeduplicatingStateMachine<C>,
+        peer_id: PeerId,
+        partition_id: PartitionId,
+        partition_key_range: RangeInclusive<PartitionKey>,
+        schemas: Schemas,
+        timer_service_options: restate_timer::Options,
+        channel_size: usize,
+        invoker_tx: I,
+        network_handle: N,
+        ack_tx: PartitionProcessorSender<AckResponse>,
+        consensus_writer: ConsensusWriter,
+        ingress_tx: IngressDispatcherInputSender,
         max_batch_duration: Option<Duration>,
-    ) -> Result<
-        (
-            InterpretationResult<Transaction<TransactionType>, Collector>,
-            Option<restate_consensus::Command<Envelope>>,
-        ),
-        state_machine::Error,
-    > {
-        let max_batch_duration_start =
-            max_batch_duration.map(|duration| (duration, Instant::now()));
+    ) -> Self {
+        let (action_effect_stream, leadership_state) = LeadershipState::follower(
+            peer_id,
+            partition_id,
+            timer_service_options,
+            channel_size,
+            invoker_tx,
+            network_handle,
+            ack_tx,
+            consensus_writer.clone(),
+            ingress_tx,
+        );
+
+        Inner {
+            peer_id,
+            partition_id,
+            partition_key_range,
+            schemas,
+            state_machine,
+            leadership_state: Some(leadership_state),
+            action_effect_stream,
+            consensus_writer,
+            action_effect_handler: None,
+            effects_buffer: Effects::with_capacity(128),
+            max_batch_duration,
+        }
+    }
+
+    async fn become_leader(
+        &mut self,
+        leader_epoch: LeaderEpoch,
+        partition_storage: &mut PartitionStorage<RocksDBStorage>,
+    ) -> anyhow::Result<()> {
+        debug!(restate.partition.peer = %self.peer_id, restate.partition.id = %self.partition_id, restate.partition.leader_epoch = %leader_epoch, "Become leader");
+
+        let (action_effect_stream, leadership_state) = self
+            .leadership_state
+            .take()
+            .expect("leadership state must be set")
+            .become_leader(
+                leader_epoch,
+                self.partition_key_range.clone(),
+                partition_storage,
+                self.schemas.clone(),
+            )
+            .await?;
+
+        self.action_effect_stream = action_effect_stream;
+        self.leadership_state = Some(leadership_state);
+
+        self.action_effect_handler = Some(ActionEffectHandler::new(
+            self.partition_id,
+            leader_epoch,
+            self.partition_key_range.clone(),
+            self.consensus_writer.clone(),
+        ));
+
+        Ok(())
+    }
+
+    async fn become_follower(&mut self) -> anyhow::Result<()> {
+        info!(restate.partition.peer = %self.peer_id, restate.partition.id = %self.partition_id, "Become follower");
+        let (action_effect_stream, leadership_state) = self
+            .leadership_state
+            .take()
+            .expect("leadership state must be set")
+            .become_follower()
+            .await?;
+
+        self.action_effect_stream = action_effect_stream;
+        self.leadership_state = Some(leadership_state);
+        self.action_effect_handler = None;
+
+        Ok(())
+    }
+
+    async fn apply_command(
+        &mut self,
+        command: Envelope,
+        consensus_reader: &mut ConsensusReader,
+        transaction: Transaction<RocksDBTransaction<'_>>,
+    ) -> anyhow::Result<Option<restate_consensus::Command<Envelope>>> {
+        self.effects_buffer.clear();
+
+        let leadership_state = self
+            .leadership_state
+            .take()
+            .expect("leadership state must be present");
+        let is_leader = leadership_state.is_leader();
+        let message_collector = leadership_state.into_message_collector();
+
+        let max_batch_duration_start = self
+            .max_batch_duration
+            .map(|duration| (duration, Instant::now()));
 
         // Apply state machine
-        let mut application_result = state_machine
-            .apply(envelope, effects, transaction, message_collector, is_leader)
+        let mut application_result = self
+            .state_machine
+            .apply(
+                command,
+                &mut self.effects_buffer,
+                transaction,
+                message_collector,
+                is_leader,
+            )
             .await?;
+
+        let mut next_command = None;
 
         while max_batch_duration_start
             .map(|(max_duration, start)| start.elapsed() < max_duration)
@@ -300,18 +365,52 @@ where
             if let Ok(command) = consensus_reader.try_recv() {
                 if let restate_consensus::Command::Apply(envelope) = command {
                     let (transaction, message_collector) = application_result.into_inner();
-                    application_result = state_machine
-                        .apply(envelope, effects, transaction, message_collector, is_leader)
+                    application_result = self
+                        .state_machine
+                        .apply(
+                            envelope,
+                            &mut self.effects_buffer,
+                            transaction,
+                            message_collector,
+                            is_leader,
+                        )
                         .await?;
                 } else {
-                    return Ok((application_result, Some(command)));
+                    next_command = Some(command);
+                    break;
                 }
             } else {
                 break;
             }
         }
 
-        Ok((application_result, None))
+        let message_collector = application_result.commit().await?;
+        self.leadership_state = Some(message_collector.send().await?);
+
+        Ok(next_command)
+    }
+
+    async fn poll_actions(&mut self) -> anyhow::Result<()> {
+        tokio::select! {
+            actuator_output = self.action_effect_stream.next() => {
+                    counter!(PARTITION_ACTUATOR_HANDLED).increment(1);
+                    let actuator_output = actuator_output.ok_or_else(|| anyhow::anyhow!("actuator stream is closed"))?;
+                    self.action_effect_handler.as_ref().expect("actuator output handler must be present when being leader").handle(actuator_output).await;
+                },
+            task_result = self.leadership_state.as_mut().expect("leadership state must be set").run_tasks() => {
+                match task_result {
+                    TaskResult::Timer(timer) => {
+                        counter!(PARTITION_TIMER_DUE_HANDLED).increment(1);
+                        self.action_effect_handler.as_ref().expect("actuator output handler must be present when being leader").handle(ActionEffect::Timer(timer)).await;
+                    },
+                    TaskResult::TerminatedTask(result) => {
+                        Err(result)?
+                    }
+                }
+            },
+        }
+
+        Ok(())
     }
 }
 
