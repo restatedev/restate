8d1364e62c1562c9e6650a85bb4fc71fcc855e5f
slinkydeveloper <francescoguard@gmail.com>
2024-02-29 18:21:38 +0100

    Remove contracts of services we don't need anymore

--- crates/pb/build.rs
+++ crates/pb/build.rs
@@ -281,20 +281,12 @@ mod manual_response_built_in_service_gen {
 fn main() -> std::io::Result<()> {
     prost_build::Config::new()
         .bytes(["."])
-        .file_descriptor_set_path(
-            PathBuf::from(env::var("OUT_DIR").expect("OUT_DIR environment variable not set"))
-                .join("file_descriptor_set.bin"),
-        )
         .service_generator(Box::new(
             MultiServiceGenerator::new()
                 .with_svc("dev.restate.Awakeables", Box::new(RestateBuiltInServiceGen))
                 .with_svc(
                     "dev.restate.internal.Proxy",
                     Box::new(RestateBuiltInServiceGen),
-                )
-                .with_svc(
-                    "dev.restate.Ingress",
-                    Box::<ManualResponseRestateBuiltInServiceGen>::default(),
                 )
                 .with_svc(
                     "dev.restate.internal.RemoteContext",
@@ -340,9 +332,6 @@ fn main() -> std::io::Result<()> {
         .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(
             &[
-                "proto/grpc/health/v1/health.proto",
-                "proto/grpc/reflection/v1/reflection.proto",
-                "proto/grpc/reflection/v1alpha/reflection.proto",
                 "proto/dev/restate/ext.proto",
                 "proto/dev/restate/services.proto",
                 "proto/dev/restate/internal/services.proto",
