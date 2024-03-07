25bbadde357d3a6a0a2bc6204e4f52fad93e7cff
slinkydeveloper <francescoguard@gmail.com>
2024-03-07 11:36:52 +0100

    Remove the old service-protocol discovery logic

--- crates/service-protocol/src/lib.rs
+++ crates/service-protocol/src/lib.rs
@@ -18,8 +18,6 @@ pub mod codec;
 pub mod discovery;
 #[cfg(feature = "message")]
 pub mod message;
-#[cfg(feature = "discovery")]
-pub mod old_discovery;
 
 #[cfg(feature = "awakeable-id")]
 pub mod awakeable_id;
