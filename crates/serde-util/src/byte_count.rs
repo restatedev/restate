1939ddbba9dd7eaf23fb0f499c30e87c30adbe80
Ahmed Farghal <me@asoli.dev>
2024-04-24 08:36:14 +0100

    Configuration support for human-readable bytes
    - Add human-readable bytes support to the configuration
    - Full review of naming
    - Remove values the default to MAX and use Option instead
    - Various fixes for better default values and hiding some internal options
    
    Default config:
    
    ```toml
    roles = [
        "worker",
        "admin",
        "metadata-store",
    ]
    cluster-name = "localcluster"
    allow-bootstrap = true
    metadata-store-address = "http://127.0.0.1:5123/"
    bind-address = "0.0.0.0:5122"
    advertised-address = "http://127.0.0.1:5122/"
    shutdown-timeout = "1m"
    tracing-filter = "info"
    log-filter = "info"
    log-format = "pretty"
    log-disable-ansi-codes = false
    disable-prometheus = false
    rocksdb-total-memory-size = "4.0 GB"
    rocksdb-total-memtables-size = "0 B"
    rocksdb-high-priority-bg-threads = 2
    
    [http-keep-alive-options]
    interval = "40s"
    timeout = "20s"
    
    [worker]
    internal-queue-length = 64
    rocksdb-write-buffer-size = "256.0 MB"
    rocksdb-max-total-wal-size = "2.0 GB"
    rocksdb-disable-wal = true
    rocksdb-disable-statistics = false
    rocksdb-max-background-jobs = 12
    rocksdb-compaction-readahead-size = "2.0 MB"
    rocksdb-statistics-level = "except-detailed-timers"
    bootstrap-num-partitions = 64
    
    [worker.invoker]
    inactivity-timeout = "1m"
    abort-timeout = "1m"
    message-size-warning = "10.0 MB"
    
    [worker.invoker.retry-policy]
    type = "exponential"
    initial-interval = "50ms"
    factor = 2.0
    max-interval = "10s"
    
    [admin]
    bind-address = "0.0.0.0:9070"
    
    [admin.query-engine]
    pgsql-bind-address = "0.0.0.0:9071"
    
    [ingress]
    bind-address = "0.0.0.0:8080"
    kafka-clusters = []
    
    [bifrost]
    default-provider = "local"
    
    [bifrost.local]
    rocksdb-write-buffer-size = "256.0 MB"
    rocksdb-max-total-wal-size = "2.0 GB"
    rocksdb-disable-wal = false
    rocksdb-disable-statistics = false
    rocksdb-max-background-jobs = 12
    rocksdb-compaction-readahead-size = "2.0 MB"
    rocksdb-statistics-level = "except-detailed-timers"
    writer-batch-commit-count = 500
    writer-batch-commit-duration = "5ns"
    sync-wal-before-ack = true
    
    [metadata-store]
    bind-address = "0.0.0.0:5123"
    request-queue-length = 32
    
    [metadata-store.rocksdb]
    rocksdb-write-buffer-size = "256.0 MB"
    rocksdb-max-total-wal-size = "2.0 GB"
    rocksdb-disable-wal = true
    rocksdb-disable-statistics = false
    rocksdb-max-background-jobs = 12
    rocksdb-compaction-readahead-size = "2.0 MB"
    rocksdb-statistics-level = "except-detailed-timers"
    ```

---
+++ crates/serde-util/src/byte_count.rs
@@ -0,0 +1,383 @@
+// Copyright (c) 2024 -  Restate Software, Inc., Restate GmbH.
+// All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+use std::fmt::{self, Display};
+use std::num::{NonZeroU64, NonZeroUsize};
+use std::str::FromStr;
+
+use bytesize::ByteSize;
+use serde::de::Visitor;
+use serde::{de, de::Deserialize, Deserializer, Serialize, Serializer};
+use serde_with::{DeserializeAs, SerializeAs};
+
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Copy, Hash)]
+pub struct ByteCount<const CAN_BE_ZERO: bool = true>(u64);
+pub type NonZeroByteCount = ByteCount<false>;
+
+#[cfg(feature = "schema")]
+impl schemars::JsonSchema for ByteCount<true> {
+    fn schema_name() -> String {
+        "HumanBytes".to_owned()
+    }
+
+    fn schema_id() -> std::borrow::Cow<'static, str> {
+        std::borrow::Cow::Owned(std::concat!(std::module_path!(), "::", "HumanBytes").to_owned())
+    }
+
+    fn json_schema(gen: &mut schemars::gen::SchemaGenerator) -> schemars::schema::Schema {
+        let mut schema: schemars::schema::SchemaObject = gen.subschema_for::<String>().into();
+        schema.format = Some("human-bytes".to_owned());
+        let validation = schema.string();
+        validation.pattern = Some(r"^\d+(\.\d+)? ?[KMG]B$".to_string());
+        validation.min_length = Some(1);
+        let validation = schema.number();
+        validation.minimum = Some(1.0);
+        let metadata = schema.metadata();
+        metadata.title = Some("Human-readable bytes".to_owned());
+        metadata.description = Some("Human-readable bytes".to_owned());
+        schema.into()
+    }
+}
+#[cfg(feature = "schema")]
+impl schemars::JsonSchema for ByteCount<false> {
+    fn schema_name() -> String {
+        "NonZeroHumanBytes".to_owned()
+    }
+
+    fn schema_id() -> std::borrow::Cow<'static, str> {
+        std::borrow::Cow::Owned(
+            std::concat!(std::module_path!(), "::", "NonZeroHumanBytes").to_owned(),
+        )
+    }
+
+    fn json_schema(gen: &mut schemars::gen::SchemaGenerator) -> schemars::schema::Schema {
+        let mut schema: schemars::schema::SchemaObject = gen.subschema_for::<String>().into();
+        schema.format = Some("non-zero human-bytes".to_owned());
+        let validation = schema.string();
+        validation.pattern = Some(r"^\d+(\.\d+)? ?[KMG]B$".to_string());
+        validation.min_length = Some(1);
+        let validation = schema.number();
+        validation.minimum = Some(1.0);
+        let metadata = schema.metadata();
+        metadata.title = Some("Non-zero human-readable bytes".to_owned());
+        metadata.description = Some("Non-zero human-readable bytes".to_owned());
+        schema.into()
+    }
+}
+
+impl ByteCount<true> {
+    pub fn new(value: u64) -> Self {
+        Self(value)
+    }
+}
+
+impl ByteCount<false> {
+    pub fn new(value: NonZeroUsize) -> Self {
+        Self(usize::from(value) as u64)
+    }
+
+    pub fn as_non_zero_usize(&self) -> NonZeroUsize {
+        NonZeroUsize::new(self.0 as usize).expect("ByteCount is not zero")
+    }
+}
+
+impl<const CAN_BE_ZERO: bool> Display for ByteCount<CAN_BE_ZERO> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        ByteSize(self.0).fmt(f)
+    }
+}
+
+impl<const CAN_BE_ZERO: bool> ByteCount<CAN_BE_ZERO> {
+    pub fn as_u64(&self) -> u64 {
+        self.0
+    }
+
+    pub fn as_usize(&self) -> usize {
+        self.0 as usize
+    }
+}
+
+impl FromStr for ByteCount<true> {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s: ByteSize = s.parse()?;
+        Ok(ByteCount(s.0))
+    }
+}
+
+impl From<NonZeroUsize> for ByteCount<false> {
+    fn from(value: NonZeroUsize) -> Self {
+        let v: usize = value.into();
+        ByteCount(v as u64)
+    }
+}
+
+impl From<NonZeroU64> for ByteCount<false> {
+    fn from(value: NonZeroU64) -> Self {
+        ByteCount(value.into())
+    }
+}
+
+impl From<u64> for ByteCount<true> {
+    fn from(value: u64) -> Self {
+        ByteCount(value)
+    }
+}
+
+impl From<usize> for ByteCount<true> {
+    fn from(value: usize) -> Self {
+        ByteCount(value as u64)
+    }
+}
+
+impl<const CAN_BE_ZERO: bool> From<ByteCount<CAN_BE_ZERO>> for u64 {
+    fn from(value: ByteCount<CAN_BE_ZERO>) -> Self {
+        value.0
+    }
+}
+
+impl<const CAN_BE_ZERO: bool> Serialize for ByteCount<CAN_BE_ZERO> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        if serializer.is_human_readable() {
+            serializer.serialize_str(&ByteSize(self.0).to_string())
+        } else {
+            // raw u64 num of bytes
+            self.0.serialize(serializer)
+        }
+    }
+}
+
+impl<'de, const CAN_BE_ZERO: bool> Deserialize<'de> for ByteCount<CAN_BE_ZERO> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        if deserializer.is_human_readable() {
+            deserializer.deserialize_any::<ByteCountVisitor<CAN_BE_ZERO>>(ByteCountVisitor)
+        } else {
+            deserializer.deserialize_u64::<ByteCountVisitor<CAN_BE_ZERO>>(ByteCountVisitor)
+        }
+    }
+}
+
+struct ByteCountVisitor<const CAN_BE_ZERO: bool>;
+
+// Because we do what appears to be runtime-check of the const CAN_BE_ZERO but
+// it's actually compile-time, compiler will inline the comparison but we need
+// to make clippy happy.
+#[allow(clippy::absurd_extreme_comparisons)]
+impl<'de, const CAN_BE_ZERO: bool> Visitor<'de> for ByteCountVisitor<CAN_BE_ZERO> {
+    type Value = ByteCount<CAN_BE_ZERO>;
+    fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        formatter.write_str("an integer or string")
+    }
+
+    // TOML Integer(i64) - toml doesn't support u64
+    fn visit_i64<E: de::Error>(self, value: i64) -> Result<Self::Value, E> {
+        if let Ok(val) = u64::try_from(value) {
+            if val <= 0 && !CAN_BE_ZERO {
+                return Err(E::invalid_value(
+                    de::Unexpected::Signed(value),
+                    &"non-zero value",
+                ));
+            }
+            Ok(ByteCount(val))
+        } else {
+            Err(E::invalid_value(
+                de::Unexpected::Signed(value),
+                &"i64 overflow",
+            ))
+        }
+    }
+
+    // JSON Integer(u64)
+    fn visit_u64<E: de::Error>(self, value: u64) -> Result<Self::Value, E> {
+        if value <= 0 && !CAN_BE_ZERO {
+            return Err(E::invalid_value(
+                de::Unexpected::Unsigned(value),
+                &"non-zero value",
+            ));
+        }
+        Ok(ByteCount(value))
+    }
+
+    fn visit_str<E: de::Error>(self, value: &str) -> Result<Self::Value, E> {
+        let val: Result<ByteSize, _> = value.parse();
+
+        if let Ok(val) = val {
+            if val.0 <= 0 && !CAN_BE_ZERO {
+                return Err(E::invalid_value(
+                    de::Unexpected::Str(value),
+                    &"non-zero value",
+                ));
+            }
+            Ok(ByteCount(val.0))
+        } else {
+            Err(E::invalid_value(
+                de::Unexpected::Str(value),
+                &"a human-readable byte count string",
+            ))
+        }
+    }
+}
+
+impl SerializeAs<u64> for ByteCount<true> {
+    fn serialize_as<S>(source: &u64, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        Self(*source).serialize(serializer)
+    }
+}
+
+impl<'de> DeserializeAs<'de, u64> for ByteCount<true> {
+    fn deserialize_as<D>(deserializer: D) -> Result<u64, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        Ok(Self::deserialize(deserializer)?.0)
+    }
+}
+
+impl SerializeAs<usize> for ByteCount<true> {
+    fn serialize_as<S>(source: &usize, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        Self(*source as u64).serialize(serializer)
+    }
+}
+
+impl<'de> DeserializeAs<'de, usize> for ByteCount<true> {
+    fn deserialize_as<D>(deserializer: D) -> Result<usize, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        Ok(Self::deserialize(deserializer)?.0 as usize)
+    }
+}
+
+impl SerializeAs<NonZeroUsize> for ByteCount<false> {
+    fn serialize_as<S>(source: &NonZeroUsize, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        Self::from(*source).serialize(serializer)
+    }
+}
+
+impl<'de> DeserializeAs<'de, NonZeroUsize> for ByteCount<false> {
+    fn deserialize_as<D>(deserializer: D) -> Result<NonZeroUsize, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        Ok(Self::deserialize(deserializer)?.as_non_zero_usize())
+    }
+}
+
+impl SerializeAs<NonZeroU64> for ByteCount<false> {
+    fn serialize_as<S>(source: &NonZeroU64, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        Self::from(*source).serialize(serializer)
+    }
+}
+
+impl<'de> DeserializeAs<'de, NonZeroU64> for ByteCount<false> {
+    fn deserialize_as<D>(deserializer: D) -> Result<NonZeroU64, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let raw_value = Self::deserialize(deserializer)?.0;
+        Ok(NonZeroU64::new(raw_value).expect("ByteCount is not zero"))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use serde::Deserialize;
+
+    #[derive(Serialize, Deserialize)]
+    struct Config(ByteCount<true>);
+
+    #[derive(Serialize, Debug, Deserialize)]
+    struct Config2(Option<NonZeroByteCount>);
+
+    #[test]
+    fn deserialize_byte_count() {
+        #[track_caller]
+        fn check_str(s: &str) {
+            assert_eq!(
+                serde_json::from_str::<Config>(&format!("{:?}", s))
+                    .unwrap()
+                    .0,
+                s.parse().unwrap()
+            );
+        }
+
+        #[track_caller]
+        fn check(s: &str) {
+            assert_eq!(
+                serde_json::from_str::<Config>(s).unwrap().0,
+                s.parse().unwrap()
+            );
+        }
+
+        check_str("5 MB");
+        check_str("5MB");
+        check_str("5mb");
+        check_str("5m");
+        check_str("12.34 KB");
+        check_str("123");
+        check("123");
+        // zero is allowed
+        check("0");
+    }
+
+    #[test]
+    fn serialize_byte_count() {
+        #[track_caller]
+        fn check_ser(v: &Config) {
+            assert_eq!(
+                serde_json::to_string(v).unwrap(),
+                format!("{:?}", v.0.to_string())
+            );
+        }
+
+        check_ser(&Config(ByteCount(5_000_000)));
+    }
+
+    #[test]
+    fn deserialize_non_zero_opt_byte_count() {
+        assert_eq!(
+            serde_json::from_str::<Config2>("\"5 MB\"").unwrap().0,
+            Some(ByteCount(5_000_000)),
+        );
+        assert_eq!(
+            serde_json::from_str::<Config2>("\"5\"").unwrap().0,
+            Some(ByteCount(5)),
+        );
+
+        assert_eq!(serde_json::from_str::<Config2>("null").unwrap().0, None,);
+
+        assert_eq!(
+            serde_json::from_str::<Config2>("\"0\"")
+                .unwrap_err()
+                .to_string(),
+            "invalid value: string \"0\", expected non-zero value at line 1 column 3"
+        );
+    }
+}
--- crates/serde-util/src/lib.rs
+++ crates/serde-util/src/lib.rs
@@ -8,6 +8,7 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
+mod byte_count;
 mod header_map;
 #[cfg(feature = "proto")]
 mod proto;
@@ -15,6 +16,7 @@ mod proto;
 pub mod default;
 pub mod header_value;
 
+pub use byte_count::*;
 pub use header_map::SerdeableHeaderHashMap;
 pub use header_value::HeaderValueSerde;
 #[cfg(feature = "proto")]
