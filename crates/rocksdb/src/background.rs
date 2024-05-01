1c3ededcd355a1012a1997a42d309155df531cf1
Ahmed Farghal <me@asoli.dev>
2024-05-01 18:01:33 +0100

    stat improvements for background io

--- crates/rocksdb/src/background.rs
+++ crates/rocksdb/src/background.rs
@@ -11,12 +11,13 @@
 use std::time::Instant;
 
 use derive_builder::Builder;
-use metrics::histogram;
+use metrics::{gauge, histogram};
+use tokio::sync::oneshot;
 
 use crate::metric_definitions::{
     STORAGE_BG_TASK_RUN_DURATION, STORAGE_BG_TASK_TOTAL_DURATION, STORAGE_BG_TASK_WAIT_DURATION,
 };
-use crate::{DbName, Owner, Priority};
+use crate::{DbName, Owner, Priority, STORAGE_BG_TASK_IN_FLIGHT};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, strum_macros::IntoStaticStr)]
 #[strum(serialize_all = "kebab-case")]
@@ -43,7 +44,7 @@ pub struct ReadyStorageTask<OP> {
     kind: StorageTaskKind,
     #[builder(setter(skip))]
     #[builder(default = "Instant::now()")]
-    enqueue_at: Instant,
+    created_at: Instant,
 }
 
 impl<OP, R> ReadyStorageTask<OP>
@@ -51,7 +52,36 @@ where
     OP: FnOnce() -> R + Send + 'static,
     R: Send + 'static,
 {
-    pub fn run(self) -> R {
+    pub fn into_runner(self) -> impl FnOnce() -> R + Send + 'static {
+        let kind: &'static str = self.kind.into();
+        let priority: &'static str = self.priority.into();
+        gauge!(STORAGE_BG_TASK_IN_FLIGHT,
+         "db" => self.db_name.to_string(),
+         "priority" => priority,
+         "kind" => kind,
+        )
+        .increment(1);
+
+        move || self.run()
+    }
+
+    pub fn into_async_runner(self, tx: oneshot::Sender<R>) -> impl FnOnce() + Send + 'static {
+        let kind: &'static str = self.kind.into();
+        let priority: &'static str = self.priority.into();
+        gauge!(STORAGE_BG_TASK_IN_FLIGHT,
+         "db" => self.db_name.to_string(),
+         "priority" => priority,
+         "kind" => kind,
+        )
+        .increment(1);
+
+        move || {
+            let result = self.run();
+            let _ = tx.send(result);
+        }
+    }
+
+    fn run(self) -> R {
         let start = Instant::now();
         let kind: &'static str = self.kind.into();
         let owner: &'static str = self.owner.into();
@@ -63,7 +93,7 @@ where
             "owner" => owner,
             "priority" => priority,
         )
-        .record(self.enqueue_at.elapsed());
+        .record(self.created_at.elapsed());
         let res = (self.op)();
         histogram!(STORAGE_BG_TASK_RUN_DURATION,
             "kind" => kind,
@@ -78,7 +108,13 @@ where
             "owner" => owner,
             "priority" => priority,
         )
-        .record(self.enqueue_at.elapsed());
+        .record(self.created_at.elapsed());
+        gauge!(STORAGE_BG_TASK_IN_FLIGHT,
+         "db" => self.db_name.to_string(),
+         "priority" => priority,
+         "kind" => kind,
+        )
+        .decrement(1);
         res
     }
 }
