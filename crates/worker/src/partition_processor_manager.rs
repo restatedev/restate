baa447c3c459aa40e6e1ffcaadf3fe0d0dd25d0a
Ahmed Farghal <me@asoli.dev>
2024-05-14 13:02:37 +0100

    Attach to cluster controller through Networking
    This PR includes:
    - Attachment of PPM to cluster controller now use Networking
    - PPM observes partition processor status through a buffered watch mechanism
    - PPM can now send control messages (unused at this PR) to processor for future use.
    - PPM collects state information from running processors. This will be used in a follow PR to response to controller requests about partitions.
    
    
    Cluster controller grpc service is kept for external tooling integration (CLI, etc.)

--- crates/worker/src/partition_processor_manager.rs
+++ crates/worker/src/partition_processor_manager.rs
@@ -8,15 +8,27 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
-use crate::partition::storage::invoker::InvokerStorageReader;
-use crate::PartitionProcessor;
+use std::collections::BTreeMap;
+use std::ops::RangeInclusive;
+use std::time::Duration;
+
 use anyhow::Context;
+use restate_network::rpc_router::{RpcError, RpcRouter};
+use tokio::sync::{mpsc, watch};
+use tracing::{debug, info, warn};
+
 use restate_bifrost::Bifrost;
-use restate_core::worker_api::{ProcessorsManagerCommand, ProcessorsManagerHandle};
+use restate_core::network::MessageRouterBuilder;
+use restate_core::worker_api::{
+    PartitionProcessorStatus, ProcessorsManagerCommand, ProcessorsManagerHandle,
+};
 use restate_core::{cancellation_watcher, task_center, Metadata, ShutdownError, TaskId, TaskKind};
 use restate_invoker_impl::InvokerHandle;
 use restate_metadata_store::{MetadataStoreClient, ReadModifyWriteError};
 use restate_network::Networking;
+use restate_node_protocol::cluster_controller::AttachRequest;
+use restate_node_protocol::cluster_controller::{Action, AttachResponse, RunMode};
+use restate_node_protocol::MessageEnvelope;
 use restate_partition_store::{OpenMode, PartitionStore, PartitionStoreManager};
 use restate_types::arc_util::ArcSwapExt;
 use restate_types::config::{UpdateableConfiguration, WorkerOptions};
@@ -24,50 +36,75 @@ use restate_types::epoch::EpochMetadata;
 use restate_types::identifiers::{LeaderEpoch, PartitionId, PartitionKey};
 use restate_types::logs::{LogId, Payload};
 use restate_types::metadata_store::keys::partition_processor_epoch_key;
-use restate_types::{GenerationalNodeId, Version};
+use restate_types::time::MillisSinceEpoch;
+use restate_types::GenerationalNodeId;
 use restate_wal_protocol::control::AnnounceLeader;
 use restate_wal_protocol::{Command as WalCommand, Destination, Envelope, Header, Source};
-use std::collections::HashMap;
-use std::ops::RangeInclusive;
-use tokio::sync::mpsc;
-use tracing::{debug, info};
+
+use crate::partition::storage::invoker::InvokerStorageReader;
+use crate::partition::PartitionProcessorControlCommand;
+use crate::PartitionProcessor;
 
 pub struct PartitionProcessorManager {
     updateable_config: UpdateableConfiguration,
-    running_partition_processors: HashMap<PartitionId, TaskId>,
+    running_partition_processors: BTreeMap<PartitionId, State>,
 
     metadata: Metadata,
     metadata_store_client: MetadataStoreClient,
     partition_store_manager: PartitionStoreManager,
+    attach_router: RpcRouter<AttachRequest>,
     networking: Networking,
     bifrost: Bifrost,
     invoker_handle: InvokerHandle<InvokerStorageReader<PartitionStore>>,
     rx: mpsc::Receiver<ProcessorsManagerCommand>,
     tx: mpsc::Sender<ProcessorsManagerCommand>,
+    latest_attach_response: Option<(GenerationalNodeId, AttachResponse)>,
+}
+
+#[derive(Debug, thiserror::Error)]
+enum AttachError {
+    #[error("No cluster controller found in nodes configuration")]
+    NoClusterController,
+    #[error(transparent)]
+    ShutdownError(#[from] ShutdownError),
+}
+
+struct State {
+    _created_at: MillisSinceEpoch,
+    _key_range: RangeInclusive<PartitionKey>,
+    _control_tx: mpsc::Sender<PartitionProcessorControlCommand>,
+    _watch_rx: watch::Receiver<PartitionProcessorStatus>,
+    _task_id: TaskId,
 }
 
 impl PartitionProcessorManager {
+    #[allow(clippy::too_many_arguments)]
     pub fn new(
         updateable_config: UpdateableConfiguration,
         metadata: Metadata,
         metadata_store_client: MetadataStoreClient,
         partition_store_manager: PartitionStoreManager,
+        router_builder: &mut MessageRouterBuilder,
         networking: Networking,
         bifrost: Bifrost,
         invoker_handle: InvokerHandle<InvokerStorageReader<PartitionStore>>,
     ) -> Self {
+        let attach_router = RpcRouter::new(networking.clone(), router_builder);
+
         let (tx, rx) = mpsc::channel(updateable_config.load().worker.internal_queue_length());
         Self {
             updateable_config,
-            running_partition_processors: HashMap::default(),
+            running_partition_processors: BTreeMap::default(),
             metadata,
             metadata_store_client,
             partition_store_manager,
             networking,
             bifrost,
             invoker_handle,
+            attach_router,
             rx,
             tx,
+            latest_attach_response: None,
         }
     }
 
@@ -75,27 +112,66 @@ impl PartitionProcessorManager {
         ProcessorsManagerHandle::new(self.tx.clone())
     }
 
+    async fn attach(&mut self) -> Result<MessageEnvelope<AttachResponse>, AttachError> {
+        loop {
+            // We try to get the admin node on every retry since it might change between retries.
+            let admin_node = self
+                .metadata
+                .nodes_config()
+                .get_admin_node()
+                .ok_or(AttachError::NoClusterController)?
+                .current_generation;
+
+            info!(
+                "Attempting to attach to cluster controller '{}'",
+                admin_node
+            );
+            if admin_node == self.metadata.my_node_id() {
+                // If this node is running the cluster controller, we need to wait a little to give cluster
+                // controller time to start up. This is only done to reduce the chances of observing
+                // connection errors in log. Such logs are benign since we retry, but it's still not nice
+                // to print, specially in a single-node setup.
+                info!( "This node is the cluster controller, giving cluster controller service 500ms to start");
+                tokio::time::sleep(Duration::from_millis(500)).await;
+            }
+
+            match self
+                .attach_router
+                .call(admin_node.into(), &AttachRequest::default())
+                .await
+            {
+                Ok(response) => return Ok(response),
+                Err(RpcError::Shutdown(e)) => return Err(AttachError::ShutdownError(e)),
+                Err(e) => {
+                    warn!(
+                        "Failed to send attach message to cluster controller: {}, retrying....",
+                        e
+                    );
+                    tokio::time::sleep(Duration::from_secs(1)).await;
+                }
+            }
+        }
+    }
+
     pub async fn run(mut self) -> anyhow::Result<()> {
-        self.attach_worker().await?;
-
-        // simulating a plan after initial attachement
-        let partition_table = self.metadata.wait_for_partition_table(Version::MIN).await?;
-        let plan = PartitionProcessorPlan::new(
-            partition_table.version(),
-            partition_table
-                .partitioner()
-                .map(|(partition_id, _)| (partition_id, Action::Start(Role::Leader)))
-                .collect(),
-        );
-        self.apply_plan(plan).await?;
-
         let shutdown = cancellation_watcher();
         tokio::pin!(shutdown);
+
+        // Initial attach
+        let response = tokio::time::timeout(Duration::from_secs(5), self.attach())
+            .await
+            .context("Timeout waiting to attach to a cluster controller")??;
+
+        let (from, msg) = response.split();
+        // We ignore errors due to shutdown
+        let _ = self.apply_plan(&msg.actions);
+        self.latest_attach_response = Some((from, msg));
+        info!("Plan applied from attaching to controller {}", from);
 
         loop {
             tokio::select! {
                 Some(command) = self.rx.recv() => {
-                    self.handle_command(command).await;
+                    self.on_command(command);
                     debug!("PartitionProcessorManager shutting down");
                 }
               _ = &mut shutdown => {
@@ -105,7 +181,7 @@ impl PartitionProcessorManager {
         }
     }
 
-    async fn handle_command(&mut self, command: ProcessorsManagerCommand) {
+    fn on_command(&mut self, command: ProcessorsManagerCommand) {
         use ProcessorsManagerCommand::*;
         match command {
             GetLivePartitions(sender) => {
@@ -115,69 +191,71 @@ impl PartitionProcessorManager {
         }
     }
 
-    async fn attach_worker(&mut self) -> anyhow::Result<()> {
-        let admin_address = self
-            .metadata
-            .nodes_config()
-            .get_admin_node()
-            .expect("at least one admin node")
-            .address
-            .clone();
-
-        info!("Worker attaching to admin at '{admin_address}'");
-        // todo: use Networking to attach to a cluster admin node.
-        Ok(())
-    }
-
-    #[allow(clippy::map_entry)]
-    pub async fn apply_plan(&mut self, plan: PartitionProcessorPlan) -> anyhow::Result<()> {
+    pub fn apply_plan(&mut self, actions: &[Action]) -> Result<(), ShutdownError> {
         let config = self.updateable_config.pinned();
         let options = &config.worker;
-        let partition_table = self
-            .metadata
-            .wait_for_partition_table(plan.min_required_partition_table_version)
-            .await?;
 
-        for (partition_id, action) in plan.actions {
+        for action in actions {
             match action {
-                Action::Start(role) => {
+                Action::RunPartition(action) => {
+                    #[allow(clippy::map_entry)]
                     if !self
                         .running_partition_processors
-                        .contains_key(&partition_id)
+                        .contains_key(&action.partition_id)
                     {
-                        let partition_range = partition_table
-                            .partition_range(partition_id)
-                            .expect("partition_range to be known");
-                        let task_id = self.spawn_partition_processor(
+                        let (control_tx, control_rx) = mpsc::channel(2);
+                        let status = PartitionProcessorStatus::new(action.mode);
+                        let (watch_tx, watch_rx) = watch::channel(status.clone());
+
+                        let _task_id = self.spawn_partition_processor(
                             options,
-                            partition_id,
-                            partition_range,
-                            role,
+                            action.partition_id,
+                            action.key_range_inclusive.clone().into(),
+                            status,
+                            control_rx,
+                            watch_tx,
                         )?;
+                        let state = State {
+                            _created_at: MillisSinceEpoch::now(),
+                            _key_range: action.key_range_inclusive.clone().into(),
+                            _task_id,
+                            _control_tx: control_tx,
+                            _watch_rx: watch_rx,
+                        };
                         self.running_partition_processors
-                            .insert(partition_id, task_id);
+                            .insert(action.partition_id, state);
                     } else {
                         debug!(
                             "Partition processor for partition id '{}' is already running.",
-                            partition_id
+                            action.partition_id
                         );
                     }
                 }
             }
         }
-
         Ok(())
     }
 
     fn spawn_partition_processor(
-        &mut self,
+        &self,
         options: &WorkerOptions,
         partition_id: PartitionId,
-        partition_range: RangeInclusive<PartitionKey>,
-        role: Role,
+        key_range: RangeInclusive<PartitionKey>,
+        status: PartitionProcessorStatus,
+        control_rx: mpsc::Receiver<PartitionProcessorControlCommand>,
+        watch_tx: watch::Sender<PartitionProcessorStatus>,
     ) -> Result<TaskId, ShutdownError> {
-        let processor =
-            self.create_partition_processor(options, partition_id, partition_range.clone());
+        let planned_mode = status.planned_mode;
+        let processor = PartitionProcessor::new(
+            partition_id,
+            key_range.clone(),
+            status,
+            options.num_timers_in_memory_limit(),
+            options.internal_queue_length(),
+            control_rx,
+            watch_tx,
+            self.invoker_handle.clone(),
+        );
         let networking = self.networking.clone();
         let mut bifrost = self.bifrost.clone();
         let metadata_store_client = self.metadata_store_client.clone();
@@ -194,18 +272,18 @@ impl PartitionProcessorManager {
                     let partition_store = storage_manager
                         .open_partition_store(
                             partition_id,
-                            partition_range.clone(),
+                            key_range.clone(),
                             OpenMode::CreateIfMissing,
                             &options.storage.rocksdb,
                         )
                         .await?;
 
-                    if role == Role::Leader {
+                    if planned_mode == RunMode::Leader {
                         Self::claim_leadership(
                             &mut bifrost,
                             metadata_store_client,
                             partition_id,
-                            partition_range,
+                            key_range,
                             node_id,
                         )
                         .await?;
@@ -214,21 +292,6 @@ impl PartitionProcessorManager {
                     processor.run(networking, bifrost, partition_store).await
                 }
             },
-        )
-    }
-
-    fn create_partition_processor(
-        &self,
-        options: &WorkerOptions,
-        partition_id: PartitionId,
-        partition_key_range: RangeInclusive<PartitionKey>,
-    ) -> PartitionProcessor {
-        PartitionProcessor::new(
-            partition_id,
-            partition_key_range,
-            options.num_timers_in_memory_limit(),
-            options.internal_queue_length(),
-            self.invoker_handle.clone(),
         )
     }
 
@@ -302,34 +365,4 @@ impl PartitionProcessorManager {
 
         Ok(())
     }
-}
-
-#[derive(Debug)]
-pub struct PartitionProcessorPlan {
-    min_required_partition_table_version: Version,
-    actions: HashMap<PartitionId, Action>,
-}
-
-impl PartitionProcessorPlan {
-    pub fn new(
-        min_required_partition_table_version: Version,
-        actions: HashMap<PartitionId, Action>,
-    ) -> Self {
-        Self {
-            min_required_partition_table_version,
-            actions,
-        }
-    }
-}
-
-#[derive(Debug)]
-pub enum Action {
-    Start(Role),
-}
-
-#[derive(Debug, PartialEq)]
-pub enum Role {
-    Leader,
-    #[allow(dead_code)]
-    Follower,
 }
