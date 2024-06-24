3339e8b2d17d1c22d074a5133716d816b62bd530
Jack Kleeman <jackkleeman@gmail.com>
2024-06-24 09:34:29 +0100

    Clippy

--- cli/src/commands/services/status/detailed_status.rs
+++ cli/src/commands/services/status/detailed_status.rs
@@ -11,7 +11,6 @@
 use anyhow::Result;
 use indicatif::ProgressBar;
 
-use restate_admin_rest_model::services::ServiceType;
 use restate_cli_util::c_title;
 
 use super::{render_locked_keys, render_services_status, Status};
