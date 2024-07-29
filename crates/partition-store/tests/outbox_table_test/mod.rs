25bf4af92846ba299ed715e9569d094b606f9a29
Pavel Tcholakov <pavel@restate.dev>
2024-07-29 23:10:38 +0200

    Update tests for outbox
    - factor out discrete stages
    - add tests for finding outbox head

--- crates/partition-store/tests/outbox_table_test/mod.rs
+++ crates/partition-store/tests/outbox_table_test/mod.rs
@@ -11,7 +11,6 @@
 use restate_partition_store::PartitionStore;
 use restate_storage_api::outbox_table::{OutboxMessage, OutboxTable};
 use restate_storage_api::Transaction;
-use restate_test_util::let_assert;
 use restate_types::identifiers::PartitionId;
 
 use crate::mock_random_service_invocation;
@@ -20,57 +19,50 @@ fn mock_outbox_message() -> OutboxMessage {
     OutboxMessage::ServiceInvocation(mock_random_service_invocation())
 }
 
-pub(crate) async fn populate_data<T: OutboxTable>(txn: &mut T) {
-    let partition1337 = PartitionId::from(1337);
-    txn.add_message(partition1337, 0, mock_outbox_message())
-        .await;
-    txn.add_message(partition1337, 1, mock_outbox_message())
-        .await;
-    txn.add_message(partition1337, 2, mock_outbox_message())
-        .await;
-    txn.add_message(partition1337, 3, mock_outbox_message())
-        .await;
-
-    // add a successor and a predecessor partitions
+pub(crate) async fn populate_data<T: OutboxTable>(txn: &mut T, initial: Vec<u64>) {
     txn.add_message(PartitionId::from(1336), 0, mock_outbox_message())
         .await;
+
+    let partition1337 = PartitionId::from(1337);
+    for seq_no in initial {
+        txn.add_message(partition1337, seq_no, mock_outbox_message())
+            .await;
+    }
+
     txn.add_message(PartitionId::from(1338), 0, mock_outbox_message())
         .await;
 }
 
-pub(crate) async fn verify_outbox_head_seq_number<T: OutboxTable>(txn: &mut T) {
+pub(crate) async fn verify_outbox_head_seq_number<T: OutboxTable>(
+    txn: &mut T,
+    expected: Option<u64>,
+) {
     let head = txn
         .get_outbox_head_seq_number(PartitionId::from(1337))
         .await
         .expect("should not fail");
-    let_assert!(Some(0) = head);
+    assert_eq!(expected, head);
 }
 
-pub(crate) async fn consume_message_and_truncate<T: OutboxTable>(txn: &mut T) {
+pub(crate) async fn consume_messages_and_truncate_range<T: OutboxTable>(
+    txn: &mut T,
+    expected: Vec<u64>,
+) {
     let partition1337 = PartitionId::from(1337);
-    let mut count = 0;
-    let mut max_seq_id = 0;
-    while let Ok(Some((seq, _))) = txn.get_next_outbox_message(partition1337, count).await {
-        count += 1;
-        max_seq_id = seq;
+
+    for seq_no in expected.clone() {
+        let msg = txn
+            .get_next_outbox_message(partition1337, seq_no)
+            .await
+            .expect("should not fail");
+        assert!(msg.is_some(), "message should be present in outbox");
     }
-    assert_eq!(count, 4);
 
-    // truncate range
-    txn.truncate_outbox(partition1337, 0..=max_seq_id - 1).await;
-
-    let result = txn
-        .get_next_outbox_message(partition1337, 0)
-        .await
-        .expect("should not fail");
-    assert!(
-        result.is_some(),
-        "one message should remain in outbox after the first truncation"
-    );
-
-    // truncate single key
-    txn.truncate_outbox(partition1337, max_seq_id..=max_seq_id)
-        .await;
+    txn.truncate_outbox(
+        partition1337,
+        *expected.first().unwrap()..=*expected.last().unwrap(),
+    )
+    .await;
 }
 
 pub(crate) async fn verify_outbox_is_empty_after_truncation<T: OutboxTable>(txn: &mut T) {
@@ -83,9 +75,7 @@ pub(crate) async fn verify_outbox_is_empty_after_truncation<T: OutboxTable>(txn:
     assert_eq!(result, None);
 }
 
-pub(crate) async fn verify_other_partitions_are_unchanged_after_truncation<T: OutboxTable>(
-    txn: &mut T,
-) {
+pub(crate) async fn verify_neighbor_partitions_unchanged<T: OutboxTable>(txn: &mut T) {
     let partition1336 = PartitionId::from(1336);
     let predecessor_partition = txn
         .get_next_outbox_message(partition1336, 0)
@@ -104,16 +94,23 @@ pub(crate) async fn verify_other_partitions_are_unchanged_after_truncation<T: Ou
 
 pub(crate) async fn run_tests(mut rocksdb: PartitionStore) {
     let mut txn = rocksdb.transaction();
+    verify_outbox_head_seq_number(&mut txn, None).await;
+    populate_data(&mut txn, vec![0, 1, 2, 3]).await;
+    txn.commit().await.expect("should not fail");
 
-    populate_data(&mut txn).await;
+    let mut txn = rocksdb.transaction();
+    verify_outbox_head_seq_number(&mut txn, Some(0)).await;
+    consume_messages_and_truncate_range(&mut txn, vec![0, 1, 2]).await;
+    verify_neighbor_partitions_unchanged(&mut txn).await;
+    txn.commit().await.expect("should not fail");
 
-    verify_outbox_head_seq_number(&mut txn).await;
-
-    consume_message_and_truncate(&mut txn).await;
-
+    let mut txn = rocksdb.transaction();
+    verify_outbox_head_seq_number(&mut txn, Some(3)).await;
+    consume_messages_and_truncate_range(&mut txn, vec![3]).await;
+    verify_neighbor_partitions_unchanged(&mut txn).await;
     txn.commit().await.expect("should not fail");
 
     let mut txn = rocksdb.transaction();
     verify_outbox_is_empty_after_truncation(&mut txn).await;
-    verify_other_partitions_are_unchanged_after_truncation(&mut txn).await;
+    verify_neighbor_partitions_unchanged(&mut txn).await;
 }
