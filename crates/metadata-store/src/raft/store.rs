9ccdf215c0119f26f31e7587c507a66c1c126edd
Till Rohrmann <till@restate.dev>
2024-08-06 10:07:39 +0200

    Classify ProposalDropped as Unavailable Tonic status
    The raft metadata store does not accept new proposals if there is
    no known leader. In this situation, request failed with an internal
    ProposalDropped error. This commit changes the behavior so that a
    ProposalDropped error will be translated into an unavailable Tonic
    status. That way, the request will get automatically retried.

--- crates/metadata-store/src/raft/store.rs
+++ crates/metadata-store/src/raft/store.rs
@@ -112,7 +112,7 @@ impl RaftMetadataStore {
                         .map_err(Into::into)
                         .and_then(|request| self.raw_node
                             .propose(vec![], request)
-                            .map_err(|err| RequestError::Internal(err.into()))) {
+                            .map_err(RequestError::from)) {
                         info!("Failed processing request: {err}");
                         callback.fail(err);
                         continue;
@@ -527,4 +527,13 @@ enum RequestKind {
         key: ByteString,
         precondition: Precondition,
     },
+}
+
+impl From<raft::Error> for RequestError {
+    fn from(value: raft::Error) -> Self {
+        match value {
+            err @ raft::Error::ProposalDropped => RequestError::Unavailable(err.into()),
+            err => RequestError::Internal(err.into()),
+        }
+    }
 }
