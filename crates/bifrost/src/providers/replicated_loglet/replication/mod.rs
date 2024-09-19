d67cc18a9a4ae7afd8cac2305b0db95663ca8ae8
Ahmed Farghal <me@asoli.dev>
2024-09-19 12:44:31 +0100

    [Bifrost] Initial NodeSetChecker implementation
    This introduces ReplicationChecker utility that performs write and f-majority quorum checks. Note that the current implementation is not LocationScope aware and will only consider node-level scope.
    The utility is designed to allow attaching any attribute type to nodes in the nodeset and run the check against the set of nodes that match a predicate against those attributes.

--- crates/bifrost/src/providers/replicated_loglet/replication/mod.rs
+++ crates/bifrost/src/providers/replicated_loglet/replication/mod.rs
@@ -7,11 +7,6 @@
 // As of the Change Date specified in that file, in accordance with
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
+mod checker;
 
-pub(crate) mod metric_definitions;
-mod provider;
-pub mod replication;
-#[cfg(any(test, feature = "test-util"))]
-pub mod test_util;
-
-pub use provider::Factory;
+pub use checker::*;
