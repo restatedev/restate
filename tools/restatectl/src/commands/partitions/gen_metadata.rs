63db1a13214c956743c71b850fb3fa5f39c0648a
Ahmed Farghal <me@asoli.dev>
2024-10-10 14:59:13 +0100

    restatectl command to generate partition table
    Generates partition table metadata (version 2 by default)
    
    ```console
    cargo run --bin restatectl -- partitions generate-metadata -n 5 --pretty
    ```
    
    ```
    {
      "version": 2,
      "num_partitions": 5,
      "partitions": [
        [
          0,
          {
            "key_range": {
              "start": 0,
              "end": 3689348814741910323
            }
          }
        ],
        [
          1,
          {
            "key_range": {
              "start": 3689348814741910324,
              "end": 7378697629483820646
            }
          }
        ],
        [
          2,
          {
            "key_range": {
              "start": 7378697629483820647,
              "end": 11068046444225730969
            }
          }
        ],
        [
          3,
          {
            "key_range": {
              "start": 11068046444225730970,
              "end": 14757395258967641292
            }
          }
        ],
        [
          4,
          {
            "key_range": {
              "start": 14757395258967641293,
              "end": 18446744073709551615
            }
          }
        ]
      ]
    }
    ```

---
+++ tools/restatectl/src/commands/partitions/gen_metadata.rs
@@ -0,0 +1,45 @@
+// Copyright (c) 2024 -  Restate Software, Inc., Restate GmbH.
+// All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+use std::num::NonZeroU32;
+
+use cling::prelude::*;
+
+use restate_types::partition_table::PartitionTable;
+use restate_types::Version;
+
+#[derive(Run, Parser, Collect, Clone, Debug)]
+#[clap()]
+#[cling(run = "generate_partition_table")]
+pub struct GeneratePartitionTableOpts {
+    #[clap(long, default_value = "2")]
+    version: NonZeroU32,
+    /// The number of logs
+    #[clap(long, short)]
+    num_partition: u16,
+    /// Pretty json?
+    #[clap(long)]
+    pretty: bool,
+}
+
+async fn generate_partition_table(opts: &GeneratePartitionTableOpts) -> anyhow::Result<()> {
+    let table = PartitionTable::with_equally_sized_partitions(
+        Version::from(u32::from(opts.version)),
+        opts.num_partition,
+    );
+
+    let output = if opts.pretty {
+        serde_json::to_string_pretty(&table)?
+    } else {
+        serde_json::to_string(&table)?
+    };
+    println!("{}", output);
+    Ok(())
+}
