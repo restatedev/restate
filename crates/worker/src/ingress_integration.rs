bbc2bdc14e3b4687b7cfa22bc0804c0884a4ef9f
slinkydeveloper <francescoguard@gmail.com>
2024-11-01 10:31:33 +0100

    Adapt the business logic of the RpcRequestDispatcher to the various changes

--- crates/worker/src/ingress_integration.rs
+++ crates/worker/src/ingress_integration.rs
@@ -9,9 +9,11 @@
 // by the Apache License, Version 2.0.
 
 use anyhow::Context;
-use restate_core::network::partition_processor_rpc_client::PartitionProcessorRpcClient;
 use restate_core::network::partition_processor_rpc_client::{
     AttachInvocationResponse, GetInvocationOutputResponse,
+};
+use restate_core::network::partition_processor_rpc_client::{
+    PartitionProcessorRpcClient, PartitionProcessorRpcClientError,
 };
 use restate_core::network::TransportConnect;
 use restate_ingress_http::{RequestDispatcher, RequestDispatcherError};
@@ -19,12 +21,12 @@ use restate_types::identifiers::PartitionProcessorRpcRequestId;
 use restate_types::invocation::{InvocationQuery, InvocationRequest, InvocationResponse};
 use restate_types::net::partition_processor::{InvocationOutput, SubmittedInvocationNotification};
 use restate_types::retries::RetryPolicy;
+use std::future::Future;
 use std::time::Duration;
 
 pub struct RpcRequestDispatcher<C> {
     partition_processor_rpc_client: PartitionProcessorRpcClient<C>,
     retry_policy: RetryPolicy,
-    rpc_timeout: Duration,
 }
 
 impl<T> Clone for RpcRequestDispatcher<T> {
@@ -32,7 +34,6 @@ impl<T> Clone for RpcRequestDispatcher<T> {
         RpcRequestDispatcher {
             partition_processor_rpc_client: self.partition_processor_rpc_client.clone(),
             retry_policy: self.retry_policy.clone(),
-            rpc_timeout: self.rpc_timeout,
         }
     }
 }
@@ -41,10 +42,26 @@ impl<C> RpcRequestDispatcher<C> {
     pub fn new(partition_processor_rpc_client: PartitionProcessorRpcClient<C>) -> Self {
         Self {
             partition_processor_rpc_client,
-            // Totally random chosen!!!
+            // TODO figure out how to tune this?
             retry_policy: RetryPolicy::fixed_delay(Duration::from_millis(50), None),
-            rpc_timeout: Duration::from_secs(60),
         }
+    }
+
+    async fn execute_rpc<Fn, Fut, T>(
+        &self,
+        is_idempotent: bool,
+        operation: Fn,
+    ) -> Result<T, RequestDispatcherError>
+    where
+        Fn: FnMut() -> Fut,
+        Fut: Future<Output = Result<T, PartitionProcessorRpcClientError>>,
+    {
+        Ok(self
+            .retry_policy
+            .clone()
+            .retry_if(operation, |e| is_idempotent || e.is_safe_to_retry())
+            .await
+            .context("error when trying to route the request internally")?)
     }
 }
 
@@ -57,39 +74,15 @@ where
         invocation_request: InvocationRequest,
     ) -> Result<SubmittedInvocationNotification, RequestDispatcherError> {
         let request_id = PartitionProcessorRpcRequestId::default();
-        if invocation_request.is_idempotent() {
-            // In this case we need to wait for the submit notification from the PP, this is safe to retry
-            self.retry_policy
-                .clone()
-                .retry(|| async {
-                    Ok(tokio::time::timeout(
-                        self.rpc_timeout,
-                        self.partition_processor_rpc_client
-                            .append_invocation_and_wait_submit_notification(
-                                request_id,
-                                invocation_request.clone(),
-                            ),
-                    )
-                    .await
-                    .context("timeout while trying to reach partition processor")?
-                    .context("error when trying to interact with partition processor")?)
-                })
-                .await
-        } else {
-            // In this case we just need to wait the invocation was appended
-            tokio::time::timeout(
-                self.rpc_timeout,
-                self.partition_processor_rpc_client
-                    .append_invocation(request_id, invocation_request),
-            )
-            .await
-            .context("timeout while trying to reach partition processor")?
-            .context("error when trying to interact with partition processor")?;
-            Ok(SubmittedInvocationNotification {
-                request_id,
-                is_new_invocation: true,
-            })
-        }
+        let is_idempotent = invocation_request.is_idempotent();
+        self.execute_rpc(is_idempotent, || {
+            self.partition_processor_rpc_client
+                .append_invocation_and_wait_submit_notification(
+                    request_id,
+                    invocation_request.clone(),
+                )
+        })
+        .await
     }
 
     async fn call(
@@ -97,34 +90,12 @@ where
         invocation_request: InvocationRequest,
     ) -> Result<InvocationOutput, RequestDispatcherError> {
         let request_id = PartitionProcessorRpcRequestId::default();
-        if invocation_request.is_idempotent() {
-            // For workflow or idempotent calls, it is safe to retry always
-            self.retry_policy
-                .clone()
-                .retry(|| async {
-                    Ok(tokio::time::timeout(
-                        self.rpc_timeout,
-                        self.partition_processor_rpc_client
-                            .append_invocation_and_wait_output(
-                                request_id,
-                                invocation_request.clone(),
-                            ),
-                    )
-                    .await
-                    .context("timeout while trying to reach partition processor")?
-                    .context("error when trying to interact with partition processor")?)
-                })
-                .await
-        } else {
-            Ok(tokio::time::timeout(
-                self.rpc_timeout,
-                self.partition_processor_rpc_client
-                    .append_invocation_and_wait_output(request_id, invocation_request),
-            )
-            .await
-            .context("timeout while trying to reach partition processor")?
-            .context("error when trying to interact with partition processor")?)
-        }
+        let is_idempotent = invocation_request.is_idempotent();
+        self.execute_rpc(is_idempotent, || {
+            self.partition_processor_rpc_client
+                .append_invocation_and_wait_output(request_id, invocation_request.clone())
+        })
+        .await
     }
 
     async fn attach_invocation(
@@ -132,35 +103,23 @@ where
         invocation_query: InvocationQuery,
     ) -> Result<AttachInvocationResponse, RequestDispatcherError> {
         let request_id = PartitionProcessorRpcRequestId::default();
-        // Attaching an invocation is idempotent and can be retried, with timeouts
-        self.retry_policy
-            .clone()
-            .retry(|| async {
-                Ok(tokio::time::timeout(
-                    self.rpc_timeout,
-                    self.partition_processor_rpc_client
-                        .attach_invocation(request_id, invocation_query.clone()),
-                )
-                .await
-                .context("timeout while trying to reach partition processor")?
-                .context("error when trying to interact with partition processor")?)
-            })
-            .await
+        self.execute_rpc(true, || {
+            self.partition_processor_rpc_client
+                .attach_invocation(request_id, invocation_query.clone())
+        })
+        .await
     }
 
     async fn get_invocation_output(
         &self,
         invocation_query: InvocationQuery,
     ) -> Result<GetInvocationOutputResponse, RequestDispatcherError> {
-        // No need to retry this
-        Ok(tokio::time::timeout(
-            self.rpc_timeout,
+        let request_id = PartitionProcessorRpcRequestId::default();
+        self.execute_rpc(true, || {
             self.partition_processor_rpc_client
-                .get_invocation_output(PartitionProcessorRpcRequestId::default(), invocation_query),
-        )
+                .get_invocation_output(request_id, invocation_query.clone())
+        })
         .await
-        .context("timeout while trying to reach partition processor")?
-        .context("error when trying to interact with partition processor")?)
     }
 
     async fn send_invocation_response(
@@ -168,16 +127,10 @@ where
         invocation_response: InvocationResponse,
     ) -> Result<(), RequestDispatcherError> {
         let request_id = PartitionProcessorRpcRequestId::default();
-        // Appending invocation response is an idempotent operation, it's always safe to try
-        self.retry_policy
-            .clone()
-            .retry(|| async {
-                Ok(self
-                    .partition_processor_rpc_client
-                    .append_invocation_response(request_id, invocation_response.clone())
-                    .await
-                    .context("error when trying to interact with partition processor")?)
-            })
-            .await
+        self.execute_rpc(true, || {
+            self.partition_processor_rpc_client
+                .append_invocation_response(request_id, invocation_response.clone())
+        })
+        .await
     }
 }
