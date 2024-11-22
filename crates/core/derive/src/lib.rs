976c909f4161194e23bd75c64ba690dced64700a
Ahmed Farghal <me@asoli.dev>
2024-11-22 18:46:55 +0000

    [TaskCenter] Stage 5 - test macro

---
+++ crates/core/derive/src/lib.rs
@@ -0,0 +1,39 @@
+// Copyright (c) 2023 - 2025 Restate Software, Inc., Restate GmbH.
+// All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+extern crate proc_macro;
+
+mod tc_test;
+
+use proc_macro::TokenStream;
+
+/// Run tests within task-center
+///
+///
+/// You can configure the underlying runtime(s) as you would do with tokio
+/// ```no_run
+/// #[restate_core::test(_args_of_tokio_test)]
+/// async fn test_name() {
+///    TaskCenter::current();
+/// }
+/// ```
+///
+/// A generalised example is
+/// ```no_run
+/// #[restate_core::test(start_paused = true)]`
+/// async fn test_name() {
+///    TaskCenter::current();
+/// }
+/// ```
+///
+#[proc_macro_attribute]
+pub fn test(args: TokenStream, item: TokenStream) -> TokenStream {
+    tc_test::test(args.into(), item.into(), true).into()
+}
