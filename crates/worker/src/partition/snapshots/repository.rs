693de9a5a49ec2b0003c4efb3d6e6593162efae5
Pavel Tcholakov <pavel@restate.dev>
2024-12-05 18:06:20 +0100

    Validate object store uploads

--- crates/worker/src/partition/snapshots/repository.rs
+++ crates/worker/src/partition/snapshots/repository.rs
@@ -648,9 +648,15 @@ mod tests {
         );
 
         let opts = SnapshotsOptions {
-            destination: Some(destination),
+            destination: Some(destination.clone()),
             ..SnapshotsOptions::default()
         };
+
+        eprintln!("Destination: {}", destination);
+
+        let destination = Url::parse(destination.as_str())?;
+        let path = destination.path().to_string();
+        let object_store = super::create_object_store_client(destination).await?;
 
         let repository = SnapshotRepository::create_if_configured(&opts)
             .await?
