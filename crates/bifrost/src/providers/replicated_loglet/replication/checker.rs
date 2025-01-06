fc0db40b3874d41d350abb2a5dea57927d8e71ff
Till Rohrmann <till@restate.dev>
2025-01-06 18:42:43 +0100

    Implement a simple mechanism to reconfigure an OmniPaxos cluster

--- crates/bifrost/src/providers/replicated_loglet/replication/checker.rs
+++ crates/bifrost/src/providers/replicated_loglet/replication/checker.rs
@@ -748,7 +748,7 @@ mod tests {
     use googletest::prelude::*;
 
     use restate_types::nodes_config::{
-        LogServerConfig, NodeConfig, NodesConfiguration, Role, StorageState,
+        LogServerConfig, MetadataServerConfig, NodeConfig, NodesConfiguration, Role, StorageState,
     };
     use restate_types::{GenerationalNodeId, PlainNodeId, Version};
 
@@ -765,6 +765,7 @@ mod tests {
             format!("unix:/tmp/my_socket-{id}").parse().unwrap(),
             Role::LogServer.into(),
             LogServerConfig { storage_state },
+            MetadataServerConfig::default(),
         )
     }
 
