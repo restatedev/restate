c87e41c49a21a6f36cbde22837f5985c0de744ab
Till Rohrmann <till@restate.dev>
2025-01-06 10:50:37 +0100

    Enable restatectl to take multiple metadata store addresses

--- tools/restatectl/src/commands/metadata/mod.rs
+++ tools/restatectl/src/commands/metadata/mod.rs
@@ -8,10 +8,10 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
+use anyhow::Context;
+use cling::prelude::*;
 use std::path::PathBuf;
 use std::str::FromStr;
-
-use cling::prelude::*;
 
 use restate_core::metadata_store::MetadataStoreClient;
 use restate_metadata_store::local::create_client;
@@ -36,14 +36,14 @@ pub enum Metadata {
 #[derive(Args, Clone, Debug)]
 #[clap()]
 pub struct MetadataCommonOpts {
-    /// Metadata store server address; for Etcd addresses use comma-separated list
+    /// Metadata store server addresses
     #[arg(
         short,
-        long = "address",
-        default_value = "http://127.0.0.1:5122",
-        env = "RESTATE_METADATA_ADDRESS"
+        long = "addresses",
+        default_values = &["http://127.0.0.1:5122"],
+        env = "RESTATE_METADATA_ADDRESSES"
     )]
-    address: String,
+    addresses: Vec<String>,
 
     /// Metadata store access mode
     #[arg(long, default_value_t)]
@@ -109,15 +109,16 @@ pub async fn create_metadata_store_client(
 ) -> anyhow::Result<MetadataStoreClient> {
     let client = match opts.remote_service_type {
         RemoteServiceType::Restate => restate_types::config::MetadataStoreClient::Embedded {
-            addresses: vec![AdvertisedAddress::from_str(opts.address.as_str())
-                .map_err(|e| anyhow::anyhow!("Failed to parse address: {}", e))?],
+            addresses: opts
+                .addresses
+                .iter()
+                .map(|address| {
+                    AdvertisedAddress::from_str(address).context("failed to parse address")
+                })
+                .collect::<Result<Vec<_>, _>>()?,
         },
         RemoteServiceType::Etcd => restate_types::config::MetadataStoreClient::Etcd {
-            addresses: opts
-                .address
-                .split(',')
-                .map(|s| s.to_string())
-                .collect::<Vec<String>>(),
+            addresses: opts.addresses.clone(),
         },
     };
 
