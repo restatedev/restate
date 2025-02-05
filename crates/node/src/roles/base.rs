641449ad26ff23984c99ecea835caca822356d64
Ahmed Farghal <me@asoli.dev>
2025-02-05 10:20:54 +0000

    Health in TaskCenter

--- crates/node/src/roles/base.rs
+++ crates/node/src/roles/base.rs
@@ -11,12 +11,12 @@
 use futures::StreamExt;
 
 use restate_core::{
-    cancellation_watcher,
-    network::{Incoming, MessageRouterBuilder, MessageStream, NetworkError},
+    network::{Incoming, MessageRouterBuilder, MessageStream},
     worker_api::ProcessorsManagerHandle,
     ShutdownError, TaskCenter, TaskKind,
 };
 use restate_types::net::node::{GetNodeState, NodeStateResponse};
+use restate_types::protobuf::common::NodeStatus;
 
 pub struct BaseRole {
     processor_manager_handle: Option<ProcessorsManagerHandle>,
@@ -28,7 +28,7 @@ impl BaseRole {
         router_builder: &mut MessageRouterBuilder,
         processor_manager_handle: Option<ProcessorsManagerHandle>,
     ) -> Self {
-        let incoming_node_state = router_builder.subscribe_to_stream(2);
+        let incoming_node_state = router_builder.subscribe_to_stream(128);
 
         Self {
             processor_manager_handle,
@@ -37,17 +37,10 @@ impl BaseRole {
     }
 
     pub fn start(self) -> Result<(), ShutdownError> {
-        TaskCenter::spawn_child(TaskKind::RoleRunner, "base-role-service", async {
-            let cancelled = cancellation_watcher();
-
-            tokio::select! {
-                result = self.run() => {
-                    result
-                }
-                _ = cancelled =>{
-                    Ok(())
-                }
-            }
+        TaskCenter::spawn_unmanaged(TaskKind::RoleRunner, "base-role-service", async {
+            let node_status = TaskCenter::with_current(|tc| tc.health().node_status());
+            node_status.update(NodeStatus::Alive);
+            self.run().await
         })?;
 
         Ok(())
@@ -55,34 +48,23 @@ impl BaseRole {
 
     async fn run(mut self) -> anyhow::Result<()> {
         while let Some(request) = self.incoming_node_state.next().await {
-            // handle request
-            self.handle_get_node_state(request).await?;
+            self.handle_get_node_state(request).await;
         }
 
         Ok(())
     }
 
-    async fn handle_get_node_state(
-        &self,
-        msg: Incoming<GetNodeState>,
-    ) -> Result<(), ShutdownError> {
+    async fn handle_get_node_state(&self, msg: Incoming<GetNodeState>) {
         let partition_state = if let Some(ref handle) = self.processor_manager_handle {
-            Some(handle.get_state().await?)
+            handle.get_state().await.ok()
         } else {
             None
         };
 
-        // only return error if Shutdown
-        if let Err(NetworkError::Shutdown(err)) = msg
+        let _ = msg
             .to_rpc_response(NodeStateResponse {
                 partition_processor_state: partition_state,
             })
-            .try_send()
-            .map_err(|err| err.source)
-        {
-            return Err(err);
-        }
-
-        Ok(())
+            .try_send();
     }
 }
