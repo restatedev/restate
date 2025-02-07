92949316ae1cf58476539f35333fba204a6e2ee5
Muhamad Awad <muhamad@restate.dev>
2025-02-07 09:24:32 +0100

    Fix configuration compatibility issues
    Summary:
    
    Making sure a single node server v1.2 can still work
    with a v1.1.6 config

--- crates/types/src/config/common.rs
+++ crates/types/src/config/common.rs
@@ -628,6 +628,7 @@ enum MetadataClientKindShadow {
     #[serde(alias = "embedded")]
     Native {
         address: Option<AdvertisedAddress>,
+        #[serde(default)]
         addresses: Vec<AdvertisedAddress>,
     },
     Etcd {
@@ -657,8 +658,14 @@ impl TryFrom<MetadataClientKindShadow> for MetadataClientKind {
 
                 Self::Native {
                     addresses: match address {
-                        Some(address) if addresses == vec![default_address] => vec![address],
-                        Some(_) => return Err("Conflicting configuration, embedded metadata-store-client cannot have both `address` and `addresses`"),
+                        Some(address)
+                            if addresses.is_empty() || addresses == vec![default_address] =>
+                        {
+                            vec![address]
+                        }
+                        Some(_) => {
+                            return Err("Conflicting configuration, embedded metadata-client cannot have both `address` and `addresses`");
+                        }
                         None => addresses,
                     },
                 }
