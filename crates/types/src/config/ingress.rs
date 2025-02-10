6ffa0b215f08682edecbefa98062bd62d9eb02b5
Pavel Tcholakov <pavel@restate.dev>
2025-02-10 11:59:34 +0200

    Document the http-ingress role as experimental
    This change also makes it impossible to use the role without the experimental flag that enables it.

--- crates/types/src/config/ingress.rs
+++ crates/types/src/config/ingress.rs
@@ -39,10 +39,11 @@ pub struct IngressOptions {
 
     /// # Experimental feature to run the ingress independent of the worker role
     ///
-    /// This feature is experimental and should be used with caution. It allows to run the ingress
-    /// role independent of the worker role. It requires that you configure the [`Role::Ingress`]
-    /// role for nodes explicitly. If you enable this feature, then you might not be able to roll
-    /// back to a previous version.
+    /// This feature is experimental and should be used with caution. It enables the HTTP ingress
+    /// to run role independently of the worker role. If you activate this feature, you will need
+    /// to specify the [`Role::HttpIngress`] role for nodes explicitly, and nodes that run
+    /// only [`Role::Worker`] will not accept HTTP ingress requests. If you enable this feature,
+    /// then you might not be able to roll back to a previous version.
     #[cfg_attr(feature = "schemars", schemars(skip))]
     pub experimental_feature_enable_separate_ingress_role: bool,
 
