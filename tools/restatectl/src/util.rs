717e2c3c7aa45242a443dccf1736a13bb5b9e431
Muhamad Awad <muhamad@restate.dev>
2025-02-10 12:44:03 +0100

    Minor restatectl improvements
    This fixes the `logs list` view and shows the configuration in a proper way
    ```
    Log Configuration (v3)
    └ Logs Provider: replicated
      ├ Log replication: {node: 2}
      └ Nodeset size: 0
     L-ID  FROM-LSN  KIND        LOGLET-ID  REPLICATION  SEQUENCER  NODESET
     0     2         Replicated  0_1        {node: 2}    N1:2       [N1, N2, N3]
     1     1         Replicated  1_1        {node: 2}    N3:1       [N1, N2, N3]
     2     2         Replicated  2_1        {node: 2}    N1:2       [N1, N2, N3]
     3     1         Replicated  3_1        {node: 2}    N3:1       [N1, N2, N3]
    ```
    instead of
    ```
    Log Configuration (v3)
    Default Provider Config: Replicated(ReplicatedLogletConfig { replication_property: {node: 2}, target_nodeset_size: NodeSetSize(0) })
     L-ID  FROM-LSN  KIND        LOGLET-ID  REPLICATION  SEQUENCER  NODESET
     0     2         Replicated  0_1        {node: 2}    N1:2       [N1, N2, N3]
     1     1         Replicated  1_1        {node: 2}    N3:1       [N1, N2, N3]
     2     2         Replicated  2_1        {node: 2}    N1:2       [N1, N2, N3]
     3     1         Replicated  3_1        {node: 2}    N3:1       [N1, N2, N3]
    ```

--- tools/restatectl/src/util.rs
+++ tools/restatectl/src/util.rs
@@ -8,13 +8,64 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
+use std::fmt::{self, Display};
+
 use tonic::transport::Channel;
 
 use restate_cli_util::CliContext;
 use restate_core::network::net_util::create_tonic_channel;
-use restate_types::net::AdvertisedAddress;
+use restate_types::{logs::metadata::ProviderConfiguration, net::AdvertisedAddress};
 
 pub fn grpc_channel(address: AdvertisedAddress) -> Channel {
     let ctx = CliContext::get();
     create_tonic_channel(address, &ctx.network)
+}
+
+pub fn write_default_provider<W: fmt::Write>(
+    w: &mut W,
+    depth: usize,
+    provider: &ProviderConfiguration,
+) -> Result<(), fmt::Error> {
+    let title = "Logs Provider";
+    match provider {
+        #[cfg(any(test, feature = "memory-loglet"))]
+        ProviderConfiguration::InMemory => {
+            write_leaf(w, depth, true, title, "in-memory")?;
+        }
+        ProviderConfiguration::Local => {
+            write_leaf(w, depth, true, title, "local")?;
+        }
+        #[cfg(feature = "replicated-loglet")]
+        ProviderConfiguration::Replicated(config) => {
+            write_leaf(w, depth, true, title, "replicated")?;
+            let depth = depth + 1;
+            write_leaf(
+                w,
+                depth,
+                false,
+                "Log replication",
+                config.replication_property.to_string(),
+            )?;
+            write_leaf(
+                w,
+                depth,
+                true,
+                "Nodeset size",
+                config.target_nodeset_size.to_string(),
+            )?;
+        }
+    }
+    Ok(())
+}
+
+pub fn write_leaf<W: fmt::Write>(
+    w: &mut W,
+    depth: usize,
+    last: bool,
+    title: impl Display,
+    value: impl Display,
+) -> Result<(), fmt::Error> {
+    let depth = depth + 1;
+    let chr = if last { '└' } else { '├' };
+    writeln!(w, "{chr:>depth$} {title}: {value}")
 }
