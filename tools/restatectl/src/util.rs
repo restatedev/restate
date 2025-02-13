92d30a12a87010a3e6a02fec9bfd84dbeab44799
Muhamad Awad <muhamad.azmy+git@gmail.com>
2025-02-13 20:51:24 +0100

    Unify the `range input` for some ids (#2723)
    Summary:
    - Support parsing an "range" input.
    - Handle id range in some common operations
      - `logs describe`
      - `logs find-tail`
      - `logs reconfigure`
      - `snapshots create-snapshot`

--- tools/restatectl/src/util.rs
+++ tools/restatectl/src/util.rs
@@ -8,8 +8,14 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
-use std::fmt::{self, Display};
+use std::{
+    fmt::{self, Display},
+    num::ParseIntError,
+    ops::RangeInclusive,
+    str::FromStr,
+};
 
+use cling::{prelude::Parser, Collect};
 use tonic::transport::Channel;
 
 use restate_cli_util::CliContext;
@@ -68,4 +74,80 @@ pub fn write_leaf<W: fmt::Write>(
     let depth = depth + 1;
     let chr = if last { '└' } else { '├' };
     writeln!(w, "{chr:>depth$} {title}: {value}")
+}
+
+#[derive(Parser, Collect, Clone, Debug)]
+pub struct RangeParam {
+    from: u32,
+    to: u32,
+}
+
+impl RangeParam {
+    fn new(from: u32, to: u32) -> Result<Self, RangeParamError> {
+        if from > to {
+            Err(RangeParamError::InvalidRange(from, to))
+        } else {
+            Ok(RangeParam { from, to })
+        }
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = u32> {
+        self.from..=self.to
+    }
+}
+
+impl<T> From<T> for RangeParam
+where
+    T: Into<u32>,
+{
+    fn from(value: T) -> Self {
+        let id = value.into();
+        Self::new(id, id).expect("equal values")
+    }
+}
+
+impl IntoIterator for RangeParam {
+    type IntoIter = RangeInclusive<u32>;
+    type Item = u32;
+    fn into_iter(self) -> Self::IntoIter {
+        self.from..=self.to
+    }
+}
+
+impl IntoIterator for &RangeParam {
+    type IntoIter = RangeInclusive<u32>;
+    type Item = u32;
+    fn into_iter(self) -> Self::IntoIter {
+        self.from..=self.to
+    }
+}
+
+impl FromStr for RangeParam {
+    type Err = RangeParamError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = s.split("-").collect();
+        match parts.len() {
+            1 => {
+                let n = parts[0].parse()?;
+                Ok(RangeParam::new(n, n)?)
+            }
+            2 => {
+                let from = parts[0].parse()?;
+                let to = parts[1].parse()?;
+                Ok(RangeParam::new(from, to)?)
+            }
+            _ => Err(RangeParamError::InvalidSyntax(s.to_string())),
+        }
+    }
+}
+
+#[derive(Debug, Clone, thiserror::Error)]
+pub enum RangeParamError {
+    #[error("Invalid id range: {0}..{1} start must be <= end range")]
+    InvalidRange(u32, u32),
+    #[error("Invalid range syntax '{0}'")]
+    InvalidSyntax(String),
+    #[error(transparent)]
+    ParseError(#[from] ParseIntError),
 }
