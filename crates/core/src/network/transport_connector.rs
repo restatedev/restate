92c565f4fa7a85333265be46b1159629fb9fb822
Ahmed Farghal <me@asoli.dev>
2025-03-06 17:46:25 +0000

    [Core] Egress stream for tighter control over the egress flow
    Notes:
    - EgressSender's shape is temporary
    - The shape of OwnedConnection will change in upcoming PRs
    - The plan is to move to unbounded channels with external capacity control

--- crates/core/src/network/transport_connector.rs
+++ crates/core/src/network/transport_connector.rs
@@ -58,7 +58,6 @@ pub mod test_util {
     use parking_lot::Mutex;
     use tokio::sync::mpsc;
     use tokio::time::Instant;
-    use tokio_stream::wrappers::ReceiverStream;
     use tracing::info;
 
     use restate_types::GenerationalNodeId;
@@ -67,6 +66,7 @@ pub mod test_util {
     use restate_types::protobuf::node::message::BinaryMessage;
 
     use super::{NetworkError, ProtocolError};
+    use crate::network::io::EgressStream;
     use crate::network::{Incoming, MockPeerConnection, PartialPeerConnection, WeakConnection};
     use crate::{TaskCenter, TaskHandle, TaskKind, my_node_id};
 
@@ -107,7 +107,7 @@ pub mod test_util {
                 node_id, current_generation
             );
 
-            let (sender, rx) = mpsc::channel(self.sendbuf);
+            let (sender, egress, drop_egress) = EgressStream::new(self.sendbuf);
 
             let peer_connection = PartialPeerConnection {
                 my_node_id: node_id,
@@ -115,6 +115,7 @@ pub mod test_util {
                 sender,
                 recv_stream: output_stream.boxed(),
                 created: Instant::now(),
+                drop_egress,
             };
 
             let peer_connection = peer_connection.handshake(nodes_config).await.unwrap();
@@ -125,8 +126,7 @@ pub mod test_util {
                     "MockConnector has been terminated, cannot connect to {node_id}"
                 )));
             }
-            let incoming = ReceiverStream::new(rx).map(Ok);
-            Ok(incoming)
+            Ok(egress.map(Ok))
         }
     }
 
