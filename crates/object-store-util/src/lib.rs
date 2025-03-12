restatedev/restate

commit 352181a1d6e344af29723e21eecc42929834f7fc

Treat invalid HTTP S3 endpoint overrides as a startup configuration error (#2858)
* Use AWS conventional env vars as fallback for object store endpoint config

* Bail if an http:// override endpoint is set for object store without aws-allow-http

This is a common Minio configuration snag which results in hard-to-understand errors at runtime.
Since this will just not work, and potentially induces a data loss risk, it's better to complain
loudly upfront.

crates/object-store-util/src/lib.rs
@@ -102,13 +102,37 @@ pub async fn create_object_store_client(
         } else {
             builder
         };
-        let builder = if let Some(endpoint_url) = &options.aws_endpoint_url {
-            builder.with_endpoint(endpoint_url)
+        let env_allow_http = std::env::var("AWS_ALLOW_HTTP")
+            .ok()
+            .map(|s| s.trim().eq_ignore_ascii_case("true"));
+        let allow_insecure_http = options.aws_allow_http.or(env_allow_http);
+        let builder = if let Some(allow_http) = allow_insecure_http {
+            builder.with_allow_http(allow_http)
         } else {
             builder
         };
-        let builder = if let Some(allow_http) = &options.aws_allow_http {
-            builder.with_allow_http(*allow_http)
+        let env_endpoint_url = std::env::var("AWS_ENDPOINT_URL_S3")
+            .ok()
+            .or_else(|| std::env::var("AWS_ENDPOINT_URL").ok());
+        let s3_endpoint = options
+            .aws_endpoint_url
+            .as_ref()
+            .or(env_endpoint_url.as_ref());
+        if !env_allow_http.unwrap_or_default()
+            && s3_endpoint.is_some_and(|endpoint| {
+                Url::parse(endpoint).is_ok_and(|url| url.scheme().eq_ignore_ascii_case("http"))
+            })
+        {
+            anyhow::bail!(
+                "Misconfiguration detected: an HTTP endpoint URL \"{}\" override is set for object \
+                store destination \"{}\", but plain HTTP is not allowed. Please set 'aws-allow-http' \
+                to `true` to enable this destination",
+                s3_endpoint.expect("is some"),
+                destination
+            );
+        }
+        let builder = if let Some(endpoint_url) = s3_endpoint {
+            builder.with_endpoint(endpoint_url)
         } else {
             builder
         };

commit b7536b64e12a643cec52d375861426a8a90784db

Fix allow-HTTP logic
Fixes an error introduced by #2858 which only recognizes the global AWS_ALLOW_HTTP setting, and not the snapshots/metadata store client-specific config section keys.

crates/object-store-util/src/lib.rs
@@ -102,23 +102,23 @@ pub async fn create_object_store_client(
         } else {
             builder
         };
-        let env_allow_http = std::env::var("AWS_ALLOW_HTTP")
+        let env_allow_http_fallback = std::env::var("AWS_ALLOW_HTTP")
             .ok()
             .map(|s| s.trim().eq_ignore_ascii_case("true"));
-        let allow_insecure_http = options.aws_allow_http.or(env_allow_http);
+        let allow_insecure_http = options.aws_allow_http.or(env_allow_http_fallback);
         let builder = if let Some(allow_http) = allow_insecure_http {
             builder.with_allow_http(allow_http)
         } else {
             builder
         };
-        let env_endpoint_url = std::env::var("AWS_ENDPOINT_URL_S3")
+        let env_endpoint_url_fallback = std::env::var("AWS_ENDPOINT_URL_S3")
             .ok()
             .or_else(|| std::env::var("AWS_ENDPOINT_URL").ok());
         let s3_endpoint = options
             .aws_endpoint_url
             .as_ref()
-            .or(env_endpoint_url.as_ref());
-        if !env_allow_http.unwrap_or_default()
+            .or(env_endpoint_url_fallback.as_ref());
+        if !allow_insecure_http.unwrap_or_default()
             && s3_endpoint.is_some_and(|endpoint| {
                 Url::parse(endpoint).is_ok_and(|url| url.scheme().eq_ignore_ascii_case("http"))
             })