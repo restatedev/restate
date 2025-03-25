b57d50b9f601fc913f801dd2d5d332e9bcbc8575
lsytj0413 <lsytj0413@gmail.com>
2025-03-25 16:45:43 +0800

    chore(*): improve cluster and node name metrics label computation

--- crates/node/src/network_server/metrics.rs
+++ crates/node/src/network_server/metrics.rs
@@ -188,15 +188,12 @@ pub async fn render_metrics(State(state): State<NodeCtrlHandlerState>) -> String
     let manager = RocksDbManager::get();
     let all_dbs = manager.get_all_dbs();
 
-    let mut global_labels: Vec<String> = vec![];
-    for (k, v) in state.prometheus_handle.global_labels() {
-        global_labels.push(format!("{}=\"{}\"", k, formatting::sanitize_label_value(v)));
-    }
+    let mut labels = state.prometheus_handle.global_labels().clone();
 
     // Overall write buffer manager stats
     format_rocksdb_property_for_prometheus(
         &mut out,
-        &global_labels,
+        &labels,
         MetricUnit::Bytes,
         "rocksdb.memory.write_buffer_manager_capacity",
         manager.get_total_write_buffer_capacity(),
@@ -204,19 +201,18 @@ pub async fn render_metrics(State(state): State<NodeCtrlHandlerState>) -> String
 
     format_rocksdb_property_for_prometheus(
         &mut out,
-        &global_labels,
+        &labels,
         MetricUnit::Bytes,
         "rocksdb.memory.write_buffer_manager_usage",
         manager.get_total_write_buffer_usage(),
     );
 
     for db in &all_dbs {
-        let mut labels = Vec::with_capacity(global_labels.len() + 1);
-        global_labels.clone_into(&mut labels);
         labels.push(format!(
             "db=\"{}\"",
             formatting::sanitize_label_value(&db.name)
         ));
+
         // Tickers (Counters)
         for ticker in ROCKSDB_TICKERS {
             format_rocksdb_stat_ticker_for_prometheus(&mut out, db, &labels, *ticker);
@@ -279,13 +275,11 @@ pub async fn render_metrics(State(state): State<NodeCtrlHandlerState>) -> String
         // For properties, we need to get them for each column family.
         for cf in &db.cfs() {
             let sanitized_cf_name = formatting::sanitize_label_value(cf);
-            let mut cf_labels = Vec::with_capacity(labels.len() + 1);
-            labels.clone_into(&mut cf_labels);
-            cf_labels.push(format!("cf=\"{sanitized_cf_name}\""));
+            labels.push(format!("cf=\"{sanitized_cf_name}\""));
             for (property, unit) in ROCKSDB_CF_PROPERTIES {
                 format_rocksdb_property_for_prometheus(
                     &mut out,
-                    &cf_labels,
+                    &labels,
                     *unit,
                     property,
                     db.inner()
@@ -294,7 +288,9 @@ pub async fn render_metrics(State(state): State<NodeCtrlHandlerState>) -> String
                         .unwrap_or_default(),
                 );
             }
+            labels.pop();
         }
+        labels.pop();
     }
     out
 }
