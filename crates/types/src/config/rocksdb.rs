93fd7913a4f19501557cefb97b72b03188a7010d
Till Rohrmann <till@restate.dev>
2025-03-25 18:03:40 +0100

    Use the correct config option for rocksdb_disable_direct_io_for_flush_and_compaction()

--- crates/types/src/config/rocksdb.rs
+++ crates/types/src/config/rocksdb.rs
@@ -165,7 +165,8 @@ impl RocksDbOptions {
     }
 
     pub fn rocksdb_disable_direct_io_for_flush_and_compaction(&self) -> bool {
-        self.rocksdb_disable_direct_io_for_reads.unwrap_or(false)
+        self.rocksdb_disable_direct_io_for_flush_and_compactions
+            .unwrap_or(false)
     }
 
     pub fn rocksdb_disable_statistics(&self) -> bool {
