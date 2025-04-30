fbab06877111cfabbf22720de2bde5bc54a864f2
Ahmed Farghal <me@asoli.dev>
2025-04-30 09:02:40 +0100

    [mechanical] Rename BaseRole into FailureDetector
    Upcycling `BaseRole` to serve as the home of the WIP failure detector. This PR does not change any behaviour.
    
    ```
    // intentionally empty
    ```

--- crates/node/src/roles/mod.rs
+++ crates/node/src/roles/mod.rs
@@ -9,11 +9,9 @@
 // by the Apache License, Version 2.0.
 
 mod admin;
-mod base;
 mod ingress;
 mod worker;
 
 pub use admin::{AdminRole, AdminRoleBuildError};
-pub use base::BaseRole;
 pub use ingress::IngressRole;
 pub use worker::{WorkerRole, WorkerRoleBuildError};
