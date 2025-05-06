323c4b6a9c39c109784a9cef699db3ed8ebdc441
Ahmed Farghal <me@asoli.dev>
2025-05-06 18:00:59 +0100

    [FailureDetector] Gossip Configuration and Network Messages

--- crates/types/src/config/mod.rs
+++ crates/types/src/config/mod.rs
@@ -17,6 +17,7 @@ mod bifrost;
 #[cfg(feature = "clap")]
 mod cli_option_overrides;
 mod common;
+mod gossip;
 mod http;
 mod ingress;
 mod kafka;
@@ -34,6 +35,7 @@ pub use bifrost::*;
 #[cfg(feature = "clap")]
 pub use cli_option_overrides::*;
 pub use common::*;
+pub use gossip::*;
 pub use http::*;
 pub use ingress::*;
 pub use kafka::*;
