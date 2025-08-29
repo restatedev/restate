633d6e9de32670c878efb45cea7320c1ca348fdd
slinkydeveloper <me@slinkydeveloper.com>
2025-08-29 16:45:04 +0200

    Add Resume API to InvocationClient

--- crates/core/src/worker_api/partition_processor_rpc_client.rs
+++ crates/core/src/worker_api/partition_processor_rpc_client.rs
@@ -21,7 +21,8 @@ use restate_types::identifiers::{
 use restate_types::invocation::client::{
     AttachInvocationResponse, CancelInvocationResponse, GetInvocationOutputResponse,
     InvocationClient, InvocationClientError, InvocationOutput, KillInvocationResponse,
-    PurgeInvocationResponse, RestartAsNewInvocationResponse, SubmittedInvocationNotification,
+    PurgeInvocationResponse, RestartAsNewInvocationResponse, ResumeInvocationResponse,
+    SubmittedInvocationNotification,
 };
 use restate_types::invocation::{InvocationQuery, InvocationRequest, InvocationResponse};
 use restate_types::journal_v2::Signal;
@@ -511,6 +512,28 @@ where
             ) => restart_as_new_invocation_response.into(),
             _ => {
                 panic!("Expecting RestartAsNewInvocation rpc response")
+            }
+        })
+    }
+
+    async fn resume_invocation(
+        &self,
+        request_id: PartitionProcessorRpcRequestId,
+        invocation_id: InvocationId,
+    ) -> Result<ResumeInvocationResponse, InvocationClientError> {
+        let response = self
+            .resolve_partition_id_and_send(
+                request_id,
+                PartitionProcessorRpcRequestInner::ResumeInvocation { invocation_id },
+            )
+            .await?;
+
+        Ok(match response {
+            PartitionProcessorRpcResponse::ResumeInvocation(resume_invocation_response) => {
+                resume_invocation_response.into()
+            }
+            _ => {
+                panic!("Expecting ResumeInvocation rpc response")
             }
         })
     }
