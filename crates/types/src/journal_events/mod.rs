71c7e60e09c1f9018a492b8bf7de370cb28d5d3c
slinkydeveloper <me@slinkydeveloper.com>
2025-09-02 18:47:48 +0200

    New Key structure for the JournalEvent table. Removed the need to store the events count in the InvocationStatus, and the deduplication hash.

--- crates/types/src/journal_events/mod.rs
+++ crates/types/src/journal_events/mod.rs
@@ -12,18 +12,28 @@
 
 use crate::errors::InvocationErrorCode;
 use crate::journal_v2::CommandType;
-use bytes::Bytes;
 use serde::{Deserialize, Serialize};
-use sha2::{Digest, Sha256};
 use strum::EnumString;
 
 pub mod raw;
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq, EnumString, strum::Display, Serialize, Deserialize)]
+#[derive(
+    Debug,
+    Copy,
+    Clone,
+    PartialEq,
+    Eq,
+    EnumString,
+    strum::Display,
+    Serialize,
+    Deserialize,
+    strum::FromRepr,
+)]
+#[repr(u8)]
 pub enum EventType {
-    TransientError,
-    Paused,
-    Unknown,
+    Unknown = 0,
+    TransientError = 1,
+    Paused = 2,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, derive_more::From)]
@@ -51,48 +61,6 @@ pub struct TransientErrorEvent {
     pub related_command_name: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub related_command_type: Option<CommandType>,
-}
-
-impl TransientErrorEvent {
-    pub fn deduplication_hash(&self) -> Bytes {
-        const HASH_SEPARATOR: u8 = 0x2c;
-
-        // WARNING: Changing this code will result in having duplicated events between restate versions.
-        let mut hasher = Sha256::new();
-        hasher.update(b"tee");
-        hasher.update([HASH_SEPARATOR]);
-        hasher.update(u16::from(self.error_code).to_le_bytes());
-        hasher.update([HASH_SEPARATOR]);
-        hasher.update(&self.error_message);
-        hasher.update([HASH_SEPARATOR]);
-        if let Some(error_code) = &self.restate_doc_error_code {
-            hasher.update(error_code);
-        } else {
-            hasher.update(b"-");
-        }
-        hasher.update([HASH_SEPARATOR]);
-        if let Some(command_index) = &self.related_command_index {
-            hasher.update(command_index.to_le_bytes());
-        } else {
-            hasher.update(b"-");
-        }
-        hasher.update([HASH_SEPARATOR]);
-        if let Some(command_name) = &self.related_command_name {
-            hasher.update(command_name);
-        } else {
-            hasher.update(b"-");
-        }
-        hasher.update([HASH_SEPARATOR]);
-        if let Some(command_type) = &self.related_command_type {
-            let str: &'static str = command_type.into();
-            hasher.update(str);
-        } else {
-            hasher.update(b"-");
-        }
-        let result = hasher.finalize();
-
-        result.to_vec().into()
-    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
--- crates/types/src/journal_events/raw.rs
+++ crates/types/src/journal_events/raw.rs
@@ -17,45 +17,27 @@ use prost::Message;
 #[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
 pub struct RawEvent {
     ty: EventType,
-    deduplication_hash: Option<Bytes>,
     value: Bytes,
 }
 
 impl RawEvent {
     pub fn new(ty: EventType, value: Bytes) -> Self {
-        RawEvent {
-            ty,
-            deduplication_hash: None,
-            value,
-        }
+        RawEvent { ty, value }
     }
 
     pub fn unknown() -> Self {
         RawEvent {
             ty: EventType::Unknown,
-            deduplication_hash: None,
             value: Bytes::default(),
         }
-    }
-
-    /// See [self.set_deduplication_hash].
-    pub fn deduplication_hash(&self) -> Option<&Bytes> {
-        self.deduplication_hash.as_ref()
-    }
-
-    /// When setting the deduplication hash, the Partition processor will try to deduplicate this event with the last event in the journal (if present) by matching the deduplication_hash.
-    ///
-    /// When unset, no deduplication will happen and the event is stored as is.
-    pub fn set_deduplication_hash(&mut self, hash: impl Into<Bytes>) {
-        self.deduplication_hash = Some(hash.into());
     }
 
     pub fn ty(&self) -> EventType {
         self.ty
     }
 
-    pub fn into_inner(self) -> (EventType, Option<Bytes>, Bytes) {
-        (self.ty, self.deduplication_hash, self.value)
+    pub fn into_inner(self) -> (EventType, Bytes) {
+        (self.ty, self.value)
     }
 
     pub fn into_event_or_unknown(self) -> Event {
