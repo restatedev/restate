acf8815e5a2d9b0c88c476e60c66adb7a68096b0
Ahmed Farghal <me@asoli.dev>
2025-09-03 11:32:37 +0100

    [fingerprint] Validate cluster fingerprint of snapshots

--- crates/partition-store/src/snapshots/repository.rs
+++ crates/partition-store/src/snapshots/repository.rs
@@ -16,6 +16,7 @@ use anyhow::{Context, anyhow, bail};
 use bytes::BytesMut;
 use object_store::path::Path as ObjectPath;
 use object_store::{MultipartUpload, ObjectStore, PutMode, PutOptions, PutPayload, UpdateVersion};
+use restate_core::Metadata;
 use serde::{Deserialize, Serialize};
 use serde_with::serde_as;
 use tempfile::TempDir;
@@ -112,6 +113,44 @@ impl LatestSnapshot {
             min_applied_lsn: snapshot.min_applied_lsn,
             path: UniqueSnapshotKey::from_metadata(snapshot).padded_key(),
         }
+    }
+
+    pub fn validate(
+        &self,
+        cluster_name: &str,
+        cluster_fingerprint: Option<ClusterFingerprint>,
+    ) -> anyhow::Result<()> {
+        if cluster_name != self.cluster_name {
+            anyhow::bail!(
+                "snapshot does not match the cluster name of this cluster, \
+                 expected: '{cluster_name}' got: '{}'",
+                self.cluster_name
+            );
+        }
+
+        // Does our nodes configuration have a fingerprint?
+        //
+        // If not, we will completely ignore the fingerprint check because we assume
+        // that there is a race and the nodes configuration will be updated soon. This
+        // can be made more strict in v1.6.0 since we'll be sure that nodes
+        // configuration will always contain a fingerprint.
+        //
+        //
+        // If we have a fingerprint in nodes configuration, we will use it to compare
+        // with the snapshot's fingerprint, if and only if the snapshot has a
+        // fingerprint as well.
+        if let Some(cluster_fingerprint) = cluster_fingerprint
+            && let Some(incoming_fingerprint) = self.cluster_fingerprint
+            && cluster_fingerprint != incoming_fingerprint
+        {
+            bail!(
+                "cluster fingerprint mismatch, \
+                 expected:'{cluster_fingerprint}' {cluster_fingerprint:?} got:'{incoming_fingerprint}' {incoming_fingerprint:?}. \
+                 This often happens if this cluster is reusing a snapshot repository path from a different cluster"
+            );
+        }
+
+        Ok(())
     }
 }
 
@@ -329,7 +368,6 @@ impl SnapshotRepository {
         name = "get-latest-snapshot",
         level = "error",
         skip_all,
-        err,
         fields(%partition_id, snapshot_id = tracing::field::Empty),
     )]
     pub async fn get_latest(
@@ -350,6 +388,16 @@ impl SnapshotRepository {
         let latest: LatestSnapshot = serde_json::from_slice(&latest.bytes().await?)?;
         tracing::Span::current().record("snapshot_id", tracing::field::display(latest.snapshot_id));
         debug!("Latest snapshot metadata: {latest:?}");
+        Metadata::with_current(|m| {
+            let nodes_config = m.nodes_config_ref();
+
+            latest.validate(
+                nodes_config.cluster_name(),
+                nodes_config.cluster_fingerprint(),
+            )?;
+            anyhow::Ok(())
+        })
+        .with_context(|| format!("'{latest_path}' has validation errors"))?;
 
         let snapshot_metadata_path = self
             .prefix
@@ -361,9 +409,9 @@ impl SnapshotRepository {
         let snapshot_metadata = match snapshot_metadata {
             Ok(result) => result,
             Err(object_store::Error::NotFound { .. }) => {
-                // todo(pavel): revisit whether we shouldn't just panic at this point - this is a bad sign!
-                warn!("Latest snapshot points to a snapshot that was not found in the repository!");
-                return Ok(None); // arguably this could also be an error
+                bail!(
+                    "Latest snapshot points to '{snapshot_metadata_path}' that was not found in the repository!"
+                );
             }
             Err(e) => return Err(e.into()),
         };
@@ -371,21 +419,27 @@ impl SnapshotRepository {
         let mut snapshot_metadata: PartitionSnapshotMetadata =
             serde_json::from_slice(&snapshot_metadata.bytes().await?)?;
         if snapshot_metadata.version != SnapshotFormatVersion::V1 {
-            return Err(anyhow!(
+            bail!(
                 "Unsupported snapshot format version: {:?}",
                 snapshot_metadata.version
-            ));
+            );
         }
 
-        if snapshot_metadata.cluster_name != self.cluster_name {
-            // todo(pavel): revisit whether we shouldn't just panic at this point - this is a bad sign!
-            warn!(
-                "Snapshot cluster name does not match node configuration! \
-                Expected: \"{}\", found: \"{}\"",
-                self.cluster_name, snapshot_metadata.cluster_name
-            );
-            return Ok(None); // perhaps this needs to be a configuration error
-        }
+        Metadata::with_current(|m| {
+            let nodes_config = m.nodes_config_ref();
+
+            snapshot_metadata.validate(
+                nodes_config.cluster_name(),
+                nodes_config.cluster_fingerprint(),
+            )?;
+            anyhow::Ok(())
+        })
+        .with_context(|| {
+            format!(
+                "failed validating metadata of snapshot {}",
+                snapshot_metadata.snapshot_id
+            )
+        })?;
 
         if !self.staging_dir.exists() {
             std::fs::create_dir_all(&self.staging_dir)?;
@@ -502,7 +556,11 @@ impl SnapshotRepository {
                 debug!("Latest snapshot data not found in repository");
                 return Ok(Lsn::INVALID);
             }
-            Err(e) => return Err(e.into()),
+            Err(e) => {
+                return Err(anyhow::Error::new(e).context(format!(
+                    "couldn't fetch '{latest_path}' from snapshot repository"
+                )));
+            }
         };
 
         let latest: LatestSnapshot = serde_json::from_slice(&latest.bytes().await?)?;
@@ -535,22 +593,16 @@ impl SnapshotRepository {
                     )
                     })
                     .map_err(|e| anyhow!("Failed to parse latest snapshot metadata: {}", e))?;
-                if snapshot.cluster_name != latest.cluster_name {
-                    // This indicates a serious misconfiguration and we should complain loudly
-                    bail!(
-                        "Snapshot does not match the cluster name of latest snapshot at destination!"
-                    );
-                }
-                // The check is disabled until we pass the point where fingerprints are enforced.
-                //
-                // If this check is turned on, nodes will not be able to migrate from "no
-                // fingerprint" to "fingerprint" being set.
-                // It's estimated that this will happen in v1.5.0
-                // if snapshot.cluster_fingerprint != latest.cluster_fingerprint {
-                //     bail!(
-                //         "Snapshot does not match the cluster fingerprint of latest snapshot at destination!"
-                //     );
-                // }
+
+                Metadata::with_current(|m| {
+                    let nodes_config = m.nodes_config_ref();
+                    let fingerprint = nodes_config.cluster_fingerprint();
+                    let cluster_name = nodes_config.cluster_name();
+                    latest.validate(cluster_name, fingerprint)?;
+                    snapshot.validate(cluster_name, fingerprint)?;
+                    anyhow::Ok(())
+                })?;
+
                 Ok(Some((latest, version)))
             }
             Err(object_store::Error::NotFound { .. }) => {
