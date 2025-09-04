86c4a90e30efdbb13db8714d29a51b286e711d7a
Ahmed Farghal <me@asoli.dev>
2025-09-04 17:46:43 +0100

    Extension trait for `Duration` for friendly display
    Introduces a crate for time utilities (`serde-time-util`). The crate currently provides a trait `DurationExt` which provides a `friendly()` method for formatting a `Duration` as a human-readable string. It also allows parsing a human-friendly duration into a string.
    
    
    Additionally, this removes the helpers from `DurationString` and keeps `restate-serde-util`'s DurationString as a serialization helper only.
    
    Minor but relevant changes:
    - Fixed parsing "0" as a duration. Previously, it was required to write "0s" to parse a zero duration.
    - Some debug traces that reported the response latency now use the `to_seconds_span()` since it's cheaper to compute.
    - Fixed typos in restate CLI service config commands
    - More efficient serialization for DurationString that avoids an extra String allocation.
    - Used jiff's native span rounding feature to round a duration to "days" instead of the manual calculation.
    - Added a `negated()` method to the time span types to support "ago" formatting.
    - Migrateed all uses of `DurationString` to `DurationExt::friendly()` including in CLI argument parsing.

--- crates/serde-util/src/duration.rs
+++ crates/serde-util/src/duration.rs
@@ -8,73 +8,48 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
+use std::str::FromStr as _;
 use std::time::Duration;
 
-use jiff::fmt::friendly::{Designator, SpanPrinter};
-use jiff::{Span, SpanRelativeTo};
-use serde::de::{Error, IntoDeserializer};
-use serde::{Deserialize, Deserializer, Serializer};
+use restate_time_util::{DurationExt, FriendlyDuration};
+
+use serde::de::Error;
+use serde::{Deserializer, Serializer};
 use serde_with::{DeserializeAs, SerializeAs};
 
 /// Serializable/Deserializable duration for use with serde_with.
 ///
-/// Deserialization uses [`jiff::Span`]'s parsing to support both human-friendly and ISO8601
+/// Deserialization uses [`restate_time_util::FriendlyDuration`]'s parsing to support both human-friendly and ISO8601
 /// inputs. Days are the largest supported unit of time, and are interpreted as 24 hours long when
 /// converting the parsed span into an actual duration.
 ///
-/// Serialization is performed using [`jiff::fmt::friendly`] for output.
+/// Serialization is performed using [`restate_time_util::FriendlyDuration::to_days_span`] for output.
 pub struct DurationString;
 
-impl DurationString {
-    /// Parse a duration from a string using the [`jiff::fmt::friendly`] format, allowing units of
-    /// up to days to be used.
-    pub fn parse_duration(s: &str) -> Result<Duration, serde::de::value::Error> {
-        serde_with::As::<DurationString>::deserialize(s.into_deserializer())
-    }
-
-    /// Prints a duration as a string using the [`jiff::fmt::friendly`] format, using units up to
-    /// days.
-    pub fn display(duration: Duration) -> String {
-        // jiff's SignedDuration pretty-print will add up units up to hours but not to days,
-        // so we use a Span to get finer control over the display output and use calendar units
-        // up to days (which we consider to be 24 hours in duration)
-        let mut span = Span::try_from(duration).expect("fits i64");
-        if span.get_seconds() >= 60 {
-            let minutes = span.get_seconds() / 60;
-            let seconds = span.get_seconds() % 60;
-            span = span.minutes(minutes).seconds(seconds);
-        }
-        if span.get_minutes() >= 60 {
-            let hours = span.get_minutes() / 60;
-            let minutes = span.get_minutes() % 60;
-            span = span.hours(hours).minutes(minutes);
-        }
-        if span.get_hours() >= 24 {
-            let days = span.get_hours() / 24;
-            let hours = span.get_hours() % 24;
-            span = span.days(days).hours(hours);
-        };
-
-        let printer = SpanPrinter::new().designator(Designator::Compact);
-        printer.span_to_string(&span)
-    }
-}
-
-impl<'de> DeserializeAs<'de, std::time::Duration> for DurationString {
+impl<'de> DeserializeAs<'de, Duration> for DurationString {
     fn deserialize_as<D>(deserializer: D) -> Result<std::time::Duration, D::Error>
     where
         D: Deserializer<'de>,
     {
-        let span: Span = String::deserialize(deserializer)?
-            .parse()
-            .map_err(Error::custom)?;
-        if span.get_years() > 0 || span.get_months() > 0 {
-            return Err(Error::custom("Please use units of days or smaller"));
+        struct Helper;
+        impl serde::de::Visitor<'_> for Helper {
+            type Value = Duration;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                formatter.write_str("a human-friendly duration string")
+            }
+
+            fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                FriendlyDuration::from_str(value)
+                    .map_err(Error::custom)
+                    .map(|d| d.to_std())
+            }
         }
-        let signed_duration = span
-            .to_duration(SpanRelativeTo::days_are_24_hours())
-            .map_err(Error::custom)?;
-        Duration::try_from(signed_duration).map_err(Error::custom)
+
+        deserializer.deserialize_str(Helper)
     }
 }
 
@@ -83,7 +58,7 @@ impl SerializeAs<std::time::Duration> for DurationString {
     where
         S: Serializer,
     {
-        serializer.collect_str(&DurationString::display(*source))
+        serializer.collect_str(&source.friendly().to_days_span())
     }
 }
 
@@ -103,6 +78,8 @@ impl schemars::JsonSchema for DurationString {
         metadata.examples = vec![
             serde_json::Value::String("10 hours".to_owned()),
             serde_json::Value::String("5 days".to_owned()),
+            serde_json::Value::String("5d".to_owned()),
+            serde_json::Value::String("1h 4m".to_owned()),
             serde_json::Value::String("P40D".to_owned()),
         ];
         schema.into()
@@ -113,7 +90,6 @@ impl schemars::JsonSchema for DurationString {
 mod tests {
     use super::*;
 
-    use jiff::{SignedDuration, ToSpan};
     use serde::{Deserialize, Serialize};
 
     use serde_with::serde_as;
@@ -122,53 +98,6 @@ mod tests {
     #[derive(Serialize, Deserialize)]
     #[serde(transparent)]
     struct MyDuration(#[serde_as(as = "DurationString")] std::time::Duration);
-
-    #[test]
-    fn parse_duration_input_formats() {
-        let duration = DurationString::parse_duration("10 min");
-        assert_eq!(Ok(Duration::from_secs(600)), duration);
-
-        // we don't support "1 month" as months have variable length
-        let duration = DurationString::parse_duration("P1M");
-        assert_eq!(
-            Err(serde::de::Error::custom(
-                "Please use units of days or smaller"
-            )),
-            duration
-        );
-
-        // we can, however, use "30 days" instead - we fix those to 24 hours
-        let duration = DurationString::parse_duration("P30D");
-        assert_eq!(Ok(Duration::from_secs(30 * 24 * 3600)), duration);
-
-        // we should not render larger units which are unsupported as inputs
-        let duration = DurationString::parse_duration("30 days 48 hours").unwrap();
-        assert_eq!(
-            30.days()
-                .hours(48)
-                .to_duration(SpanRelativeTo::days_are_24_hours())
-                .unwrap(),
-            SignedDuration::try_from(duration).unwrap()
-        );
-        let duration_str = DurationString::display(duration);
-        assert_eq!("32d", duration_str);
-
-        // more complex inputs are also supported - but will be serialized as a more humane output
-        let duration = DurationString::parse_duration("P30DT10H30M15S");
-        assert_eq!(
-            Ok(Duration::from_secs(
-                30 * 24 * 3600 + 10 * 3600 + 30 * 60 + 15
-            )),
-            duration
-        );
-        assert_eq!(
-            serde_json::from_str::<String>(
-                &serde_json::to_string(&MyDuration(duration.unwrap())).unwrap()
-            )
-            .unwrap(),
-            "30d 10h 30m 15s"
-        );
-    }
 
     #[test]
     fn serialize_friendly() {
