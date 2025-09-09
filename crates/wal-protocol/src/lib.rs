d8107529762610763387f9be5f879d454e6b1a32
Ahmed Farghal <me@asoli.dev>
2025-09-09 09:56:49 +0100

    Revert #3614 until further changes in Envelope

--- crates/wal-protocol/src/lib.rs
+++ crates/wal-protocol/src/lib.rs
@@ -275,8 +275,8 @@ mod envelope {
         }
 
         fn default_codec(&self) -> StorageCodecKind {
-            // Switched to `Custom` in v1.5 to allow for more flexibility in the future.
-            StorageCodecKind::Custom
+            // todo: Could be changed in v1.6
+            StorageCodecKind::FlexbuffersSerde
         }
     }
 
