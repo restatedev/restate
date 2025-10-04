f1440b8582423e9f84529f407511af0401bd0b2d
Pavel Tcholakov <pavel@restate.dev>
2025-10-04 07:46:31 +0200

    Enable Azure and GCS object-store protocols for partition snapshots
    This change enables native Azure and GCS access for snapshots. The new
    protocols are unavailable for use use as a metadata store backend, and
    S3 remains the only supported provider when metadata client type is
    "object-store" pending further validation.
    
    The change also removes the undocumented file:// protocol support which
    suffers from not supporting conditional updates.

--- crates/object-store-util/src/lib.rs
+++ crates/object-store-util/src/lib.rs
@@ -13,65 +13,47 @@ use std::pin::Pin;
 use std::sync::Arc;
 use std::time::Duration;
 
-use anyhow::Context;
+use anyhow::{Context, bail};
 use aws_config::default_provider::region::DefaultRegionChain;
 use aws_config::{BehaviorVersion, Region};
 use aws_smithy_runtime_api::client::identity::ResolveCachedIdentity;
 use aws_smithy_runtime_api::client::runtime_components::RuntimeComponentsBuilder;
 use futures::FutureExt;
 use object_store::aws::{AmazonS3Builder, S3ConditionalPut};
-use object_store::{BackoffConfig, ObjectStore, RetryConfig};
+use object_store::azure::MicrosoftAzureBuilder;
+use object_store::gcp::GoogleCloudStorageBuilder;
+use object_store::{BackoffConfig, ObjectStore, ObjectStoreScheme, RetryConfig};
 use tracing::debug;
 use url::Url;
 
 use restate_types::retries::RetryPolicy;
 
 pub async fn create_object_store_client(
-    destination: Url,
+    url: Url,
     options: &restate_types::config::ObjectStoreOptions,
     retry_policy: &RetryPolicy,
 ) -> anyhow::Result<Arc<dyn ObjectStore>> {
-    // We use the AWS SDK configuration and credentials provider so that the conventional AWS
-    // environment variables and config files work as expected. The object_store crate has its
-    // own configuration mechanism which doesn't support many of the AWS conventions. This
-    // differs quite a lot from the Lambda invoker which uses the AWS SDK, and that would be a
-    // very surprising inconsistency for customers. This mechanism allows us to infer the region
-    // and securely obtain session credentials without any hard-coded configuration.
-    let object_store: Arc<dyn ObjectStore> = if destination.scheme() == "s3" {
-        let builder = match &options.aws_profile {
-            Some(profile) => {
-                debug!(profile, "Using AWS profile for object store access");
+    // We do not use the top-level convenience method object_store::parse_url() as the
+    // store-specific builders' from_env() provide a more ergonomic setup mechanism that allows us
+    // to avoid having too many provider-specific config keys, while still supporting them when it
+    // makes sense to do so.
+    let object_store: Arc<dyn ObjectStore> = match ObjectStoreScheme::parse(&url)?.0 {
+        ObjectStoreScheme::AmazonS3 => {
+            // We use the AWS SDK configuration and credentials provider so that the conventional
+            // AWS environment variables and config files work as expected. The object_store crate
+            // has its own configuration mechanism which doesn't support many of the AWS
+            // conventions. This differs quite a lot from the Lambda invoker which uses the AWS SDK,
+            // and that would be a very surprising inconsistency for customers. This mechanism
+            // allows us to infer the region and securely obtain session credentials without any
+            // hard-coded configuration.
+            let builder = match &options.aws_profile {
+                Some(profile) => {
+                    debug!(profile, "Using AWS profile for object store access");
 
-                let sdk_config = aws_config::defaults(BehaviorVersion::latest())
-                    .profile_name(profile)
-                    .load()
-                    .await;
-
-                let region = options
-                    .aws_region
-                    .clone()
-                    .map(Region::new)
-                    .or_else(|| sdk_config.region().cloned())
-                    .context("Unable to determine AWS region")?;
-
-                debug!(?region, ?profile, "Using AWS SDK credentials provider");
-
-                let builder = AmazonS3Builder::new()
-                    .with_region(region.to_string())
-                    .with_credentials(Arc::new(AwsSdkCredentialsProvider::new(&sdk_config)?));
-
-                if let Some(endpoint_url) = sdk_config.endpoint_url() {
-                    debug!(endpoint_url, "Using custom AWS endpoint override");
-                    // we'll override this with the explicit endpoint URL from Restate config, if any, later on
-                    builder.with_endpoint(endpoint_url)
-                } else {
-                    builder
-                }
-            }
-
-            None => {
-                if options.aws_access_key_id.is_none() {
-                    let sdk_config = aws_config::defaults(BehaviorVersion::latest()).load().await;
+                    let sdk_config = aws_config::defaults(BehaviorVersion::latest())
+                        .profile_name(profile)
+                        .load()
+                        .await;
 
                     let region = options
                         .aws_region
@@ -80,92 +62,131 @@ pub async fn create_object_store_client(
                         .or_else(|| sdk_config.region().cloned())
                         .context("Unable to determine AWS region")?;
 
-                    debug!(?region, "Using AWS SDK credentials provider");
+                    debug!(?region, ?profile, "Using AWS SDK credentials provider");
 
-                    AmazonS3Builder::new()
+                    let builder = AmazonS3Builder::new()
                         .with_region(region.to_string())
-                        .with_credentials(Arc::new(AwsSdkCredentialsProvider::new(&sdk_config)?))
-                } else {
-                    let region = if let Some(region) = options.aws_region.as_ref() {
-                        Region::new(region.clone())
+                        .with_credentials(Arc::new(AwsSdkCredentialsProvider::new(&sdk_config)?));
+
+                    if let Some(endpoint_url) = sdk_config.endpoint_url() {
+                        debug!(endpoint_url, "Using custom AWS endpoint override");
+                        // we'll override this with the explicit endpoint URL from Restate config, if any, later on
+                        builder.with_endpoint(endpoint_url)
                     } else {
-                        DefaultRegionChain::builder()
-                            .build()
-                            .region()
-                            .await
-                            .context("Unable to determine AWS region")?
-                    };
-
-                    AmazonS3Builder::new().with_region(region.to_string())
+                        builder
+                    }
                 }
-            }
-        };
 
-        let builder = if let Some(region) = &options.aws_region {
-            builder.with_region(region)
-        } else {
-            builder
-        };
-        let env_allow_http_fallback = std::env::var("AWS_ALLOW_HTTP")
-            .ok()
-            .map(|s| s.trim().eq_ignore_ascii_case("true"));
-        let allow_insecure_http = options.aws_allow_http.or(env_allow_http_fallback);
-        let builder = if let Some(allow_http) = allow_insecure_http {
-            builder.with_allow_http(allow_http)
-        } else {
-            builder
-        };
-        let env_endpoint_url_fallback = std::env::var("AWS_ENDPOINT_URL_S3")
-            .ok()
-            .or_else(|| std::env::var("AWS_ENDPOINT_URL").ok());
-        let s3_endpoint = options
-            .aws_endpoint_url
-            .as_ref()
-            .or(env_endpoint_url_fallback.as_ref());
-        if !allow_insecure_http.unwrap_or_default()
-            && s3_endpoint.is_some_and(|endpoint| {
-                Url::parse(endpoint).is_ok_and(|url| url.scheme().eq_ignore_ascii_case("http"))
-            })
-        {
-            anyhow::bail!(
-                "Misconfiguration detected: an HTTP endpoint URL \"{}\" override is set for object \
+                None => {
+                    if options.aws_access_key_id.is_none() {
+                        let sdk_config =
+                            aws_config::defaults(BehaviorVersion::latest()).load().await;
+
+                        let region = options
+                            .aws_region
+                            .clone()
+                            .map(Region::new)
+                            .or_else(|| sdk_config.region().cloned())
+                            .context("Unable to determine AWS region")?;
+
+                        debug!(?region, "Using AWS SDK credentials provider");
+
+                        AmazonS3Builder::new()
+                            .with_region(region.to_string())
+                            .with_credentials(Arc::new(AwsSdkCredentialsProvider::new(
+                                &sdk_config,
+                            )?))
+                    } else {
+                        let region = if let Some(region) = options.aws_region.as_ref() {
+                            Region::new(region.clone())
+                        } else {
+                            DefaultRegionChain::builder()
+                                .build()
+                                .region()
+                                .await
+                                .context("Unable to determine AWS region")?
+                        };
+
+                        AmazonS3Builder::new().with_region(region.to_string())
+                    }
+                }
+            };
+
+            let builder = if let Some(region) = &options.aws_region {
+                builder.with_region(region)
+            } else {
+                builder
+            };
+            let env_allow_http_fallback = std::env::var("AWS_ALLOW_HTTP")
+                .ok()
+                .map(|s| s.trim().eq_ignore_ascii_case("true"));
+            let allow_insecure_http = options.aws_allow_http.or(env_allow_http_fallback);
+            let builder = if let Some(allow_http) = allow_insecure_http {
+                builder.with_allow_http(allow_http)
+            } else {
+                builder
+            };
+            let env_endpoint_url_fallback = std::env::var("AWS_ENDPOINT_URL_S3")
+                .ok()
+                .or_else(|| std::env::var("AWS_ENDPOINT_URL").ok());
+            let s3_endpoint = options
+                .aws_endpoint_url
+                .as_ref()
+                .or(env_endpoint_url_fallback.as_ref());
+            if !allow_insecure_http.unwrap_or_default()
+                && s3_endpoint.is_some_and(|endpoint| {
+                    Url::parse(endpoint).is_ok_and(|url| url.scheme().eq_ignore_ascii_case("http"))
+                })
+            {
+                anyhow::bail!(
+                    "Misconfiguration detected: an HTTP endpoint URL \"{}\" override is set for object \
                 store destination \"{}\", but plain HTTP is not allowed. Please set 'aws-allow-http' \
                 to `true` to enable this destination",
-                s3_endpoint.expect("is some"),
-                destination
-            );
+                    s3_endpoint.expect("is some"),
+                    url
+                );
+            }
+            let builder = if let Some(endpoint_url) = s3_endpoint {
+                builder.with_endpoint(endpoint_url)
+            } else {
+                builder
+            };
+            let builder = if let Some(access_key_id) = &options.aws_access_key_id {
+                builder.with_access_key_id(access_key_id)
+            } else {
+                builder
+            };
+            let builder = if let Some(secret_access_key) = &options.aws_secret_access_key {
+                builder.with_secret_access_key(secret_access_key)
+            } else {
+                builder
+            };
+            let builder = if let Some(token) = &options.aws_session_token {
+                builder.with_token(token)
+            } else {
+                builder
+            };
+
+            let builder = builder
+                .with_url(url)
+                .with_conditional_put(S3ConditionalPut::ETagMatch)
+                .with_retry(from_retry_policy(retry_policy));
+
+            Arc::new(builder.build()?)
         }
-        let builder = if let Some(endpoint_url) = s3_endpoint {
-            builder.with_endpoint(endpoint_url)
-        } else {
-            builder
-        };
-        let builder = if let Some(access_key_id) = &options.aws_access_key_id {
-            builder.with_access_key_id(access_key_id)
-        } else {
-            builder
-        };
-        let builder = if let Some(secret_access_key) = &options.aws_secret_access_key {
-            builder.with_secret_access_key(secret_access_key)
-        } else {
-            builder
-        };
-        let builder = if let Some(token) = &options.aws_session_token {
-            builder.with_token(token)
-        } else {
-            builder
-        };
-
-        let builder = builder
-            .with_url(destination)
-            .with_conditional_put(S3ConditionalPut::ETagMatch)
-            .with_retry(from_retry_policy(retry_policy));
-
-        Arc::new(builder.build()?)
-    } else {
-        // Since we only compile object_store with AWS support, the only other possibility is a file:// URL
-        object_store::parse_url(&destination)?.0.into()
+        ObjectStoreScheme::MicrosoftAzure => {
+            Arc::new(MicrosoftAzureBuilder::from_env().with_url(url).build()?)
+        }
+        ObjectStoreScheme::GoogleCloudStorage => Arc::new(
+            GoogleCloudStorageBuilder::from_env()
+                .with_url(url)
+                .build()?,
+        ),
+        #[cfg(any(test, feature = "test-util"))]
+        ObjectStoreScheme::Local => object_store::parse_url(&url)?.0.into(),
+        _ => bail!("Unsupported protocol: {url}"),
     };
+
     Ok(object_store)
 }
 
