214c1522734a0946f61299a3a9346145e895706f
Ahmed Farghal <me@asoli.dev>
2025-11-13 15:27:20 +0000

    [2/N][VQueues] Storage API

---
+++ crates/storage-api/src/vqueue_table/mod.rs
@@ -0,0 +1,124 @@
+// Copyright (c) 2023 - 2025 Restate Software, Inc., Restate GmbH.
+// All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+mod entry;
+pub mod metadata;
+mod store;
+mod tables;
+
+use std::hash::Hash;
+
+use restate_types::clock::UniqueTimestamp;
+use restate_types::time::MillisSinceEpoch;
+
+pub use entry::*;
+pub use store::*;
+pub use tables::*;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum VisibleAt {
+    Now,
+    At(UniqueTimestamp),
+}
+
+impl VisibleAt {
+    pub fn from_unix_millis(millis: MillisSinceEpoch) -> Self {
+        VisibleAt::At(UniqueTimestamp::from_unix_millis(millis).unwrap())
+    }
+    pub fn from_raw(raw: u64) -> Self {
+        if raw == 0 {
+            return VisibleAt::Now;
+        }
+        VisibleAt::At(UniqueTimestamp::try_from(raw).unwrap())
+    }
+
+    pub fn as_u64(&self) -> u64 {
+        match self {
+            VisibleAt::Now => 0,
+            VisibleAt::At(ts) => ts.as_u64(),
+        }
+    }
+
+    pub fn is_visible(&self, now: UniqueTimestamp) -> bool {
+        match self {
+            VisibleAt::Now => true,
+            VisibleAt::At(ts) => *ts <= now,
+        }
+    }
+}
+
+impl From<Option<MillisSinceEpoch>> for VisibleAt {
+    fn from(value: Option<MillisSinceEpoch>) -> Self {
+        value.map_or(VisibleAt::Now, VisibleAt::from_unix_millis)
+    }
+}
+
+impl PartialEq<UniqueTimestamp> for VisibleAt {
+    fn eq(&self, other: &UniqueTimestamp) -> bool {
+        match self {
+            VisibleAt::Now => false,
+            VisibleAt::At(ts) => ts == other,
+        }
+    }
+}
+
+impl PartialOrd<UniqueTimestamp> for VisibleAt {
+    fn partial_cmp(&self, other: &UniqueTimestamp) -> Option<std::cmp::Ordering> {
+        match self {
+            VisibleAt::Now => None,
+            VisibleAt::At(ts) => ts.partial_cmp(other),
+        }
+    }
+}
+
+mod bilrost_encoding {
+    use super::*;
+
+    use bilrost::Canonicity::Canonical;
+    use bilrost::encoding::{DistinguishedProxiable, ForOverwrite, Proxiable};
+    use bilrost::{Canonicity, DecodeErrorKind};
+
+    impl Proxiable for VisibleAt {
+        type Proxy = u64;
+
+        fn encode_proxy(&self) -> Self::Proxy {
+            self.as_u64()
+        }
+
+        fn decode_proxy(&mut self, proxy: Self::Proxy) -> Result<(), DecodeErrorKind> {
+            *self = Self::from_raw(proxy);
+            Ok(())
+        }
+    }
+
+    impl DistinguishedProxiable for VisibleAt {
+        fn decode_proxy_distinguished(
+            &mut self,
+            proxy: Self::Proxy,
+        ) -> Result<Canonicity, DecodeErrorKind> {
+            self.decode_proxy(proxy)?;
+            Ok(Canonical)
+        }
+    }
+
+    impl ForOverwrite<(), VisibleAt> for () {
+        fn for_overwrite() -> VisibleAt {
+            VisibleAt::Now
+        }
+    }
+
+    bilrost::empty_state_via_for_overwrite!(VisibleAt);
+
+    bilrost::delegate_proxied_encoding!(
+        use encoding (bilrost::encoding::Varint)
+        to encode proxied type (VisibleAt)
+        with general encodings including distinguished
+    );
+}
---
+++ crates/storage-api/src/vqueue_table/store.rs
@@ -0,0 +1,69 @@
+// Copyright (c) 2023 - 2025 Restate Software, Inc., Restate GmbH.
+// All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+use restate_types::vqueue::{EffectivePriority, VQueueId};
+
+use super::{EntryCard, EntryKind, VisibleAt};
+use crate::Result;
+
+/// Storage for vqueue heads (e.g., RocksDB `ready_idx`).
+pub trait VQueueStore {
+    type Item: VQueueEntry;
+    type RunningReader: VQueueCursor<Item = Self::Item> + Send + Unpin;
+    type InboxReader: VQueueCursor<Item = Self::Item> + Send + Unpin;
+
+    fn new_run_reader(&self, qid: &VQueueId) -> Self::RunningReader;
+    fn new_inbox_reader(&self, qid: &VQueueId) -> Self::InboxReader;
+}
+
+pub trait VQueueEntry: PartialOrd + PartialEq + Eq + Clone {
+    fn unique_hash(&self) -> u64;
+    fn priority(&self) -> EffectivePriority;
+    fn visible_at(&self) -> VisibleAt;
+    fn kind(&self) -> EntryKind;
+    fn is_token_held(&self) -> bool;
+}
+
+/// Iterator over vqueue entries
+pub trait VQueueCursor {
+    type Item: PartialOrd + PartialEq + Eq + Clone;
+    /// Moves the cursor to the beginning (min key) of the vqueue
+    fn seek_to_first(&mut self);
+    /// Moves the cursor to point strictly after `item`
+    fn seek_after(&mut self, qid: &VQueueId, item: &Self::Item);
+    /// Peek item without advancing the cursor
+    fn peek(&mut self) -> Result<Option<Self::Item>>;
+    /// Advancing the cursor. If this fails, the error is returned on the next call to peek()
+    fn advance(&mut self);
+}
+
+impl VQueueEntry for EntryCard {
+    #[inline(always)]
+    fn unique_hash(&self) -> u64 {
+        self.unique_hash()
+    }
+
+    fn priority(&self) -> EffectivePriority {
+        self.priority
+    }
+
+    fn visible_at(&self) -> VisibleAt {
+        self.visible_at
+    }
+
+    fn kind(&self) -> EntryKind {
+        self.kind
+    }
+
+    #[inline(always)]
+    fn is_token_held(&self) -> bool {
+        self.priority.token_held()
+    }
+}
