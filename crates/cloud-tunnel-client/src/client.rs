a6d7ffbd1d118815bebe5566cd9d3492f8f16056
Jack Kleeman <jackkleeman@gmail.com>
2025-11-19 11:23:51 +0000

    Tunnel client should let you pass in a conn (#3986)
    So we can be more flexible with https connector in the standalone client

--- crates/cloud-tunnel-client/src/client.rs
+++ crates/cloud-tunnel-client/src/client.rs
@@ -15,7 +15,7 @@ use std::future::Future;
 use std::pin::pin;
 use std::str::FromStr;
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::{Arc, LazyLock, OnceLock};
+use std::sync::{Arc, OnceLock};
 use std::time::Duration;
 
 use anyhow::Result;
@@ -24,26 +24,12 @@ use http::{Request, StatusCode, Uri};
 use http_body_util::BodyExt;
 use hyper::Response;
 use hyper::body::Incoming;
-use hyper_rustls::{ConfigBuilderExt, HttpsConnector};
-use hyper_util::client::legacy::connect::HttpConnector;
 use hyper_util::rt::TokioTimer;
-use rustls::ClientConfig;
 use tokio::sync::Notify;
 use tokio_util::sync::CancellationToken;
-use tower::Service;
 use tracing::error;
 
-pub static TLS_CLIENT_CONFIG: LazyLock<ClientConfig> = LazyLock::new(|| {
-    ClientConfig::builder_with_provider(Arc::new(rustls::crypto::aws_lc_rs::default_provider()))
-        .with_protocol_versions(rustls::DEFAULT_VERSIONS)
-        .expect("default versions are supported")
-        .with_native_roots()
-        .expect("Can load native certificates")
-        .with_no_client_auth()
-});
-
 pub struct Handler<Notify, Client> {
-    https_connector: HttpsConnector<HttpConnector>,
     inner: Arc<HandlerInner<Notify, Client>>,
 }
 
@@ -54,7 +40,6 @@ impl<N, C> Handler<N, C> {
         Client: hyper::service::Service<Request<Incoming>>,
     >(
         client: Client,
-        connect_timeout: Duration,
         environment_id: &str,
         signing_public_key: &str,
         bearer_token: &str,
@@ -64,21 +49,7 @@ impl<N, C> Handler<N, C> {
         let request_identity_key = super::request_identity::parse_public_key(signing_public_key)
             .expect("Problem validating request identity public key");
 
-        let mut http_connector = HttpConnector::new();
-        http_connector.set_nodelay(true);
-        http_connector.set_connect_timeout(Some(connect_timeout));
-        http_connector.enforce_http(false);
-        // default interval on linux is 75 secs, also use this as the start-after
-        http_connector.set_keepalive(Some(Duration::from_secs(75)));
-
-        let https_connector = hyper_rustls::HttpsConnectorBuilder::new()
-            .with_tls_config(TLS_CLIENT_CONFIG.clone())
-            .https_or_http()
-            .enable_http2()
-            .wrap_connector(http_connector);
-
         Ok(Handler {
-            https_connector,
             inner: Arc::new(HandlerInner {
                 tunnel_name: force_tunnel_name.map(OnceLock::from).unwrap_or_default(),
                 request_identity_key,
@@ -94,7 +65,6 @@ impl<N, C> Handler<N, C> {
 impl<N, C> Clone for Handler<N, C> {
     fn clone(&self) -> Self {
         Self {
-            https_connector: self.https_connector.clone(),
             inner: self.inner.clone(),
         }
     }
@@ -209,23 +179,11 @@ where
         Display + Into<Box<dyn std::error::Error + Send + Sync + 'static>> + Send + Sync + 'static,
 {
     pub async fn serve(
-        mut self,
-        tunnel_url: Uri,
+        self,
+        io: impl hyper::rt::Read + hyper::rt::Write + Unpin,
         cancel: CancellationToken,
         request_drain: CancellationToken,
     ) -> ServeError {
-        let io = tokio::select! {
-             io_result = self.https_connector.call(tunnel_url) => {
-                 match io_result {
-                     Ok(io) => io,
-                     Err(err) => return ServeError::Connection(err),
-                 }
-             }
-             _ = cancel.cancelled() => {
-                 return ServeError::ClientClosed("connecting to tunnel")
-             }
-        };
-
         let inner = self.inner.clone();
         let serve_state = Arc::new(ServeState {
             started: AtomicBool::new(false),
