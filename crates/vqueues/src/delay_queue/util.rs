b8a5018a13a657c3ee9eff7127f95be9f8ef3d77
Ahmed Farghal <me@asoli.dev>
2025-11-24 09:56:53 +0000

    Vendoring DelayQueue to enable customizations

---
+++ crates/vqueues/src/delay_queue/util.rs
@@ -0,0 +1,28 @@
+use std::time::Duration;
+
+pub(super) enum Round {
+    Up,
+    Down,
+}
+
+/// Convert a `Duration` to milliseconds, rounding up and saturating at
+/// `u64::MAX`.
+///
+/// The saturating is fine because `u64::MAX` milliseconds are still many
+/// million years.
+#[inline]
+pub(super) fn ms(duration: Duration, round: Round) -> u64 {
+    const NANOS_PER_MILLI: u32 = 1_000_000;
+    const MILLIS_PER_SEC: u64 = 1_000;
+
+    // Round up.
+    let millis = match round {
+        Round::Up => duration.subsec_nanos().div_ceil(NANOS_PER_MILLI),
+        Round::Down => duration.subsec_millis(),
+    };
+
+    duration
+        .as_secs()
+        .saturating_mul(MILLIS_PER_SEC)
+        .saturating_add(u64::from(millis))
+}
