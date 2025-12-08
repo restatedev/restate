466504b82f90ea78991be49e17049688571d817a
Ahmed Farghal <me@asoli.dev>
2025-12-08 11:37:26 +0000

    [12/N][VQueues] Partial scheduler redesign
    - Fixes Handling of invoker concurrency tokens when yielding
    - Uses a generational arena allocator (slotmap) for state storage
    - Prepares for a new budget management system (Concurrency will be replaced in later PR)
    - Initial implementation to handle throttling
    - A variety of fixes

--- crates/vqueues/src/scheduler.rs
+++ crates/vqueues/src/scheduler.rs
@@ -19,7 +19,7 @@ use smallvec::SmallVec;
 
 use restate_futures_util::concurrency::{Concurrency, Permit};
 use restate_storage_api::StorageError;
-use restate_storage_api::vqueue_table::{ScanVQueueTable, VQueueStore};
+use restate_storage_api::vqueue_table::{ScanVQueueTable, VQueueEntry, VQueueStore, WaitStats};
 use restate_types::vqueue::VQueueId;
 
 use crate::VQueueEvent;
@@ -30,12 +30,38 @@ use self::drr::DRRScheduler;
 
 mod clock;
 mod drr;
+mod eligible;
+mod queue;
 mod vqueue_state;
 
-const INLINED_SIZE: usize = vqueue_state::QUANTUM as usize;
+const INLINED_SIZE: usize = 4;
 
-#[derive(Debug)]
+slotmap::new_key_type! { struct VQueueHandle; }
+
+// Token bucket used for throttling over all vqueues
+type GlobalTokenBucket<C = gardal::TokioClock> =
+    gardal::TokenBucket<gardal::PaddedAtomicSharedStorage, C>;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ThrottleScope {
+    Global,
+    VQueue,
+}
+
+pub struct Entry<Item> {
+    pub item: Item,
+    pub stats: WaitStats,
+    pub permit: Permit,
+}
+impl<Item> Entry<Item> {
+    pub fn split(self) -> (Item, WaitStats, Permit) {
+        (self.item, self.stats, self.permit)
+    }
+}
+
 pub struct Assignments<Item> {
+    latest_run_tb_zero_time: Option<f64>,
+
     // In the overwhelming majority of cases, we will have only one segment (inbox or running)
     // and in rare cases we may have both. If we (in the future) support sending the three actions
     // on the same scheduler's poll, we'll accept to allocated in those rare cases where the three actions
@@ -46,13 +72,14 @@ pub struct Assignments<Item> {
 impl<Item> Default for Assignments<Item> {
     fn default() -> Self {
         Self {
+            latest_run_tb_zero_time: None,
             segments: smallvec::smallvec![],
         }
     }
 }
 
 impl<Item> Assignments<Item> {
-    pub fn iter(&self) -> impl ExactSizeIterator<Item = (Action, &[Item])> {
+    pub fn iter(&self) -> impl ExactSizeIterator<Item = (Action, &[Entry<Item>])> {
         self.segments
             .iter()
             .map(|segment| (segment.action, segment.items.as_slice()))
@@ -60,49 +87,46 @@ impl<Item> Assignments<Item> {
 
     pub fn into_iter_per_action(
         self,
-    ) -> impl ExactSizeIterator<Item = (Action, impl ExactSizeIterator<Item = Item>)> {
+    ) -> impl ExactSizeIterator<Item = (Action, impl ExactSizeIterator<Item = Entry<Item>>)> {
         self.segments
             .into_iter()
             .map(|segment| (segment.action, segment.items.into_iter()))
     }
 
-    pub fn push(&mut self, action: Action, item: Item) {
+    pub fn push(&mut self, action: Action, entry: Entry<Item>) {
         // manipulate the last segment if the action is the same.
         if let Some(last_segment) = self.segments.last_mut()
             && last_segment.action == action
         {
-            last_segment.items.push(item);
+            last_segment.items.push(entry);
         } else {
             self.segments.push(AssignmentSegment {
                 action,
-                items: smallvec::smallvec![item],
+                items: smallvec::smallvec![entry],
             });
         }
     }
 
-    pub fn merge(&mut self, other: Self) {
-        self.segments.extend(other.segments);
+    /// Allows us to track the token bucket's zero time across scheduler runs.
+    pub fn set_latest_run_tb_zero_time(&mut self, zero_time: Option<f64>) {
+        self.latest_run_tb_zero_time = zero_time;
     }
 
     pub fn is_empty(&self) -> bool {
         self.segments.is_empty()
     }
+
+    pub fn updated_tb_zero_time(&self) -> Option<f64> {
+        self.latest_run_tb_zero_time
+    }
 }
 
-#[derive(Debug)]
 pub struct AssignmentSegment<Item> {
     pub action: Action,
-    pub items: SmallVec<[Item; INLINED_SIZE]>,
+    pub items: SmallVec<[Entry<Item>; INLINED_SIZE]>,
 }
 
 impl<Item> AssignmentSegment<Item> {
-    pub fn new(action: Action, item: Item) -> Self {
-        Self {
-            action,
-            items: smallvec::smallvec![item],
-        }
-    }
-
     pub fn is_empty(&self) -> bool {
         self.items.is_empty()
     }
@@ -122,44 +146,89 @@ pub enum Action {
     Yield,
 }
 
-#[derive(derive_more::IntoIterator, Debug)]
-pub struct Decision<Item>(HashMap<VQueueId, Assignments<Item>>);
+#[derive(derive_more::IntoIterator, derive_more::Debug)]
+pub struct Decision<Item> {
+    #[into_iterator]
+    #[debug(skip)]
+    q: HashMap<VQueueId, Assignments<Item>>,
+    /// items running for the first time
+    num_start: u16,
+    /// running items previous started
+    num_run: u16,
+    /// running items that should continue to run
+    num_resume: u16,
+    /// Items in run queue that need to go back to waiting inbox
+    num_yield: u16,
+}
 
-impl<Item> Decision<Item> {
+impl<Item> Default for Decision<Item> {
+    fn default() -> Self {
+        Self {
+            q: HashMap::default(),
+            num_start: 0,
+            num_run: 0,
+            num_resume: 0,
+            num_yield: 0,
+        }
+    }
+}
+
+impl<Item: VQueueEntry> Decision<Item> {
+    pub fn push(
+        &mut self,
+        qid: &VQueueId,
+        action: Action,
+        entry: Entry<Item>,
+        updated_zt: Option<f64>,
+    ) {
+        let assignments = self.q.entry_ref(qid).or_default();
+        assignments.set_latest_run_tb_zero_time(updated_zt);
+        match action {
+            Action::ResumeAlreadyRunning => self.num_resume += 1,
+            Action::Yield => self.num_yield += 1,
+            Action::MoveToRunning if entry.item.priority().is_new() => self.num_start += 1,
+            Action::MoveToRunning => self.num_run += 1,
+        }
+        assignments.push(action, entry);
+    }
+
     pub fn is_empty(&self) -> bool {
-        self.0.is_empty()
+        self.q.is_empty()
+    }
+
+    /// The number of vqueues in this decision
+    pub fn num_queues(&self) -> usize {
+        self.q.len()
+    }
+
+    /// Total number of items in all queues
+    pub fn total_items(&self) -> usize {
+        self.num_start as usize
+            + self.num_run as usize
+            + self.num_resume as usize
+            + self.num_yield as usize
     }
 
     pub fn report_metrics(&self) {
-        let mut num_run = 0;
-        let mut num_resume = 0;
-        let mut num_yield = 0;
-        for segment in self
-            .0
-            .values()
-            .flat_map(|assignments| &assignments.segments)
-        {
-            let count = segment.items.len();
-            match segment.action {
-                Action::ResumeAlreadyRunning => num_resume += count,
-                Action::MoveToRunning => num_run += count,
-                Action::Yield => num_yield += count,
-            }
-        }
-        publish_scheduler_decision_metrics(num_run, num_yield, num_resume);
+        publish_scheduler_decision_metrics(
+            self.num_start,
+            self.num_run,
+            self.num_yield,
+            self.num_resume,
+        );
     }
 }
 
-enum State<S: VQueueStore, Token> {
-    Active(Pin<Box<DRRScheduler<S, Token>>>),
+enum State<S: VQueueStore> {
+    Active(Pin<Box<DRRScheduler<S>>>),
     Disabled,
 }
 
-pub struct SchedulerService<S: VQueueStore, Token> {
-    state: State<S, Token>,
+pub struct SchedulerService<S: VQueueStore> {
+    state: State<S>,
 }
 
-impl<S, Token> SchedulerService<S, Token>
+impl<S> SchedulerService<S>
 where
     S: VQueueStore,
     S::Item: std::fmt::Debug,
@@ -169,12 +238,13 @@ where
         S: ScanVQueueTable,
     {
         Self {
-            state: State::<S, Token>::Disabled,
+            state: State::<S>::Disabled,
         }
     }
 
     pub async fn create(
-        concurrency: Concurrency<Token>,
+        concurrency: Concurrency,
+        global_throttling: Option<GlobalTokenBucket>,
         storage: S,
         vqueues_cache: &mut VQueuesMetaMut,
     ) -> Result<Self, StorageError>
@@ -195,7 +265,10 @@ where
             // Note that propose_many will error out if the number of commands is greater than the
             // channel's max capacity.
             NonZeroU16::new(25).unwrap(),
+            // currently constant but we can make it configurable if needed
+            NonZeroU16::new(1000).unwrap(),
             concurrency,
+            global_throttling,
             storage,
             vqueues_cache.view(),
         )));
@@ -230,15 +303,6 @@ where
             State::Disabled => Poll::Pending,
             State::Active(ref mut drr_scheduler) => {
                 drr_scheduler.as_mut().poll_schedule_next(cx, vqueues)
-            }
-        }
-    }
-
-    pub fn confirm_assignment(&mut self, qid: &VQueueId, item_hash: u64) -> Option<Permit<Token>> {
-        match self.state {
-            State::Disabled => None,
-            State::Active(ref mut drr_scheduler) => {
-                drr_scheduler.as_mut().confirm_assignment(qid, item_hash)
             }
         }
     }
