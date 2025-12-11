0e06959837cd579de79c3294446927e8a9846f41
slinkydeveloper <me@slinkydeveloper.com>
2025-12-11 12:03:53 +0100

    [CLI] Various improvements to batch cancel/kill and co
    Summary:
    
    Assorted improvements to cancel/kill:
    
    * Use the limited concurrency map to execute concurrently the admin api commands. Limiting concurrency to 500 seems to be the way to not make it fail :shrug: There's also the mistery of 1011...
    * Don't print twice the invocations we're going to kill, there's no need. Just print the failed ones, if any. Same for pause/resume, etc.
    * Limit the amount of output too. This is a problem when killing 2milion invocations...
    * Add progress bar when reading the invocations
    * Add another progress bar when cancelling them
    * Add a limit to the batch cancel/kill and co. Mitigates #3857
    * Introduce a utility for running concurrent `map` on a stream, limiting the concurrency.

--- cli/src/commands/invocations/cancel.rs
+++ cli/src/commands/invocations/cancel.rs
@@ -7,19 +7,24 @@
 // As of the Change Date specified in that file, in accordance with
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
+
 use anyhow::{Result, anyhow, bail};
 use cling::prelude::*;
 use comfy_table::{Cell, Color, Table};
-use futures::TryFutureExt;
 use restate_cli_util::ui::console::{Styled, StyledTable, confirm_or_exit};
 use restate_cli_util::ui::stylesheet::Style;
-use restate_cli_util::{c_error, c_indent_table, c_println, c_success, c_warn};
+use restate_cli_util::{c_indent_table, c_println, c_success, c_warn};
 
 use crate::cli_env::CliEnv;
-use crate::clients::datafusion_helpers::{InvocationState, find_active_invocations_simple};
-use crate::clients::{self, AdminClientInterface, collect_and_split_futures};
-use crate::commands::invocations::create_query_filter;
+use crate::clients::batch_execute;
+use crate::clients::datafusion_helpers::find_active_invocations_simple;
+use crate::clients::{self, AdminClientInterface};
+use crate::commands::invocations::{
+    DEFAULT_BATCH_INVOCATIONS_OPERATION_LIMIT, DEFAULT_BATCH_INVOCATIONS_OPERATION_PRINT_LIMIT,
+    create_query_filter,
+};
 use crate::ui::invocations::render_simple_invocation_list;
+use crate::ui::with_progress;
 
 #[derive(Run, Parser, Collect, Clone)]
 #[cling(run = "run_cancel")]
@@ -38,6 +43,9 @@ pub struct Cancel {
     /// Ungracefully kill the invocation and its children
     #[clap(long)]
     pub(super) kill: bool,
+    /// Limit the number of fetched invocations
+    #[clap(long, default_value_t = DEFAULT_BATCH_INVOCATIONS_OPERATION_LIMIT)]
+    pub(super) limit: usize,
 }
 
 pub async fn run_cancel(State(env): State<CliEnv>, opts: &Cancel) -> Result<()> {
@@ -45,11 +53,16 @@ pub async fn run_cancel(State(env): State<CliEnv>, opts: &Cancel) -> Result<()> 
     let sql_client = clients::DataFusionHttpClient::from(client.clone());
 
     let filter = format!(
-        "{} AND status != 'completed'",
-        create_query_filter(&opts.query)
+        "{} AND status != 'completed' LIMIT {}",
+        create_query_filter(&opts.query),
+        opts.limit
     );
 
-    let invocations = find_active_invocations_simple(&sql_client, &filter).await?;
+    let invocations = with_progress(
+        "Reading invocations...",
+        find_active_invocations_simple(&sql_client, &filter),
+    )
+    .await?;
     if invocations.is_empty() {
         bail!(
             "No invocations found for query {}! Note that the cancel command only works on non-completed invocations. \
@@ -58,19 +71,10 @@ pub async fn run_cancel(State(env): State<CliEnv>, opts: &Cancel) -> Result<()> 
         );
     };
 
-    render_simple_invocation_list(&invocations);
-
-    if invocations
-        .iter()
-        .all(|inv| matches!(inv.status, InvocationState::Completed))
-    {
-        // Should only happen with explicit invocation ids, as we filter the completed state out otherwise
-        c_error!(
-            "The invocations matching your query are completed; cancel/kill has no effect on them. \
-            If you want to remove a completed invocation, consider using the purge command instead.",
-        );
-        return Ok(());
-    }
+    render_simple_invocation_list(
+        &invocations,
+        DEFAULT_BATCH_INVOCATIONS_OPERATION_PRINT_LIMIT,
+    );
 
     // Get the invocation and confirm
     let prompt = format!(
@@ -86,28 +90,18 @@ pub async fn run_cancel(State(env): State<CliEnv>, opts: &Cancel) -> Result<()> 
     if opts.kill {
         // Kill invocations
         let (killed, failed_to_kill) =
-            collect_and_split_futures(invocations.into_iter().map(|invocation| invocation.id).map(
-                |invocation_id| async {
-                    client
-                        .kill_invocation(&invocation_id)
-                        .map_err(anyhow::Error::from)
-                        .await
-                        .map(|_| invocation_id.clone())
-                        .map_err(|e| (invocation_id, e))
-                },
-            ))
+            batch_execute(client, invocations, |client, invocation| async move {
+                client
+                    .kill_invocation(&invocation.id)
+                    .await
+                    .map_err(anyhow::Error::from)
+            })
             .await;
+        let succeeded_count = killed.len();
+        let failed_count = failed_to_kill.len();
 
         c_println!();
-        c_success!("Killed invocations:");
-
-        // Print success
-        let mut invocations_table = Table::new_styled();
-        invocations_table.set_styled_header(vec!["KILLED INVOCATIONS"]);
-        for id in killed {
-            invocations_table.add_row(vec![Cell::new(&id)]);
-        }
-        c_indent_table!(0, invocations_table);
+        c_success!("Killed {} invocations", succeeded_count);
 
         // Print failed ones, if any
         if !failed_to_kill.is_empty() {
@@ -115,39 +109,35 @@ pub async fn run_cancel(State(env): State<CliEnv>, opts: &Cancel) -> Result<()> 
             c_warn!("Failed to kill:");
             let mut failed_to_kill_table = Table::new_styled();
             failed_to_kill_table.set_styled_header(vec!["ID", "REASON"]);
-            for (id, reason) in failed_to_kill {
-                failed_to_kill_table
-                    .add_row(vec![Cell::new(&id), Cell::new(reason).fg(Color::DarkRed)]);
+            for (inv, reason) in failed_to_kill {
+                failed_to_kill_table.add_row(vec![
+                    Cell::new(&inv.id),
+                    Cell::new(reason).fg(Color::DarkRed),
+                ]);
             }
             c_indent_table!(0, failed_to_kill_table);
 
-            return Err(anyhow!("Failed to kill some invocations"));
+            return Err(anyhow!(
+                "Failed to kill {} invocations out of {}",
+                failed_count,
+                failed_count + succeeded_count
+            ));
         }
     } else {
         // Cancel invocations
         let (cancelled, failed_to_cancel) =
-            collect_and_split_futures(invocations.into_iter().map(|invocation| invocation.id).map(
-                |invocation_id| async {
-                    client
-                        .cancel_invocation(&invocation_id)
-                        .map_err(anyhow::Error::from)
-                        .await
-                        .map(|_| invocation_id.clone())
-                        .map_err(|e| (invocation_id, e))
-                },
-            ))
+            batch_execute(client, invocations, |client, invocation| async move {
+                client
+                    .cancel_invocation(&invocation.id)
+                    .await
+                    .map_err(anyhow::Error::from)
+            })
             .await;
+        let succeeded_count = cancelled.len();
+        let failed_count = failed_to_cancel.len();
 
         c_println!();
-        c_success!("Cancelled invocations:");
-
-        // Print success
-        let mut invocations_table = Table::new_styled();
-        invocations_table.set_styled_header(vec!["CANCELLED INVOCATIONS"]);
-        for id in cancelled {
-            invocations_table.add_row(vec![Cell::new(&id)]);
-        }
-        c_indent_table!(0, invocations_table);
+        c_success!("Cancelled {} invocations", succeeded_count);
 
         // Print failed ones, if any
         if !failed_to_cancel.is_empty() {
@@ -155,13 +145,19 @@ pub async fn run_cancel(State(env): State<CliEnv>, opts: &Cancel) -> Result<()> 
             c_warn!("Failed to cancel:");
             let mut failed_to_cancel_table = Table::new_styled();
             failed_to_cancel_table.set_styled_header(vec!["ID", "REASON"]);
-            for (id, reason) in failed_to_cancel {
-                failed_to_cancel_table
-                    .add_row(vec![Cell::new(&id), Cell::new(reason).fg(Color::DarkRed)]);
+            for (inv, reason) in failed_to_cancel {
+                failed_to_cancel_table.add_row(vec![
+                    Cell::new(&inv.id),
+                    Cell::new(reason).fg(Color::DarkRed),
+                ]);
             }
             c_indent_table!(0, failed_to_cancel_table);
 
-            return Err(anyhow!("Failed to cancel some invocations"));
+            return Err(anyhow!(
+                "Failed to cancel {} invocations out of {}",
+                failed_count,
+                failed_count + succeeded_count
+            ));
         }
     }
 
---
+++ crates/futures-util/src/streams.rs
@@ -0,0 +1,61 @@
+// Copyright (c) 2023 - 2025 Restate Software, Inc., Restate GmbH.
+// All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+use async_channel::Receiver;
+use futures::Stream;
+use std::ops::Fn;
+use std::pin::pin;
+
+pub trait StreamExt
+where
+    Self: Stream,
+{
+    fn concurrent_map_unordered<T, Ctx, Fun, Fut>(
+        self,
+        concurrency_limit: usize,
+        ctx: Ctx,
+        fun: Fun,
+    ) -> Receiver<T>
+    where
+        T: Send + 'static,
+        Ctx: Send + Clone + 'static,
+        Fun: Fn(Ctx, Self::Item) -> Fut + Copy + Send + 'static,
+        Fut: Future<Output = T> + Send,
+        Self: Sized + Send + 'static,
+        <Self as Stream>::Item: Send + 'static,
+    {
+        let (in_tx, in_rx) = async_channel::bounded(concurrency_limit);
+        let (out_tx, out_rx) = async_channel::unbounded();
+
+        for _worker in 0..concurrency_limit {
+            let rx = in_rx.clone();
+            let tx = out_tx.clone();
+            let ctx = ctx.clone();
+            tokio::spawn(async move {
+                while let Ok(item) = rx.recv().await {
+                    let res = fun(ctx.clone(), item).await;
+                    tx.send(res).await.unwrap();
+                }
+            });
+        }
+
+        tokio::spawn(async move {
+            let mut this = pin!(self);
+            while let Some(item) = futures::StreamExt::next(&mut this).await {
+                in_tx.send(item).await.unwrap();
+            }
+            in_tx.close();
+        });
+
+        out_rx
+    }
+}
+
+impl<S> StreamExt for S where S: Stream {}
