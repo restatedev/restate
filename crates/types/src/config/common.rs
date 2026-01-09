1ae592fd135afc9fbfe690871ed188074a38392f
Ahmed Farghal <me@asoli.dev>
2026-01-09 10:03:04 +0000

    [minor] Remove preview status from node-location

--- crates/types/src/config/common.rs
+++ crates/types/src/config/common.rs
@@ -206,7 +206,6 @@ pub struct CommonOptions {
 
     /// # Node Location
     ///
-    /// [PREVIEW FEATURE]
     /// Setting the location allows Restate to form a tree-like cluster topology.
     /// The value is written in the format of "region[.zone]" to assign this node
     /// to a specific region, or to a zone within a region.
