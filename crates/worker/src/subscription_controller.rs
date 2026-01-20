09ae06f49310dbe855323157ff8033f17eaf575e
slinkydeveloper <me@slinkydeveloper.com>
2026-01-20 15:49:07 +0100

    Support new KafkaCluster data-model in subscription controller

--- crates/worker/src/subscription_controller.rs
+++ crates/worker/src/subscription_controller.rs
@@ -10,7 +10,7 @@
 
 use std::future::Future;
 
-use restate_types::identifiers::SubscriptionId;
+use restate_types::schema::kafka::KafkaCluster;
 use restate_types::schema::subscriptions::Subscription;
 
 use crate::WorkerHandleError;
@@ -18,19 +18,11 @@ use crate::WorkerHandleError;
 // This is just an interface to isolate the interaction between meta and subscription controller.
 // Depending on how we evolve the Kafka ingress deployment, this might end up living in a separate process.
 pub trait SubscriptionController {
-    fn start_subscription(
-        &self,
-        subscription: Subscription,
-    ) -> impl Future<Output = Result<(), WorkerHandleError>> + Send;
-    fn stop_subscription(
-        &self,
-        id: SubscriptionId,
-    ) -> impl Future<Output = Result<(), WorkerHandleError>> + Send;
-
     /// Updates the subscription controller with the provided set of subscriptions. The subscription controller
     /// is supposed to only run the set of provided subscriptions after this call succeeds.
     fn update_subscriptions(
         &self,
+        kafka_clusters: Vec<KafkaCluster>,
         subscriptions: Vec<Subscription>,
     ) -> impl Future<Output = Result<(), WorkerHandleError>> + Send;
 }
