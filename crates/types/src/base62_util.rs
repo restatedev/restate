c8984746627870aa040f0707e244b3e2d347d321
Jack Kleeman <jackkleeman@gmail.com>
2026-01-30 17:40:51 +0000

    ID encoding performance boost (#4253)
    * Include forked base62
    
    * Speed up digit_count in base62_util

--- crates/types/src/base62_util.rs
+++ crates/types/src/base62_util.rs
@@ -11,7 +11,7 @@
 //! Note: We use base62 [0-9a-zA-Z], that's why we use encode_alternative functions on
 //! base62 crate. Otherwise, by default it uses [0-9A-Za-z].
 
-use std::mem::size_of;
+use std::{mem::size_of, num::NonZeroU128};
 
 const BITS_PER_BASE62_CHAR: usize = 6;
 const BITS_PER_BYTE: usize = 8;
@@ -39,35 +39,47 @@ const BASE_TO_20: u128 = BASE_TO_19 * BASE as u128;
 const BASE_TO_21: u128 = BASE_TO_20 * BASE as u128;
 
 fn digit_count(n: u128) -> usize {
-    const POWERS: [u128; 22] = [
+    const THRESHOLDS: [u128; 23] = [
         0,
-        BASE as u128,
-        BASE_TO_2 as u128,
-        BASE_TO_3 as u128,
-        BASE_TO_4 as u128,
-        BASE_TO_5 as u128,
-        BASE_TO_6 as u128,
-        BASE_TO_7 as u128,
-        BASE_TO_8 as u128,
-        BASE_TO_9 as u128,
-        BASE_TO_10,
-        BASE_TO_11,
-        BASE_TO_12,
-        BASE_TO_13,
-        BASE_TO_14,
-        BASE_TO_15,
-        BASE_TO_16,
-        BASE_TO_17,
-        BASE_TO_18,
-        BASE_TO_19,
-        BASE_TO_20,
-        BASE_TO_21,
+        BASE as u128 - 1,
+        BASE_TO_2 as u128 - 1,
+        BASE_TO_3 as u128 - 1,
+        BASE_TO_4 as u128 - 1,
+        BASE_TO_5 as u128 - 1,
+        BASE_TO_6 as u128 - 1,
+        BASE_TO_7 as u128 - 1,
+        BASE_TO_8 as u128 - 1,
+        BASE_TO_9 as u128 - 1,
+        BASE_TO_10 - 1,
+        BASE_TO_11 - 1,
+        BASE_TO_12 - 1,
+        BASE_TO_13 - 1,
+        BASE_TO_14 - 1,
+        BASE_TO_15 - 1,
+        BASE_TO_16 - 1,
+        BASE_TO_17 - 1,
+        BASE_TO_18 - 1,
+        BASE_TO_19 - 1,
+        BASE_TO_20 - 1,
+        BASE_TO_21 - 1,
+        u128::MAX, // sentinel, u128 cannot be larger than this value
     ];
 
-    match POWERS.binary_search(&n) {
-        Ok(n) => n.wrapping_add(1),
-        Err(n) => n,
-    }
+    let Some(n) = NonZeroU128::new(n) else {
+        return 1;
+    };
+    // We want to find floor(log62(n)) + 1 = floor(log2(n) / log2(62)) + 1
+    // First, approximate log2(n) with ilog2 = floor(log2(n)), underestimating by 0 <= err < 1
+    let ilog2 = n.ilog2() as usize;
+
+    // Next, we find floor(ilog2/log2(62)) + 1, which is exactly equal to floor(ilog2 * 43/256) + 1 for all ilog2 in [0, 127]
+    // The result is an underestimate by up to 1, purely because ilog2 is an underestimate
+    let estimate = ((ilog2 * 43) >> 8) + 1;
+
+    // SAFETY: estimate is in [1,22] since ilog2 is in [0,127] and (127*43)>>8 + 1 = 22
+    let threshold = unsafe { *THRESHOLDS.get_unchecked(estimate) };
+    let bump = (n.get() > threshold) as usize;
+    estimate + bump
 }
 
 /// Encodes a u64 into base62 string, this function offsets the string with
