937e9a51685b6c5571f0718677e769d450a7d7f5
Ahmed Farghal <me@asoli.dev>
2026-04-24 12:39:56 +0100

    [VQueues] Concrete implementation of invoker throttling
    I'm not loving this, but it works well enough to unblock.

--- crates/vqueues/src/lib.rs
+++ crates/vqueues/src/lib.rs
@@ -16,9 +16,7 @@ mod util;
 // Re-exports
 pub use cache::{VQueuesMeta, VQueuesMetaCache};
 pub use metric_definitions::describe_metrics;
-pub use restate_worker_api::{
-    ResourceKind, SchedulingStatus, ThrottleScope, VQueueSchedulerStatus,
-};
+pub use restate_worker_api::{ResourceKind, SchedulingStatus, VQueueSchedulerStatus};
 pub use scheduler::{ResourceManager, SchedulerService};
 use smallvec::SmallVec;
 use tracing::debug;
