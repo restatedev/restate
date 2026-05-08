f8ac6262964acdafc700d9a381a61c7a81e68dd7
Muhamad Awad <muhamad@restate.dev>
2026-05-08 10:41:15 +0200

    wip: write v2 envelope

--- crates/worker/src/partition/rpc/resume_invocation.rs
+++ crates/worker/src/partition/rpc/resume_invocation.rs
@@ -8,17 +8,19 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
-use super::*;
 use restate_storage_api::invocation_status_table::{
     InFlightInvocationMetadata, InvocationStatus, ReadInvocationStatusTable,
 };
-use restate_types::identifiers::{InvocationId, WithPartitionKey};
+use restate_types::identifiers::InvocationId;
 use restate_types::invocation::client::PatchDeploymentId;
 use restate_types::invocation::{
     IngressInvocationResponseSink, InvocationMutationResponseSink, ResumeInvocationRequest,
 };
 use restate_types::net::partition_processor::ResumeInvocationRpcResponse;
 use restate_types::schema::deployment::DeploymentResolver;
+use restate_wal_protocol::v2::{RecordWithKeys, records};
+
+use super::*;
 
 pub(super) struct Request {
     pub(super) request_id: PartitionProcessorRpcRequestId,
@@ -127,8 +129,7 @@ where
                 // We need to propose the message, PP will deal with invoking this back
                 self.proposer
                     .handle_rpc_proposal_command(
-                        invocation_id.partition_key(),
-                        Command::ResumeInvocation(ResumeInvocationRequest {
+                        records::ResumeInvocation::partial(ResumeInvocationRequest {
                             invocation_id,
                             update_pinned_deployment_id,
                             response_sink: Some(InvocationMutationResponseSink::Ingress(
@@ -165,7 +166,6 @@ mod tests {
     use super::*;
 
     use crate::partition::rpc::MockActuator;
-    use assert2::let_assert;
     use futures::FutureExt;
     use googletest::prelude::*;
     use restate_storage_api::invocation_status_table::{
@@ -179,6 +179,7 @@ mod tests {
     use restate_types::schema::deployment::Deployment;
     use restate_types::schema::deployment::test_util::MockDeploymentMetadataRegistry;
     use restate_types::service_protocol::ServiceProtocolVersion;
+    use restate_wal_protocol::v2::RecordKind;
     use rstest::rstest;
     use std::future::ready;
     use test_log::test;
@@ -252,8 +253,10 @@ mod tests {
         proposer.expect_is_leader().return_const(true);
         proposer
             .expect_handle_rpc_proposal_command::<ResumeInvocationRpcResponse>()
-            .return_once_st(move |_, cmd, request_id, replier| {
-                let_assert!(Command::ResumeInvocation(resume_invocation_request) = cmd);
+            .return_once_st(move |cmd, request_id, replier| {
+                assert_eq!(cmd.kind(), RecordKind::ResumeInvocation);
+                let resume_invocation_request: ResumeInvocationRequest =
+                    cmd.unwrap::<records::ResumeInvocation>().into();
                 assert_that!(
                     resume_invocation_request,
                     all!(
@@ -511,16 +514,19 @@ mod tests {
         proposer.expect_is_leader().return_const(true);
         proposer
             .expect_handle_rpc_proposal_command::<ResumeInvocationRpcResponse>()
-            .return_once_st(move |_, cmd, request_id, replier| {
+            .return_once_st(move |cmd, request_id, replier| {
+                assert_eq!(cmd.kind(), RecordKind::ResumeInvocation);
+                let resume_invocation_request: ResumeInvocationRequest =
+                    cmd.unwrap::<records::ResumeInvocation>().into();
                 assert_that!(
-                    cmd,
-                    pat!(Command::ResumeInvocation(pat!(ResumeInvocationRequest {
+                    resume_invocation_request,
+                    pat!(ResumeInvocationRequest {
                         invocation_id: eq(invocation_id),
                         update_pinned_deployment_id: some(eq(expected_deployment_id)),
                         response_sink: some(eq(InvocationMutationResponseSink::Ingress(
                             IngressInvocationResponseSink { request_id }
                         )))
-                    })))
+                    })
                 );
                 replier.send(ResumeInvocationRpcResponse::Ok);
                 ready(()).boxed()
