19a211d9c11e465f01eb0a2e00dbee3e711fbf1f
Ahmed Farghal <me@asoli.dev>
2026-06-24 10:36:10 +0100

    [VQueues] sys_scheduler better blocked_on field(s)
    - `blocked_on` now contains a kebab-cased string of the resource name only (e.g. `limit-key-concurrency`).
    - Adds `blocked_on_json` field which contains json-encoded details of the blocked resource (including `resource`: `limit-key-concurrency`) to aid with automated parsing of the blocked resource details.
    
    Example:
    
    ```
    ~/w/r/restate ❯❯❯ restate sql --environment=local  "select * from sys_scheduler where id='vq_1hUs0qb6BgUS7jetvBb7A1Kqj3UfWTYzLw'" --json | jq
    1 rows. Query took 3.192333ms
    [
      {
        "partition_key": 6795114440974114256,
        "id": "vq_1hUs0qb6BgUS7jetvBb7A1Kqj3UfWTYzLw",
        "num_inbox": 7429,
        "status": "blocked",
        "head_entry_id": "inv_1hUs0qb6BgUS45EsK1SsuqfhMsn2DJd1Qc",
        "blocked_on": "limit-key-concurrency",
        "blocked_on_json": "{\"resource\":\"limit-key-concurrency\",\"scope\":\"my-user\",\"limit_key\":\"\",\"blocked_level\":\"scope\",\"blocked_rule\":\"my-user\"}",
        "invoker_concurrency_block_duration": "P0D",
        "throttling_rules_block_duration": "P0D",
        "invoker_throttling_block_duration": "P0D",
        "invoker_memory_block_duration": "P0D",
        "concurrency_rules_block_duration": "PT1.239S",
        "lock_block_duration": "P0D",
        "deployment_concurrency_block_duration": "P0D"
      }
    ]
    ```

--- crates/storage-query-datafusion/src/scheduler_status/schema.rs
+++ crates/storage-query-datafusion/src/scheduler_status/schema.rs
@@ -24,7 +24,7 @@ define_table!(sys_scheduler(
     /// Number of entries currently waiting in the inbox stage.
     num_inbox: DataType::UInt64,
 
-    /// High-level scheduler state (Dormant, Ready, BlockedOn, ...).
+    /// High-level scheduler state (dormant, blocked, scheduled, ...).
     status: DataType::Utf8,
 
     /// Identifier of the head entry if the scheduler has already advanced to it.
@@ -36,8 +36,11 @@ define_table!(sys_scheduler(
     /// Set only when status is `Scheduled`.
     scheduled_at: TimestampMillisecond,
 
-    /// Detailed blocking resource when status is `BlockedOn`.
+    /// The current resource blocking the queue from dequeuing.
     blocked_on: DataType::Utf8,
+
+    /// Detailed blocking resource when status is `blocked`.
+    blocked_on_json: DataType::Utf8,
 
     /// Time the head entry spent waiting on global invoker concurrency.
     invoker_concurrency_block_duration: DataType::Duration,
