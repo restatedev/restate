ad0dc33ac9c833b97038c9626ad876b569157f3a
Muhamad Awad <muhamad@restate.dev>
2026-07-27 14:35:07 +0300

    Ingress Integration API
    ## Summary
    
    - Implement the new ingress integration grpc API
    - Add configuration
    
    
    Fixes #5066

--- crates/ingress-http/src/integration/integration_svc.rs
+++ crates/ingress-http/src/integration/integration_svc.rs
@@ -106,11 +106,16 @@ use crate::metric_definitions::INTEGRATION_INGESTED;
 pub(crate) fn integration_server<T, Schemas>(
     ingestion_client: IngestionClient<T, Envelope>,
     schemas: Live<Schemas>,
+    max_window_size: NonZeroU64,
 ) -> IntegrationSvcServer<IntegrationService<T, Schemas>>
 where
     Schemas: InvocationTargetResolver + Clone + Send + Sync + 'static,
 {
-    IntegrationSvcServer::new(IntegrationService::new(ingestion_client, schemas))
+    IntegrationSvcServer::new(IntegrationService::new(
+        ingestion_client,
+        schemas,
+        max_window_size,
+    ))
 }
 
 /// Window-replenishment threshold, as a percentage of the maximum window size.
@@ -131,13 +136,19 @@ const UPDATE_WINDOW_THRESHOLD: u64 = 20; // 20% of max window size
 pub(crate) struct IntegrationService<T, Schemas> {
     ingestion_client: IngestionClient<T, Envelope>,
     schemas: Live<Schemas>,
+    max_window_size: NonZeroU64,
 }
 
 impl<T, Schemas> IntegrationService<T, Schemas> {
-    fn new(ingestion_client: IngestionClient<T, Envelope>, schemas: Live<Schemas>) -> Self {
+    fn new(
+        ingestion_client: IngestionClient<T, Envelope>,
+        schemas: Live<Schemas>,
+        max_window_size: NonZeroU64,
+    ) -> Self {
         Self {
             ingestion_client,
             schemas,
+            max_window_size,
         }
     }
 }
@@ -161,6 +172,7 @@ where
             request.into_inner(),
             self.ingestion_client.clone(),
             self.schemas.clone(),
+            self.max_window_size,
         );
         let response = futures::stream::unfold(stream, IngestionStream::step);
 
@@ -201,13 +213,14 @@ where
         inbound: S,
         ingestion_client: IngestionClient<T, Envelope>,
         schemas: Live<Schemas>,
+        max_window_size: NonZeroU64,
     ) -> Self {
         Self {
             inbound,
             ingestion_client,
             schemas,
             state: State::WaitingStart,
-            max_window_size: NonZeroU64::new(128 * 1024).unwrap(), // 128KiB
+            max_window_size,
         }
     }
 
@@ -352,7 +365,6 @@ where
             return Err(Status::invalid_argument("expecting a Start message"));
         };
 
-        // todo: verify settings if set
         let settings = start.settings;
         if let Some(ref settings) = settings {
             self.validate_settings(settings)
