17c31d0b7d1fc3d85bcab9f0913bb68b63c81e59
Muhamad Awad <muhamad@restate.dev>
2026-08-10 12:20:30 +0300

    envelope-v2: Move `ingestion` to use V2
    Switch the code paths that uses IngestionClient to start ingesting
    v2 Envelope instead of v1

--- crates/worker/src/partition_processor_manager/spawn_processor_task.rs
+++ crates/worker/src/partition_processor_manager/spawn_processor_task.rs
@@ -24,7 +24,7 @@ use restate_platform::prelude::ReString;
 use restate_types::cluster::cluster_state::PartitionProcessorStatus;
 use restate_types::logs::Lsn;
 use restate_types::partitions::Partition;
-use restate_wal_protocol::Envelope;
+use restate_wal_protocol::v2::{Envelope, Raw};
 
 use crate::PartitionProcessorBuilder;
 use crate::partition::NodeContext;
@@ -37,7 +37,7 @@ pub struct SpawnPartitionProcessorTask<T> {
     partition: Partition,
     partition_store_manager: Arc<PartitionStoreManager>,
     fast_forward_lsn: Option<Lsn>,
-    ingestion_client: IngestionClient<T, Envelope>,
+    ingestion_client: IngestionClient<T, Envelope<Raw>>,
 }
 
 impl<T> SpawnPartitionProcessorTask<T>
@@ -50,7 +50,7 @@ where
         partition: Partition,
         partition_store_manager: Arc<PartitionStoreManager>,
         fast_forward_lsn: Option<Lsn>,
-        ingestion_client: IngestionClient<T, Envelope>,
+        ingestion_client: IngestionClient<T, Envelope<Raw>>,
     ) -> Self {
         Self {
             node_ctx,
