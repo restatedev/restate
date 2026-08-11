9065a7fb6a8efab3b9a6c8f57c5cc48670508369
Muhamad Awad <muhamad@restate.dev>
2026-08-11 11:07:22 +0200

    Ingestion API implementation
    Summary:
    Implement the grpc ingestion endpoint
    
    Fixes #5066

--- crates/types/src/config/common.rs
+++ crates/types/src/config/common.rs
@@ -840,6 +840,13 @@ experimental! {
     ///
     /// Since v1.7.5
     vqueues_migration_skip_completed,
+
+    /// # Enables the new invocation::Source::Integration
+    ///
+    /// This new source can be set by the ingestion API.
+    ///
+    /// Since v1.8.0
+    invocation_source_integration,
 }
 
 serde_with::with_prefix!(pub prefix_tokio_console "tokio_console_");
