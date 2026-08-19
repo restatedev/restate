5a9d89bf3b57183803a9d86edfaaf8fe39844d52
Muhamad Awad <muhamad@restate.dev>
2026-08-19 13:40:33 +0200

    Write `Reference` response result
    Enable writing reference response result
    if experimental feature is enabled

--- crates/types/src/config/common.rs
+++ crates/types/src/config/common.rs
@@ -863,7 +863,17 @@ experimental! {
     /// already cached by RocksDB.
     ///
     /// Since v1.7.9
+
     vqueues_async_refill,
+
+    /// # Write invocation response result using reference
+    ///
+    /// Instead of embedding the response result (which can be huge)
+    /// in the invocation status, we instead keep a reference to the
+    /// output journal entry.
+    ///
+    /// Since v1.8.0
+    write_result_reference,
 }
 
 serde_with::with_prefix!(pub prefix_tokio_console "tokio_console_");
