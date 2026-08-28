ebeba810771ea73fc82ab79c87c0617d9001043e
Muhamad Awad <muhamad@restate.dev>
2026-08-28 13:36:43 +0200

    Killed state also hold a reference
    This will allow killed output entry to be retained
    correctly on journal purge

--- crates/storage-api/src/invocation_status_table/mod.rs
+++ crates/storage-api/src/invocation_status_table/mod.rs
@@ -765,10 +765,10 @@ impl ResponseResultRef {
     /// Returns Some(index) only if the response result is a reference
     /// to an output journal. If embedded returns None
     pub fn referenced_journal_index(&self) -> Option<EntryIndex> {
-        if let Self::Completed(CompletionReference { entry_index, .. }) = self {
-            Some(*entry_index)
-        } else {
-            None
+        match self {
+            Self::Completed(CompletionReference { entry_index, .. }) => Some(*entry_index),
+            Self::Killed(entry_index) => Some(*entry_index),
+            Self::Success(_) | Self::Failure(_) => None,
         }
     }
 }
