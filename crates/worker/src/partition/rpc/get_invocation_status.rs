7fe2271052f68e3acad8bd5b18db844404ce3d15
Muhamad Awad <muhamad@restate.dev>
2026-09-03 08:33:56 +0200

    Fix rebase issue

--- crates/worker/src/partition/rpc/get_invocation_status.rs
+++ crates/worker/src/partition/rpc/get_invocation_status.rs
@@ -9,13 +9,16 @@
 // by the Apache License, Version 2.0.
 
 use super::*;
+use restate_storage_api::StorageError;
 use restate_storage_api::invocation_status_table::{
-    CompletedInvocation, InvocationStatus, ReadInvocationStatusTable,
+    CompletedInvocation, CompletionStatus, InvocationStatus, ReadInvocationStatusTable,
+    ResponseResultRef,
 };
 use restate_types::invocation::{ResponseResult, client};
 use restate_types::net::partition_processor::{
     PartitionProcessorRpcError, PartitionProcessorRpcResponse,
 };
+use tracing::warn;
 
 pub(super) struct Request {
     pub(super) invocation_id: InvocationId,
@@ -23,7 +26,7 @@ pub(super) struct Request {
 
 impl<'a, TSchemas, Storage> RpcHandler<Request> for RpcContext<'a, TSchemas, Storage>
 where
-    Storage: ReadInvocationStatusTable,
+    Storage: ReadInvocationStatusTable + ReadOutputTable,
 {
     async fn handle(self, Request { invocation_id }: Request) -> Decision {
         if !self.is_leader {
@@ -33,57 +36,94 @@ where
         }
 
         Decision::Reply(
-            self.storage
-                .get_invocation_status(&invocation_id)
+            handle(self.storage, &invocation_id)
                 .await
-                .map(|invocation_status| match invocation_status {
-                    InvocationStatus::Scheduled(_) => {
-                        PartitionProcessorRpcResponse::Status(client::InvocationStatus {
-                            state: client::InvocationState::Scheduled,
-                            error: None,
-                        })
-                    }
-                    InvocationStatus::Inboxed(_) => {
-                        PartitionProcessorRpcResponse::Status(client::InvocationStatus {
-                            state: client::InvocationState::Inboxed,
-                            error: None,
-                        })
-                    }
-                    InvocationStatus::Invoked(_) => {
-                        PartitionProcessorRpcResponse::Status(client::InvocationStatus {
-                            state: client::InvocationState::Invoked,
-                            error: None,
-                        })
-                    }
-                    InvocationStatus::Suspended { .. } => {
-                        PartitionProcessorRpcResponse::Status(client::InvocationStatus {
-                            state: client::InvocationState::Suspended,
-                            error: None,
-                        })
-                    }
-                    InvocationStatus::Paused(_) => {
-                        PartitionProcessorRpcResponse::Status(client::InvocationStatus {
-                            state: client::InvocationState::Paused,
-                            error: None,
-                        })
-                    }
-                    InvocationStatus::Completed(CompletedInvocation {
-                        response_result: ResponseResult::Success(_),
-                        ..
-                    }) => PartitionProcessorRpcResponse::Status(client::InvocationStatus {
-                        state: client::InvocationState::Succeeded,
-                        error: None,
-                    }),
-                    InvocationStatus::Completed(CompletedInvocation {
-                        response_result: ResponseResult::Failure(error),
-                        ..
-                    }) => PartitionProcessorRpcResponse::Status(client::InvocationStatus {
-                        state: client::InvocationState::Failed,
-                        error: Some(error),
-                    }),
-                    InvocationStatus::Free => PartitionProcessorRpcResponse::NotFound,
-                })
                 .map_err(|err| PartitionProcessorRpcError::Internal(err.to_string())),
         )
     }
+}
+
+async fn handle<S>(
+    storage: &mut S,
+    invocation_id: &InvocationId,
+) -> Result<PartitionProcessorRpcResponse, StorageError>
+where
+    S: ReadInvocationStatusTable + ReadOutputTable,
+{
+    let invocation_status = storage.get_invocation_status(invocation_id).await?;
+
+    let response = match invocation_status {
+        InvocationStatus::Scheduled(_) => {
+            PartitionProcessorRpcResponse::Status(client::InvocationStatus {
+                state: client::InvocationState::Scheduled,
+                error: None,
+            })
+        }
+        InvocationStatus::Inboxed(_) => {
+            PartitionProcessorRpcResponse::Status(client::InvocationStatus {
+                state: client::InvocationState::Inboxed,
+                error: None,
+            })
+        }
+        InvocationStatus::Invoked(_) => {
+            PartitionProcessorRpcResponse::Status(client::InvocationStatus {
+                state: client::InvocationState::Invoked,
+                error: None,
+            })
+        }
+        InvocationStatus::Suspended { .. } => {
+            PartitionProcessorRpcResponse::Status(client::InvocationStatus {
+                state: client::InvocationState::Suspended,
+                error: None,
+            })
+        }
+        InvocationStatus::Paused(_) => {
+            PartitionProcessorRpcResponse::Status(client::InvocationStatus {
+                state: client::InvocationState::Paused,
+                error: None,
+            })
+        }
+        InvocationStatus::Completed(CompletedInvocation {
+            response_result, ..
+        }) => match response_result {
+            ResponseResultRef::Success(_)
+            | ResponseResultRef::Completed(CompletionStatus::Success) => {
+                PartitionProcessorRpcResponse::Status(client::InvocationStatus {
+                    state: client::InvocationState::Succeeded,
+                    error: None,
+                })
+            }
+            ResponseResultRef::Failure(error) => {
+                PartitionProcessorRpcResponse::Status(client::InvocationStatus {
+                    state: client::InvocationState::Failed,
+                    error: Some(error),
+                })
+            }
+            ResponseResultRef::Killed
+            | ResponseResultRef::Completed(CompletionStatus::Failure(_)) => {
+                // todo: Do we need the full error, or is the error code is enough.
+                // If the error code is enough then this should be way more efficient
+                // since we have this already.
+                // Currently we have to get the full failure output to extract the full
+                // error.
+                match storage.get_output(invocation_id).await? {
+                    None => PartitionProcessorRpcResponse::NotFound,
+                    Some(result) => {
+                        let ResponseResult::Failure(error) = result else {
+                            warn!(invocation_id=%invocation_id, "invocation response result inconsistency");
+                            return Err(StorageError::DataIntegrityError);
+                        };
+
+                        PartitionProcessorRpcResponse::Status(client::InvocationStatus {
+                            state: client::InvocationState::Failed,
+                            error: Some(error),
+                        })
+                    }
+                }
+            }
+        },
+        InvocationStatus::Free => PartitionProcessorRpcResponse::NotFound,
+    };
+
+    Ok(response)
 }
