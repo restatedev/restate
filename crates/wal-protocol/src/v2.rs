621a14261237406b6a6f0d69a0e7cba143e11d34
Muhamad Awad <muhamad@restate.dev>
2026-09-04 11:58:04 +0200

    self review

--- crates/wal-protocol/src/v2.rs
+++ crates/wal-protocol/src/v2.rs
@@ -565,6 +565,15 @@ impl ErasedCommand {
     }
 }
 
+impl<C> From<C> for ErasedCommand
+where
+    C: Command,
+{
+    fn from(value: C) -> Self {
+        Self::new(value)
+    }
+}
+
 #[cfg(test)]
 mod test {
 
