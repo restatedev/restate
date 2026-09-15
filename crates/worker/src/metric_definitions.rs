747a7ba8f39eafa66c1ce9ab36b48bec930571e3
MohamedBassem <me@mbassem.com>
2026-09-15 11:40:28 +0100

    [worker] add counter for journal entry bytes
    Similar to how we're reporting the number of journal entries broken down by type, let's also report the size of those entries.

--- crates/worker/src/metric_definitions.rs
+++ crates/worker/src/metric_definitions.rs
@@ -49,6 +49,7 @@ pub const SNAPSHOT_AGE: &str = "restate.partition.snapshot_age.seconds";
 pub const USAGE_LEADER_ACTION_COUNT: &str = "restate.usage.leader_action_count.total";
 
 pub const USAGE_LEADER_JOURNAL_ENTRY_COUNT: &str = "restate.usage.leader_journal_entry_count.total";
+pub const USAGE_LEADER_JOURNAL_ENTRY_BYTES: &str = "restate.usage.leader_journal_entry_bytes.total";
 
 pub const NUM_PARTITIONS: &str = "restate.num_partitions";
 pub const NUM_ACTIVE_PARTITIONS: &str = "restate.num_active_partitions";
