8e255f3dc723e007c979b5e188a7649f2afc261d
MohamedBassem <me@mbassem.com>
2026-09-24 11:07:55 +0100

    [cleaner] Time slice the cleanup interval into smaller key range slices
    Currently, the cleaner by default runs every hour and generates large spikes of expensive `PurgeInvocations` and `PurgeJournal` commands. How large the spike, depends on how many invocations expire per cleanup interval. We've seen this clearly causing multi-second spikes of write to read latency.
    
    To smoothen the cleaner spikes, this PR instead redefines the cleaner interval to be the interval after which the cleaner is expected to have done a full sweep on all invocations in a partition. Then it divides that interval into smaller slices of 5mins (or up to 1000 slice), and each one of those slices scan only a slice of the partitions key range. For example, using the default 1 hour cleaner interval, the full invocation status sweep will be done in 12 smaller scans each running every 5mins. Each scan scans a disjoint subset of the invocation status table.

--- crates/worker/src/partition/leadership/mod.rs
+++ crates/worker/src/partition/leadership/mod.rs
@@ -722,6 +722,7 @@ where
             let cleaner = Cleaner::new(
                 partition_store.clone(),
                 processor.partition_id(),
+                processor.key_range(),
                 config.worker.cleanup_interval(),
             );
 
