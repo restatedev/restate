9e3008a54086ef3acfd9720883fc6ff71c73b3ce
Pavel Tcholakov <pavel@restate.dev>
2026-09-25 09:11:19 +0300

    Use the configured retry policy for Azure object store clients (#5418)

--- crates/object-store-util/src/lib.rs
+++ crates/object-store-util/src/lib.rs
@@ -174,9 +174,12 @@ pub async fn create_object_store_client(
 
             Arc::new(builder.build()?)
         }
-        ObjectStoreScheme::MicrosoftAzure => {
-            Arc::new(MicrosoftAzureBuilder::from_env().with_url(url).build()?)
-        }
+        ObjectStoreScheme::MicrosoftAzure => Arc::new(
+            MicrosoftAzureBuilder::from_env()
+                .with_url(url)
+                .with_retry(from_retry_policy(retry_policy))
+                .build()?,
+        ),
         ObjectStoreScheme::GoogleCloudStorage => Arc::new(
             GoogleCloudStorageBuilder::from_env()
                 .with_url(url)
