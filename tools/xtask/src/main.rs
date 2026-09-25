c0a2702fcc15d9af0ad2ded4d442051b2bc6cd41
MohamedBassem <me@mbassem.com>
2026-09-25 14:00:00 +0100

    [pp rpc] Introduce typed PartitionProcessorRpc structure
    Ok.

--- tools/xtask/src/main.rs
+++ tools/xtask/src/main.rs
@@ -11,7 +11,6 @@
 use std::future::pending;
 use std::io::Write;
 use std::num::NonZeroUsize;
-use std::sync::Arc;
 use std::{env, io};
 
 use anyhow::bail;
@@ -28,20 +27,13 @@ use restate_service_protocol_v4::discovery::ServiceDiscovery;
 use restate_service_protocol_v4::serdes::SerdesClient;
 use restate_storage_query_datafusion::table_docs;
 use restate_types::config::Configuration;
-use restate_types::identifiers::{InvocationId, PartitionProcessorRpcRequestId};
-use restate_types::invocation::client::{
-    AttachInvocationResponse, CancelInvocationResponse, GetInvocationOutputResponse,
-    GetInvocationStatusResponse, InvocationClient, InvocationOutput, KillInvocationResponse,
-    PatchDeploymentId, PauseInvocationResponse, PurgeInvocationResponse,
-    RestartAsNewInvocationResponse, ResumeInvocationResponse, SubmittedInvocationNotification,
-};
-use restate_types::invocation::{
-    InvocationQuery, InvocationRequest, InvocationResponse, InvocationTermination,
-};
-use restate_types::journal_v2::{EntryIndex, Signal};
+use restate_types::identifiers::PartitionProcessorRpcRequestId;
+use restate_types::invocation::InvocationTermination;
 use restate_types::live::Constant;
 use restate_types::net::listener::Listeners;
-use restate_types::partition_processor::client::PartitionProcessorClientError;
+use restate_types::partition_processor::client::{
+    PartitionProcessorClient, PartitionProcessorClientError, PartitionProcessorRpc,
+};
 use restate_types::partitions::state::PartitionReplicaSetStates;
 use restate_types::retries::RetryPolicy;
 use restate_types::schema::kafka::KafkaCluster;
@@ -98,131 +90,12 @@ impl SubscriptionController for Mock {
     }
 }
 
-impl InvocationClient for Mock {
-    fn append_invocation_and_wait_submit_notification(
+impl PartitionProcessorClient for Mock {
+    fn send<R: PartitionProcessorRpc>(
         &self,
         _: PartitionProcessorRpcRequestId,
-        _: Arc<InvocationRequest>,
-    ) -> impl Future<Output = Result<SubmittedInvocationNotification, PartitionProcessorClientError>>
-    + Send {
-        pending()
-    }
-
-    fn append_invocation_and_wait_output(
-        &self,
-        _: PartitionProcessorRpcRequestId,
-        _: Arc<InvocationRequest>,
-    ) -> impl Future<Output = Result<InvocationOutput, PartitionProcessorClientError>> + Send {
-        pending()
-    }
-
-    fn attach_invocation(
-        &self,
-        _: PartitionProcessorRpcRequestId,
-        _: InvocationQuery,
-    ) -> impl Future<Output = Result<AttachInvocationResponse, PartitionProcessorClientError>> + Send
-    {
-        pending()
-    }
-
-    fn get_invocation_output(
-        &self,
-        _: PartitionProcessorRpcRequestId,
-        _: InvocationQuery,
-    ) -> impl Future<Output = Result<GetInvocationOutputResponse, PartitionProcessorClientError>> + Send
-    {
-        pending()
-    }
-
-    fn get_invocation_status(
-        &self,
-        _: PartitionProcessorRpcRequestId,
-        _: InvocationId,
-    ) -> impl Future<Output = Result<GetInvocationStatusResponse, PartitionProcessorClientError>> + Send
-    {
-        pending()
-    }
-
-    fn append_invocation_response(
-        &self,
-        _: PartitionProcessorRpcRequestId,
-        _: InvocationResponse,
-    ) -> impl Future<Output = Result<(), PartitionProcessorClientError>> + Send {
-        pending()
-    }
-
-    fn append_signal(
-        &self,
-        _: PartitionProcessorRpcRequestId,
-        _: InvocationId,
-        _: Signal,
-    ) -> impl Future<Output = Result<(), PartitionProcessorClientError>> + Send {
-        pending()
-    }
-
-    fn cancel_invocation(
-        &self,
-        _: PartitionProcessorRpcRequestId,
-        _: InvocationId,
-    ) -> impl Future<Output = Result<CancelInvocationResponse, PartitionProcessorClientError>> + Send
-    {
-        pending()
-    }
-
-    fn kill_invocation(
-        &self,
-        _: PartitionProcessorRpcRequestId,
-        _: InvocationId,
-    ) -> impl Future<Output = Result<KillInvocationResponse, PartitionProcessorClientError>> + Send
-    {
-        pending()
-    }
-
-    fn purge_invocation(
-        &self,
-        _: PartitionProcessorRpcRequestId,
-        _: InvocationId,
-    ) -> impl Future<Output = Result<PurgeInvocationResponse, PartitionProcessorClientError>> + Send
-    {
-        pending()
-    }
-
-    fn purge_journal(
-        &self,
-        _: PartitionProcessorRpcRequestId,
-        _: InvocationId,
-    ) -> impl Future<Output = Result<PurgeInvocationResponse, PartitionProcessorClientError>> + Send
-    {
-        pending()
-    }
-
-    fn restart_as_new_invocation(
-        &self,
-        _: PartitionProcessorRpcRequestId,
-        _: InvocationId,
-        _: EntryIndex,
-        _: PatchDeploymentId,
-    ) -> impl Future<Output = Result<RestartAsNewInvocationResponse, PartitionProcessorClientError>> + Send
-    {
-        pending()
-    }
-
-    fn resume_invocation(
-        &self,
-        _: PartitionProcessorRpcRequestId,
-        _: InvocationId,
-        _: PatchDeploymentId,
-    ) -> impl Future<Output = Result<ResumeInvocationResponse, PartitionProcessorClientError>> + Send
-    {
-        pending()
-    }
-
-    fn pause_invocation(
-        &self,
-        _: PartitionProcessorRpcRequestId,
-        _: InvocationId,
-    ) -> impl Future<Output = Result<PauseInvocationResponse, PartitionProcessorClientError>> + Send
-    {
+        _: R,
+    ) -> impl Future<Output = Result<R::Response, PartitionProcessorClientError>> + Send {
         pending()
     }
 }
